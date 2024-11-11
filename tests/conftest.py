08648683cfaa68e2969803b0bdf1c38624061774
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-11 16:57:02 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,15 @@
 # tests/conftest.py
 
+import sys
+import os
 import pytest
-from myapp import create_app
+
+# Додавання кореневої директорії до sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from myapp import create_app  # Переконайтеся, що шлях правильний
 
 @pytest.fixture
 def app():
     app = create_app()
-    yield app
-    # Додаткові дії після тесту, якщо необхідно
+    return app
