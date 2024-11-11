2173adf72ca72762d35643c088f87568e7946b7b
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-11 23:59:18 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,1 +1,11 @@
-# Основні налаштування (токени, URL-адреси тощо)
+# config/settings.py
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+DATABASE_URL = os.getenv('DATABASE_URL')
+ENABLE_QUIZZES = os.getenv('ENABLE_QUIZZES', 'false').lower() == 'true'
+ENABLE_NEWS = os.getenv('ENABLE_NEWS', 'false').lower() == 'true'# Основні налаштування (токени, URL-адреси тощо)
