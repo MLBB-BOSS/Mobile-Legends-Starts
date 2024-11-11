ae99324065b63881a6c47389f791b2dd73394325
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-11 22:59:27 +0200

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -6,3 +6,4 @@ from .profile_handler import view_profile
 from .leaderboard_handler import view_leaderboard
 from .heroes_info_handler import handle_heroes_info
 from .help_handler import handle_help
+from .callback_handler import handle_callback  # Якщо використовуєте CallbackQueryHandler
