b0ea218a0baf613bceecfb8048bc7632e58ff8f8
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-11 22:41:50 +0200

    Create __init__.py

---
+++ core/__init__.py
@@ -0,0 +1,8 @@
+# core/__init__.py
+
+from .info_handler import get_main_menu
+from .screenshot_handler import handle_screenshot
+from .profile_handler import view_profile
+from .leaderboard_handler import view_leaderboard
+from .heroes_info_handler import handle_heroes_info
+from .help_handler import handle_help
