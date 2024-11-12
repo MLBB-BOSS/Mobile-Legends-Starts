dca8075dff47c05eac2efad1c83fc17f19c09e53
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-12 17:31:10 +0200

    Update contribution.py

--- models/contribution.py
+++ models/contribution.py
@@ -1,20 +1,12 @@
 # models/contribution.py
 
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-from services.database import Base
-
-class Contribution(Base):
-    __tablename__ = 'contributions'
-
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, index=True)
-    contribution_type = Column(String, index=True)
-    points = Column(Integer)
-    date = Column(String)
-
-    # Ставимо відношення до таблиці "User" (потрібно створити модель користувачів)
-    user = relationship("User", back_populates="contributions")
+class Contribution:
+    def __init__(self, user_id, contribution_type, points, date, screenshot_url):
+        self.user_id = user_id
+        self.contribution_type = contribution_type
+        self.points = points
+        self.date = date
+        self.screenshot_url = screenshot_url
 
     def __repr__(self):
-        return f"<Contribution(user_id={self.user_id}, contribution_type={self.contribution_type}, points={self.points})>"
+        return f"<Contribution(user_id={self.user_id}, type={self.contribution_type}, points={self.points})>"
