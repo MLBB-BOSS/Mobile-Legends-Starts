b6068b32b3d2f2053aaf98459f57018edcd59ff6
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-12 17:39:12 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -6,6 +6,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-DATABASE_URL = os.getenv('DATABASE_URL')
-ENABLE_QUIZZES = os.getenv('ENABLE_QUIZZES', 'false').lower() == 'true'
-ENABLE_NEWS = os.getenv('ENABLE_NEWS', 'false').lower() == 'true'# Основні налаштування (токени, URL-адреси тощо)
+
+# AWS S3 Configuration
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_REGION = os.getenv('AWS_REGION', 'eu-north-1')
+AWS_S3_BUCKET_NAME = os.getenv('AWS_S3_BUCKET_NAME')
