a1ae343e30b7339d392fda9fe1d40b12cc89351e
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-12 18:09:25 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -3,12 +3,20 @@
 import os
 from dotenv import load_dotenv
 
+# Завантаження змінних середовища з файлу .env
 load_dotenv()
 
+# Telegram Bot Token
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 
 # AWS S3 Configuration
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_REGION = os.getenv('AWS_REGION', 'eu-north-1')
+AWS_REGION = os.getenv('AWS_REGION')
 AWS_S3_BUCKET_NAME = os.getenv('AWS_S3_BUCKET_NAME')
+
+# Перевірка наявності критичних змінних середовища
+if not TELEGRAM_BOT_TOKEN:
+    raise ValueError("TELEGRAM_BOT_TOKEN не встановлено в змінних середовища.")
+if not AWS_ACCESS_KEY_ID or not AWS_SECRET_ACCESS_KEY or not AWS_S3_BUCKET_NAME:
+    raise ValueError("Одна або декілька AWS змінних середовища не встановлено.")
