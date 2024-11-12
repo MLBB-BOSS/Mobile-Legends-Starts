d2753c5332d90c467a4d5c8e7714a54ebd1ef144
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-12 17:51:49 +0200

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,6 +1,19 @@
 # core/__init__.py
 
+from .info_handler import get_main_menu
 from .screenshot_handler import handle_screenshot
 from .profile_handler import view_profile
+from .leaderboard_handler import view_leaderboard
+from .heroes_info_handler import handle_heroes_info
+from .help_handler import handle_help
+from .callback_handler import handle_callback
 
-__all__ = ['handle_screenshot', 'view_profile']
+__all__ = [
+    'get_main_menu',
+    'handle_screenshot',
+    'view_profile',
+    'view_leaderboard',
+    'handle_heroes_info',
+    'handle_help',
+    'handle_callback'
+]
