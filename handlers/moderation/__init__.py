34be903607d893305bf03494278c91efd2ca5d76
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-13 12:47:53 +0200

    Delete handlers directory

--- handlers/moderation/__init__.py
+++
@@ -1,8 +0,0 @@
-# handlers/moderation/__init__.py
-from .vote_handler import VoteHandler
-from .moderation_handler import ModerationHandler
-
-__all__ = [
-    'VoteHandler',
-    'ModerationHandler'
-]
--- handlers/user/__init__.py
+++
@@ -1,8 +0,0 @@
-# handlers/user/__init__.py
-from .profile_handler import ProfileHandler
-from .achievement_handler import AchievementHandler
-
-__all__ = [
-    'ProfileHandler',
-    'AchievementHandler'
-]
