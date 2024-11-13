069664c1b118a8d911603334d739de8116c70397
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-13 21:45:52 +0200

    Update connection.py

--- database/connection.py
+++ database/connection.py
@@ -6,7 +6,10 @@ from config.settings import settings
 
 DATABASE_URL = settings.ASYNC_DATABASE_URL or settings.DATABASE_URL
 
+# Створення асинхронного engine
 engine = create_async_engine(DATABASE_URL, echo=True)
+
+# Створення sessionmaker
 AsyncSessionLocal = sessionmaker(
     bind=engine,
     class_=AsyncSession,
@@ -14,5 +17,6 @@ AsyncSessionLocal = sessionmaker(
 )
 
 async def get_db():
+    """Функція для отримання сесії бази даних"""
     async with AsyncSessionLocal() as session:
         yield session
