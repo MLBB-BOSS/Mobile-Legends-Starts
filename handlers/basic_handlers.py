fd1362139aeb6c71ed5aaa0cef653d767e6cab47
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-14 01:41:00 +0200

    Update basic_handlers.py

--- handlers/basic_handlers.py
+++ handlers/basic_handlers.py
@@ -1,13 +1,7 @@
 # handlers/basic_handlers.py
-
 from aiogram import types
-from aiogram.dispatcher.filters import Command
 from core.bot import dp
 
-@dp.message_handler(Command("start"))
-async def send_welcome(message: types.Message):
-    welcome_text = (
-        "👋 <b>Вітаємо!</b>\n\n"
-        "Цей бот допоможе вам з інформацією про героїв та керування скріншотами."
-    )
-    await message.reply(welcome_text, parse_mode="HTML")
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
+    await message.reply("Вітаю! Я ваш бот, готовий допомогти.")
