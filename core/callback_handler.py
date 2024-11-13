9c9919ebacef951bbaefab129f26b2baab15c2a2
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-13 21:35:34 +0200

    Update callback_handler.py

--- core/callback_handler.py
+++ core/callback_handler.py
@@ -2,10 +2,8 @@
 
 from aiogram import types
 from aiogram.dispatcher.filters import Text
-from aiogram.dispatcher import Dispatcher
 from core.bot import dp
 
-# Приклад обробника колбеків
 @dp.callback_query_handler(Text(startswith="example"))
 async def handle_example_callback(callback_query: types.CallbackQuery):
     await callback_query.answer("Це приклад обробника колбеків.")
