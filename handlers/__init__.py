64b6949f466bd44c7f4594a5ac083724255c0442
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-13 18:11:03 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,3 +1,3 @@
-from .screenshot_handler import handle_screenshot, list_screenshots
+# handlers/__init__.py
 
-__all__ = ['handle_screenshot', 'list_screenshots']
+# Пустий файл ініціалізації пакету
