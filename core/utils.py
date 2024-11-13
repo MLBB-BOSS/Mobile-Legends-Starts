8c18f3fbf0e940655a90b520ab5eb7ae347c476f
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-13 03:12:27 +0200

    Create utils.py

---
+++ core/utils.py
@@ -0,0 +1,12 @@
+# core/utils.py
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+async def some_shared_function():
+    """
+    Приклад спільної асинхронної функції.
+    """
+    # Ваш код тут
+    pass
