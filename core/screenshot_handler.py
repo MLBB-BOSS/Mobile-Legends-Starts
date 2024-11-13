69b11407117e60ea61ca6a098c445082b95b0dfb
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-14 00:32:18 +0200

    Delete core/screenshot_handler.py

--- core/screenshot_handler.py
+++
@@ -1,9 +0,0 @@
-# core/screenshot_handler.py
-
-from aiogram import types
-from core.bot import dp
-from services.screenshot_service import handle_screenshot_upload
-
-@dp.message_handler(content_types=types.ContentType.PHOTO)
-async def handle_photo(message: types.Message):
-    await handle_screenshot_upload(message)
