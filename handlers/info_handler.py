e36551fc75939b341a9e18c89c7937dbd8ceaa60
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-15 00:13:21 +0200

    Update info_handler.py

--- handlers/info_handler.py
+++ handlers/info_handler.py
@@ -1,10 +1,9 @@
 # handlers/info_handler.py
-
-from aiogram import Router
+from aiogram import F, Router
 from aiogram.types import Message
 
 info_router = Router()
 
-@info_router.message(commands=["info"])
+@info_router.message(F.text == "/info")
 async def info_command(message: Message):
-    await message.reply("Це загальна інформація!")
+    await message.reply("Це загальна інформація про бота Mobile Legends.")
