56709322d7eceed694809f5aaab6676d62076347
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-15 00:45:09 +0200

    Update bot.py

--- core/bot.py
+++ core/bot.py
@@ -1,9 +1,10 @@
-# core/bot_runner.py
+# core/bot.py
 
-import asyncio
 import logging
-from core.bot import bot, dp
-from handlers.hero_handler import router as hero_router
+from aiogram import Bot, Dispatcher
+from aiogram.client.bot import DefaultBotProperties
+from aiogram.fsm.storage.memory import MemoryStorage
+from config.settings import settings
 
 # Налаштування логування
 logging.basicConfig(
@@ -12,26 +13,7 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-async def on_startup():
-    """Ініціалізація всіх необхідних налаштувань при запуску бота."""
-    logger.info("Бот запускається...")
-    dp.include_router(hero_router)
-
-async def main():
-    await on_startup()
-    try:
-        logger.info("Початок роботи бота...")
-        await dp.start_polling(bot)
-    except Exception as e:
-        logger.error(f"❌ Помилка під час роботи бота: {e}")
-    finally:
-        await bot.session.close()
-        logger.info("Бот зупинено.")
-
-if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        logger.info("👋 Бот зупинено користувачем.")
-    except Exception as e:
-        logger.error(f"❌ Несподівана помилка: {e}")
+# Ініціалізація бота та диспетчера
+bot = Bot(token=settings.TELEGRAM_BOT_TOKEN, default=DefaultBotProperties(parse_mode="HTML"))
+storage = MemoryStorage()
+dp = Dispatcher(storage=storage)
