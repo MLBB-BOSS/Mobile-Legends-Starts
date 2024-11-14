df288d3eaaf4ed464702c4f610bbf3ecc7f52fe1
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-14 22:42:21 +0200

    Update bot.py

--- core/bot.py
+++ core/bot.py
@@ -2,15 +2,18 @@
 
 import logging
 from aiogram import Bot, Dispatcher
+from aiogram.client.bot import DefaultBotProperties  # Оптимізований імпорт
 from aiogram.fsm.storage.memory import MemoryStorage
 from config.settings import settings
 
+# Налаштування логування
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 logger = logging.getLogger(__name__)
 
-bot = Bot(token=settings.TELEGRAM_BOT_TOKEN, parse_mode="HTML")
+# Ініціалізація бота з використанням DefaultBotProperties для встановлення parse_mode
+bot = Bot(token=settings.TELEGRAM_BOT_TOKEN, default=DefaultBotProperties(parse_mode="HTML"))
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
