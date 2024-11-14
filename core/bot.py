4fd0c992afc421de6dd8635851e9ba0509c60f5a
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-14 21:34:54 +0200

    Update bot.py

--- core/bot.py
+++ core/bot.py
@@ -1,36 +1,16 @@
-# core/bot_runner.py
+# core/bot.py
 
-import asyncio
 import logging
-from aiogram import Dispatcher
-from core.bot import bot, dp
-from handlers.basic_handlers import basic_router
-from handlers.callback_handler import callback_router
-from handlers.help_handler import help_router
-from handlers.heroes_info_handler import heroes_info_router
-from handlers.info_handler import info_router
-from handlers.leaderboard_handler import leaderboard_router
-from handlers.profile_handler import profile_router
-from handlers.screenshot_handler import screenshot_router
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+from config.settings import settings
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
 
-async def on_startup():
-    dp.include_router(basic_router)
-    dp.include_router(callback_router)
-    dp.include_router(help_router)
-    dp.include_router(heroes_info_router)
-    dp.include_router(info_router)
-    dp.include_router(leaderboard_router)
-    dp.include_router(profile_router)
-    dp.include_router(screenshot_router)
-
-async def main():
-    await on_startup()
-    await dp.start_polling(bot)
-
-if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        logging.info("👋 Бот зупинено користувачем.")
+bot = Bot(token=settings.TELEGRAM_BOT_TOKEN, parse_mode="HTML")
+storage = MemoryStorage()
+dp = Dispatcher(storage=storage)
