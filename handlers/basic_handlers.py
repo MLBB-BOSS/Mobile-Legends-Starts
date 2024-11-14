48f82b57fbfe897b845c5205585a29d48f8fc62e
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-14 21:20:24 +0200

    Update basic_handlers.py

--- handlers/basic_handlers.py
+++ handlers/basic_handlers.py
@@ -1,7 +1,19 @@
 # handlers/basic_handlers.py
-from aiogram import types
-from core.bot import dp
 
-@dp.message_handler(commands=['start'])
-async def start_command(message: types.Message):
+from aiogram import Router
+from aiogram.types import Message
+
+basic_router = Router()
+
+@basic_router.message(commands=["start"])
+async def start_command(message: Message):
     await message.reply("Вітаю! Я ваш бот, готовий допомогти.")
+
+@basic_router.message(commands=["help"])
+async def help_command(message: Message):
+    await message.reply("📖 Допомога\n\n"
+                        "/start - Запустити бота\n"
+                        "/help - Отримати допомогу\n"
+                        "/screenshots - Переглянути скріншоти\n"
+                        "/leaderboard - Таблиця лідерів\n"
+                        "/profile - Ваш профіль")
