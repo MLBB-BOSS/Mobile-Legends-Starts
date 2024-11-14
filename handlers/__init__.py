10445b31e9c89e5bcbbb80279c7734dc1765ba47
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-14 10:14:34 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,5 +1,3 @@
-# handlers/__init__.py
-
 from .callback_handler import *
 from .help_handler import *
 from .heroes_info_handler import *
@@ -7,3 +5,5 @@ from .info_handler import *
 from .leaderboard_handler import *
 from .profile_handler import *
 from .screenshot_handler import *
+from .hero_handler import *  # Додаємо обробник hero_handler
+from .main_menu_handler import *  # Додаємо обробник main_menu_handler
