7e85f1de03cfb8a1d1ef624d3a518a71837d104f
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-15 00:44:44 +0200

    Update bot_runner.py

--- core/bot_runner.py
+++ core/bot_runner.py
@@ -5,7 +5,7 @@ import logging
 from core.bot import bot, dp
 from handlers.hero_handler import router as hero_router
 
-# Налаштування логування з відповідним форматом
+# Налаштування логування
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
@@ -15,13 +15,13 @@ logger = logging.getLogger(__name__)
 async def on_startup():
     """Ініціалізація всіх необхідних налаштувань при запуску бота."""
     logger.info("Бот запускається...")
-    dp.include_router(hero_router)  # Додаємо маршрутизатор обробників
+    dp.include_router(hero_router)
 
 async def main():
     await on_startup()
     try:
         logger.info("Початок роботи бота...")
-        await dp.start_polling(bot)  # Виклик без контекстного менеджера
+        await dp.start_polling(bot)
     except Exception as e:
         logger.error(f"❌ Помилка під час роботи бота: {e}")
     finally:
