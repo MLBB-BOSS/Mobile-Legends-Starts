7d3ae52d2625482082010a91e2eb05985459bd97
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-15 00:11:41 +0200

    Update screenshot_handler.py

--- handlers/screenshot_handler.py
+++ handlers/screenshot_handler.py
@@ -1,12 +1,11 @@
 # handlers/screenshot_handler.py
-
-from aiogram import Router
+from aiogram import F, Router
 from aiogram.types import Message
 from services.screenshot_service import get_hero_info
 
 screenshot_router = Router()
 
-@screenshot_router.message(commands=["screenshots"])
+@screenshot_router.message(F.text == "/screenshots")
 async def screenshots_command(message: Message):
     hero_info = get_hero_info()
     await message.reply(f"Скріншоти: {hero_info}")
