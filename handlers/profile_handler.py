0d82e18ab10a89faa774715abe2c415879e86b66
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-14 21:28:20 +0200

    Update profile_handler.py

--- handlers/profile_handler.py
+++ handlers/profile_handler.py
@@ -1,1 +1,10 @@
+# handlers/profile_handler.py
 
+from aiogram import Router
+from aiogram.types import Message
+
+profile_router = Router()
+
+@profile_router.message(commands=["profile"])
+async def profile_command(message: Message):
+    await message.reply("Ваш профіль!")
