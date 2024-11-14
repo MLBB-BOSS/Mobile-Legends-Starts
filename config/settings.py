a381491985e6351ab21874f674b5ce356a1fe0a2
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-15 00:45:57 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,15 +1,8 @@
 # config/settings.py
 
-from pydantic_settings import BaseSettings  # Імпортуємо BaseSettings з pydantic_settings
-from pydantic import Field  # Імпортуємо Field з pydantic
+from pydantic_settings import BaseSettings, Field
 
 class Settings(BaseSettings):
     TELEGRAM_BOT_TOKEN: str = Field(..., env="TELEGRAM_BOT_TOKEN")
-    DATABASE_URL: str = Field(..., env="DATABASE_URL")
-    ASYNC_DATABASE_URL: str = Field("", env="ASYNC_DATABASE_URL")
-
-    class Config:
-        env_file = ".env"
-        case_sensitive = True
 
 settings = Settings()
