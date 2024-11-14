d1cbc92775d86c883618958dd9caa27d1659d565
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-15 00:12:55 +0200

    Update leaderboard_handler.py

--- handlers/leaderboard_handler.py
+++ handlers/leaderboard_handler.py
@@ -1,10 +1,9 @@
 # handlers/leaderboard_handler.py
-
-from aiogram import Router
+from aiogram import F, Router
 from aiogram.types import Message
 
 leaderboard_router = Router()
 
-@leaderboard_router.message(commands=["leaderboard"])
+@leaderboard_router.message(F.text == "/leaderboard")
 async def leaderboard_command(message: Message):
     await message.reply("Тут буде таблиця лідерів.")
