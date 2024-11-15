89ebbf024b443ab1b87b76c53161e1a3f369d19e
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-15 19:03:04 +0200

    Update base.py

--- models/base.py
+++ models/base.py
@@ -1,5 +1,19 @@
-# models/base.py
+from datetime import datetime
+from sqlalchemy import DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+from services.database import Base
 
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+class TimeStampedBase(Base):
+    __abstract__ = True
+    
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=datetime.utcnow,
+        nullable=False
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow,
+        nullable=False
+    )
