821fdb4c5b263ae23f0d18bf8208bda81f653196
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-15 05:43:50 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,10 +1,16 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
+    # Основні налаштування
     TELEGRAM_BOT_TOKEN: str
+    
+    # Налаштування бази даних
+    database_url: str = "postgresql+asyncpg://user:password@localhost/dbname"
+    async_database_url: str = "postgresql+asyncpg://user:password@localhost/dbname_async"
 
     class Config:
         env_file = '.env'
         env_file_encoding = 'utf-8'
+        case_sensitive = False  # Дозволяє використовувати різні стилі іменування змінних
 
 settings = Settings()
