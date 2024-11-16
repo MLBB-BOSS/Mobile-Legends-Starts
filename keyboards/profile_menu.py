dc43e25a12b0ba846211f4fd9e257529d3d58fca
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-16 03:01:53 +0200

    Create profile_menu.py

---
+++ keyboards/profile_menu.py
@@ -0,0 +1,15 @@
+from .base import BaseKeyboard
+
+class ProfileMenu(BaseKeyboard):
+    """Меню особистого кабінету"""
+    
+    @classmethod
+    def get_profile_menu(cls) -> InlineKeyboardMarkup:
+        """Повертає меню особистого кабінету"""
+        buttons = [
+            [{"text": "📊 Статистика", "callback_data": "statistics"}],
+            [{"text": "🏅 Мої Досягнення", "callback_data": "achievements"}],
+            [{"text": "⚙️ Налаштування", "callback_data": "settings"}],
+            [{"text": "🔙 Назад", "callback_data": "back_to_main"}]
+        ]
+        return cls.create_keyboard(buttons, is_inline=True)
