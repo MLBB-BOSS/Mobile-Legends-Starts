a69f5efaba03d4e4f3fb6b39bccd3b72c11c7ee1
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-16 19:29:31 +0200

    Update utils.py

--- keyboards/utils.py
+++ keyboards/utils.py
@@ -1,10 +1,14 @@
-# Шлях: keyboards/utils.py
-# Цей файл містить допоміжні функції для створення кнопок клавіатури
+# File: keyboards/utils.py
 from aiogram.types import InlineKeyboardButton
 from typing import List, Union, Dict
 
 def create_keyboard_row(*buttons: Union[Dict, InlineKeyboardButton]) -> List[InlineKeyboardButton]:
-    """Створює ряд кнопок з словників або готових кнопок"""
+    """
+    Створює ряд кнопок з словників або готових кнопок
+    
+    :param buttons: Кнопки у вигляді словників або об'єктів InlineKeyboardButton
+    :return: Список кнопок InlineKeyboardButton
+    """
     row = []
     for button in buttons:
         if isinstance(button, dict):
