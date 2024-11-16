e88833e2d55b8bf2dc852318a2deec11464436a4
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-16 21:24:37 +0200

    Update message_handlers.py

--- handlers/message_handlers.py
+++ handlers/message_handlers.py
@@ -16,7 +16,7 @@ async def cmd_start(message: Message):
         reply_markup=MainMenu.get_main_menu()
     )
 
-@router.message(F.text == "🧭 Навігація")
+@router.message(F.text == "🧭")
 async def handle_navigation(message: Message):
     """Обробляє натискання кнопки Навігація"""
     logger.info(f"Отримано команду навігації: {message.text}")
@@ -25,7 +25,7 @@ async def handle_navigation(message: Message):
         reply_markup=NavigationMenu.get_navigation_menu()
     )
 
-@router.message(F.text == "🪧 Мій Кабінет")
+@router.message(F.text == "🪪")
 async def handle_profile(message: Message):
     """Обробляє натискання кнопки Мій Кабінет"""
     logger.info(f"Отримано команду профілю: {message.text}")
