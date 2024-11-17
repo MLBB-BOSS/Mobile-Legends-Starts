b3103eb3630c79c9b631c22a1c42b7e214d5638e
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-17 11:43:40 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,2 +1,4 @@
-# handlers/__init__.py
-# Порожній файл для позначення директорії як Python пакет
+from .start_command import router as start_router
+# Імпортуйте інші роутери, якщо вони є
+
+__all__ = ['start_router']  # Додайте інші роутери, якщо потрібно
