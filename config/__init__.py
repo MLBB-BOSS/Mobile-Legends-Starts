75cbca5ec4abe75b1fda52d767abfd142e16d037
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-17 12:41:20 +0200

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,1 +1,14 @@
 # File: config/__init__.py
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings
+
+class Config(BaseSettings):
+    TELEGRAM_BOT_TOKEN: SecretStr
+
+    class Config:
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
+
+config = Config()
+
+__all__ = ['config']
