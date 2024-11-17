468e5cd15fa7891e408040530bb6c23026d07307
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-17 13:52:10 +0200

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,3 +1,9 @@
+from .keyboard_utils import create_keyboard_row
 from .main_menu import MainMenu
+from .hero_menu import HeroMenu
 
-__all__ = ['MainMenu']
+__all__ = [
+    'create_keyboard_row',
+    'MainMenu',
+    'HeroMenu'
+]
