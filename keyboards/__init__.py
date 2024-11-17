c8a130f96112056915b3f2439f5e23d14a47927f
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-17 14:28:34 +0200

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,9 +1,8 @@
+# File: keyboards/__init__.py
 from .keyboard_utils import create_keyboard_row
 from .main_menu import MainMenu
-from .hero_menu import HeroMenu
 
 __all__ = [
     'create_keyboard_row',
-    'MainMenu',
-    'HeroMenu'
+    'MainMenu'
 ]
