acf713cc219810c4cb9b6a3db905f1ca68ab2ed5
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-19 20:25:25 +0200

    Update navigation_menu.py

--- keyboards/navigation_menu.py
+++ keyboards/navigation_menu.py
@@ -1,5 +1,5 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-from config.messages.localization import get_message as _
+from config.localization.localize import get_message as _
 
 class NavigationMenu:
     def get_navigation_menu(self) -> ReplyKeyboardMarkup:
