5724921f2573d77f6a2fe12347ba1e17fe1dd75d
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-19 20:41:33 +0200

    Update profile_menu.py

--- keyboards/profile_menu.py
+++ keyboards/profile_menu.py
@@ -1,5 +1,5 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-from config.localization.localize import get_message as _
+from utils.localization import get_message as _
 
 class ProfileMenu:
     def get_profile_menu(self) -> ReplyKeyboardMarkup:
