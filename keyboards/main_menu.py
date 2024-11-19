384e9ef62273086d43fcbe0f26d67fa398f776b8
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-19 20:00:13 +0200

    Update main_menu.py

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -1,14 +1,14 @@
-# File: keyboards/main_menu.py
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-from utils.localization import loc
-
+# keyboards/main_menu.py
 class MainMenu:
     @staticmethod
     def get_main_menu() -> ReplyKeyboardMarkup:
-        buttons = [
-            [
-                KeyboardButton(text=loc.get_message("buttons.navigation")),
-                KeyboardButton(text=loc.get_message("buttons.profile"))
-            ]
-        ]
-        return ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
+        keyboard = ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(text=loc.get_message("buttons.navigation")),
+                    KeyboardButton(text=loc.get_message("buttons.profile"))
+                ]
+            ],
+            resize_keyboard=True
+        )
+        return keyboard
