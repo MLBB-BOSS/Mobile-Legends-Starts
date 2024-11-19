a1564d51fdc3a1c7900d87e92e827f8571f8a9fd
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-19 20:46:16 +0200

    Update navigation_menu.py

--- keyboards/navigation_menu.py
+++ keyboards/navigation_menu.py
@@ -1,21 +1,21 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-from utils.localization import get_message as _
+from utils.localization import loc
 
 class NavigationMenu:
     def get_navigation_menu(self) -> ReplyKeyboardMarkup:
         keyboard = ReplyKeyboardMarkup(
             keyboard=[
                 [
-                    KeyboardButton(text=_("buttons.guides")),
-                    KeyboardButton(text=_("buttons.characters"))
+                    KeyboardButton(text=loc.get_message("buttons.guides")),
+                    KeyboardButton(text=loc.get_message("buttons.characters"))
                 ],
                 [
-                    KeyboardButton(text=_("buttons.counter_picks")),
-                    KeyboardButton(text=_("buttons.builds"))
+                    KeyboardButton(text=loc.get_message("buttons.counter_picks")),
+                    KeyboardButton(text=loc.get_message("buttons.builds"))
                 ],
                 [
-                    KeyboardButton(text=_("buttons.voting")),
-                    KeyboardButton(text=_("buttons.back"))
+                    KeyboardButton(text=loc.get_message("buttons.voting")),
+                    KeyboardButton(text=loc.get_message("buttons.back"))
                 ]
             ],
             resize_keyboard=True
