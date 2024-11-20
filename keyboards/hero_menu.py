29606c67e8710ffb926b41b65f9a11cbddd665b2
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-20 20:10:58 +0200

    Update hero_menu.py

--- keyboards/hero_menu.py
+++ keyboards/hero_menu.py
@@ -1,44 +1,52 @@
-# File: keyboards/hero_menu.py
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-from .base import BaseKeyboard
+# keyboards/hero_menu.py
 
-class HeroMenu(BaseKeyboard):
-    """Меню вибору героїв"""
-    
-    _heroes = {
-        "tank": ["Tigreal", "Franco", "Minotaur"],
-        "fighter": ["Alucard", "Zilong", "Balmond"],
-        "assassin": ["Saber", "Fanny", "Karina"],
-        "mage": ["Eudora", "Gord", "Aurora"],
-        "marksman": ["Layla", "Miya", "Bruno"],
-        "support": ["Rafaela", "Estes", "Angela"]
-    }
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from utils.localization import loc
 
-    @classmethod
-    def get_class_keyboard(cls):
-        """Повертає клавіатуру з класами героїв"""
-        buttons = [
-            "Tank", "Fighter",
-            "Assassin", "Mage",
-            "Marksman", "Support"
-        ]
-        return cls.create_keyboard(
-            buttons=buttons,
-            row_width=2,
-            resize_keyboard=True
-        )
+class HeroMenu:
+    def get_hero_class_menu(self):
+        keyboard = InlineKeyboardMarkup(row_width=2)
+        hero_classes = loc.get_message("heroes.classes")
+        for class_key, class_info in hero_classes.items():
+            class_name = class_info["name"]
+            button = InlineKeyboardButton(
+                text=class_name,
+                callback_data=f'hero_class_{class_key}'
+            )
+            keyboard.insert(button)
+        keyboard.add(InlineKeyboardButton(
+            text=loc.get_message("buttons.back_to_navigation"),
+            callback_data='back_to_navigation'
+        ))
+        return keyboard
 
-    @classmethod
-    def get_heroes_keyboard(cls, hero_class: str):
-        """Повертає клавіатуру з героями вибраного класу"""
-        if hero_class.lower() not in cls._heroes:
-            return None
-        
-        buttons = cls._heroes[hero_class.lower()]
-        buttons.append("↩️ Назад до класів")
-        
-        return cls.create_keyboard(
-            buttons=buttons,
-            row_width=2,
-            resize_keyboard=True
-        )
+    def get_heroes_by_class(self, class_key):
+        keyboard = InlineKeyboardMarkup(row_width=2)
+        class_info = loc.get_message(f"heroes.classes.{class_key}")
+        heroes = class_info.get("heroes", [])
+        for hero_name in heroes:
+            button = InlineKeyboardButton(
+                text=hero_name,
+                callback_data=f'hero_select_{hero_name}'
+            )
+            keyboard.insert(button)
+        keyboard.add(InlineKeyboardButton(
+            text=loc.get_message("buttons.back_to_classes"),
+            callback_data='back_to_hero_classes'
+        ))
+        return keyboard
+
+    def get_hero_details_menu(self, hero_id):
+        keyboard = InlineKeyboardMarkup()
+        keyboard.add(InlineKeyboardButton(
+            text=loc.get_message("buttons.back_to_hero_list"),
+            callback_data='back_to_hero_list'
+        ))
+        return keyboard
+
+    def get_class_from_hero(self, hero_id):
+        hero_classes = loc.get_message("heroes.classes")
+        for class_key, class_info in hero_classes.items():
+            if hero_id in class_info.get("heroes", []):
+                return class_key
+        return None
