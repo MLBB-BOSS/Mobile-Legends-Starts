663e6fbd21f8cc9647f046649001c2056d8c4e07
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-20 22:12:56 +0200

    Update hero_menu.py

--- keyboards/hero_menu.py
+++ keyboards/hero_menu.py
@@ -1,29 +1,52 @@
 # File: keyboards/hero_menu.py
 
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from utils.localization import loc
 
 class HeroMenu:
-    @staticmethod
-    def get_hero_class_menu() -> ReplyKeyboardMarkup:
-        keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    def get_hero_class_menu(self):
+        keyboard = []
         hero_classes = loc.get_message("heroes.classes")
-        for class_info in hero_classes.values():
-            keyboard.add(KeyboardButton(text=class_info["name"]))
-        keyboard.add(KeyboardButton(text=loc.get_message("buttons.back_to_navigation")))
-        return keyboard
+        for class_key, class_info in hero_classes.items():
+            class_name = class_info["name"]
+            button = InlineKeyboardButton(
+                text=class_name,
+                callback_data=f'hero_class_{class_key}'
+            )
+            keyboard.append([button])
+        keyboard.append([
+            InlineKeyboardButton(
+                text=loc.get_message("buttons.back_to_navigation"),
+                callback_data='back_to_navigation'
+            )
+        ])
+        return InlineKeyboardMarkup(inline_keyboard=keyboard)
 
-    @staticmethod
-    def get_heroes_by_class(class_key) -> ReplyKeyboardMarkup:
-        keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    def get_heroes_by_class(self, class_key):
+        keyboard = []
         class_info = loc.get_message(f"heroes.classes.{class_key}")
-        for hero_name in class_info["heroes"]:
-            keyboard.add(KeyboardButton(text=hero_name))
-        keyboard.add(KeyboardButton(text=loc.get_message("buttons.back_to_hero_classes")))
-        return keyboard
+        heroes = class_info.get("heroes", [])
+        for hero_name in heroes:
+            button = InlineKeyboardButton(
+                text=hero_name,
+                callback_data=f'hero_select_{hero_name}'
+            )
+            keyboard.append([button])
+        keyboard.append([
+            InlineKeyboardButton(
+                text=loc.get_message("buttons.back_to_classes"),
+                callback_data='back_to_hero_classes'
+            )
+        ])
+        return InlineKeyboardMarkup(inline_keyboard=keyboard)
 
-    @staticmethod
-    def get_hero_details_menu() -> ReplyKeyboardMarkup:
-        keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-        keyboard.add(KeyboardButton(text=loc.get_message("buttons.back_to_hero_classes")))
-        return keyboard
+    def get_hero_details_menu(self, hero_id):
+        keyboard = [
+            [
+                InlineKeyboardButton(
+                    text=loc.get_message("buttons.back_to_hero_list"),
+                    callback_data='back_to_hero_list'
+                )
+            ]
+        ]
+        return InlineKeyboardMarkup(inline_keyboard=keyboard)
