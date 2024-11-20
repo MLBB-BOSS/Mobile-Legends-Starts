e6a6e1d1e1fbc8b0a766783325e7a371c828d34d
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-20 22:33:13 +0200

    Update profile_menu.py

--- keyboards/profile_menu.py
+++ keyboards/profile_menu.py
@@ -1,17 +1,20 @@
-# File: keyboards/profile_menu.py
-
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-from utils.localization import loc
+from utils.localization import loc  # Змінений імпорт
 
 class ProfileMenu:
-    @staticmethod
-    def get_profile_menu() -> ReplyKeyboardMarkup:
-        keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-        keyboard.row(
-            KeyboardButton(text=loc.get_message("buttons.statistics")),
-            KeyboardButton(text=loc.get_message("buttons.achievements"))
-        )
-        keyboard.add(
-            KeyboardButton(text=loc.get_message("buttons.back"))
+    def get_profile_menu(self) -> ReplyKeyboardMarkup:
+        keyboard = ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(text=loc.get_message("buttons.statistics")),
+                    KeyboardButton(text=loc.get_message("buttons.achievements"))
+                ],
+                [
+                    KeyboardButton(text=loc.get_message("buttons.settings")),
+                    KeyboardButton(text=loc.get_message("buttons.feedback"))
+                ],
+                [KeyboardButton(text=loc.get_message("buttons.back"))]
+            ],
+            resize_keyboard=True
         )
         return keyboard
