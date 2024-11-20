eb3d53b80dadf8aa4a667da36f25924775a4e900
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-20 20:27:07 +0200

    Update hero_menu.py

--- keyboards/hero_menu.py
+++ keyboards/hero_menu.py
@@ -1,11 +1,9 @@
-# keyboards/hero_menu.py
-
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from utils.localization import loc
 
 class HeroMenu:
     def get_hero_class_menu(self):
-        keyboard = InlineKeyboardMarkup(row_width=2)
+        keyboard = []
         hero_classes = loc.get_message("heroes.classes")
         for class_key, class_info in hero_classes.items():
             class_name = class_info["name"]
@@ -13,15 +11,17 @@ class HeroMenu:
                 text=class_name,
                 callback_data=f'hero_class_{class_key}'
             )
-            keyboard.insert(button)
-        keyboard.add(InlineKeyboardButton(
-            text=loc.get_message("buttons.back_to_navigation"),
-            callback_data='back_to_navigation'
-        ))
-        return keyboard
+            keyboard.append([button])  # Додаємо кожну кнопку в окремий ряд
+        keyboard.append([
+            InlineKeyboardButton(
+                text=loc.get_message("buttons.back_to_navigation"),
+                callback_data='back_to_navigation'
+            )
+        ])
+        return InlineKeyboardMarkup(inline_keyboard=keyboard)
 
     def get_heroes_by_class(self, class_key):
-        keyboard = InlineKeyboardMarkup(row_width=2)
+        keyboard = []
         class_info = loc.get_message(f"heroes.classes.{class_key}")
         heroes = class_info.get("heroes", [])
         for hero_name in heroes:
@@ -29,24 +29,22 @@ class HeroMenu:
                 text=hero_name,
                 callback_data=f'hero_select_{hero_name}'
             )
-            keyboard.insert(button)
-        keyboard.add(InlineKeyboardButton(
-            text=loc.get_message("buttons.back_to_classes"),
-            callback_data='back_to_hero_classes'
-        ))
-        return keyboard
+            keyboard.append([button])  # Додаємо кожну кнопку в окремий ряд
+        keyboard.append([
+            InlineKeyboardButton(
+                text=loc.get_message("buttons.back_to_classes"),
+                callback_data='back_to_hero_classes'
+            )
+        ])
+        return InlineKeyboardMarkup(inline_keyboard=keyboard)
 
     def get_hero_details_menu(self, hero_id):
-        keyboard = InlineKeyboardMarkup()
-        keyboard.add(InlineKeyboardButton(
-            text=loc.get_message("buttons.back_to_hero_list"),
-            callback_data='back_to_hero_list'
-        ))
-        return keyboard
-
-    def get_class_from_hero(self, hero_id):
-        hero_classes = loc.get_message("heroes.classes")
-        for class_key, class_info in hero_classes.items():
-            if hero_id in class_info.get("heroes", []):
-                return class_key
-        return None
+        keyboard = [
+            [
+                InlineKeyboardButton(
+                    text=loc.get_message("buttons.back_to_hero_classes"),
+                    callback_data='back_to_hero_classes'
+                )
+            ]
+        ]
+        return InlineKeyboardMarkup(inline_keyboard=keyboard)
