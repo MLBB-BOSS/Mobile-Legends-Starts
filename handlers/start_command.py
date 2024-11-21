d24f1f770fe73c887a41b1178ea7271da9e9ef96
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-22 00:34:10 +0200

    Update start_command.py

--- handlers/start_command.py
+++ handlers/start_command.py
@@ -6,9 +6,7 @@ router = Router()
 
 @router.message(commands=["start"])
 async def start_command(message: Message):
-    """
-    Обробник команди /start
-    """
+    """Обробник для команди /start"""
     await message.answer(
         text="Ласкаво просимо до бота! Оберіть дію:",
         reply_markup=MainMenuKeyboard.get_keyboard()
