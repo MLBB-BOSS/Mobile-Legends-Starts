d3a5e0080a753d41c6a88c94c981e8687618edcb
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-21 15:39:24 +0200

    Update base.py

--- keyboards/base.py
+++ keyboards/base.py
@@ -1,36 +1,16 @@
-# File: keyboards/base.py
+# File: keyboards/main_menu.py
 
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-from typing import List
-from utils.localization import loc
+from .base import BaseKeyboard
+from aiogram.types import ReplyKeyboardMarkup
 
-class BaseKeyboard:
-    @staticmethod
-    def create_keyboard(
-        button_keys: List[str], 
-        row_width: int = 2, 
-        add_back: bool = True,
-        back_key: str = "buttons.back"
-    ) -> ReplyKeyboardMarkup:
-        """Create keyboard with localized buttons"""
-        keyboard = []
-        row = []
-        
-        for key in button_keys:
-            button_text = loc.get_message(key)
-            row.append(KeyboardButton(text=button_text))
-            if len(row) == row_width:
-                keyboard.append(row)
-                row = []
-        
-        if row:
-            keyboard.append(row)
-            
-        if add_back:
-            back_text = loc.get_message(back_key)
-            keyboard.append([KeyboardButton(text=back_text)])
-            
-        return ReplyKeyboardMarkup(
-            keyboard=keyboard,
-            resize_keyboard=True
-        )
+class MainMenu(BaseKeyboard):
+    @classmethod
+    def get_keyboard(cls) -> ReplyKeyboardMarkup:
+        """Main menu keyboard"""
+        buttons = [
+            "buttons.navigation",
+            "buttons.profile",
+            "buttons.settings",
+            "buttons.help"
+        ]
+        return cls.create_keyboard(buttons, row_width=2, add_back=False)
