fa5569968321e98f54ce3ed51ab4597120395344
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-21 18:34:16 +0200

    Update navigation_keyboard.py

--- keyboards/navigation_keyboard.py
+++ keyboards/navigation_keyboard.py
@@ -1,20 +1,20 @@
 # keyboards/navigation_keyboard.py
 from aiogram.types import ReplyKeyboardMarkup
 from keyboards.base_keyboard import BaseKeyboard
-from keyboards.keyboard_buttons import KeyboardButtons as kb
+from keyboards.keyboard_buttons import Buttons
 
 class NavigationKeyboard(BaseKeyboard):
     def get_main_menu(self) -> ReplyKeyboardMarkup:
         buttons = [
-            [kb.NAVIGATION, kb.HEROES],
-            [kb.PROFILE, kb.SETTINGS]
+            [Buttons.NAVIGATION, Buttons.HEROES],
+            [Buttons.PROFILE, Buttons.SETTINGS]
         ]
         return self.create_keyboard(buttons)
 
     def get_navigation_menu(self) -> ReplyKeyboardMarkup:
         buttons = [
-            [kb.CHARACTERS, kb.MAPS],
-            [kb.TOURNAMENTS, kb.GUIDES],
-            [kb.MAIN_MENU]
+            [Buttons.CHARACTERS, Buttons.MAPS],
+            [Buttons.TOURNAMENTS, Buttons.GUIDES],
+            [Buttons.MAIN_MENU]
         ]
         return self.create_keyboard(buttons)
