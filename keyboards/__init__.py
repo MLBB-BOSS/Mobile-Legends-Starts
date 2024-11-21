85c390ba86fc0c5389d2e40f095c250d9b97c7d0
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-22 00:00:24 +0200

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,14 +1,17 @@
-# keyboards/__init__.py
+from .start_command import router as start_router
+from .menu_handlers import router as menu_router
+from .message_handlers import router as message_router
+from .error_handler import router as error_router
+from .hero_class_handlers import router as hero_class_router
+from .hero_handlers import router as hero_router
+from .navigation_handlers import router as navigation_router
 
-# Імпортуємо необхідні класи з відповідних модулів
-from .keyboard_buttons import Buttons, MenuLevel
-from .base_keyboard import BaseKeyboard
-from .menu_keyboard import MenuKeyboard
-
-# Визначаємо, які імена будуть доступні при імпорті з пакету keyboards
 __all__ = [
-    'BaseKeyboard',
-    'MenuKeyboard',
-    'Buttons',
-    'MenuLevel'
+    "start_router",
+    "menu_router",
+    "message_router",
+    "error_router",
+    "hero_class_router",
+    "hero_router",
+    "navigation_router"
 ]
