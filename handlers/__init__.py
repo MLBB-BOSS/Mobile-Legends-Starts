184b6b46be8ae59567289eac748d8e77a520ebbc
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-21 18:40:00 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,16 +1,5 @@
-# File: handlers/__init__.py
+from .base_keyboard import BaseKeyboard
+from .keyboard_buttons import Buttons
+from .navigation_keyboard import NavigationKeyboard
 
-import logging
-
-logger = logging.getLogger(__name__)
-logger.debug("Початок обробки імпортів у handlers/__init__.py")
-
-try:
-    from .start_command import router as start_router
-    from .menu_handlers import router as menu_router
-    
-    __all__ = ['start_router', 'menu_router']
-    
-except ImportError as e:
-    logger.error(f"Помилка імпорту роутерів у handlers/__init__.py: {e}")
-    raise
+__all__ = ['BaseKeyboard', 'Buttons', 'NavigationKeyboard']
