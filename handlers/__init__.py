a0b3aa922ceec758d0cdfc7a8e576d0d220e192f
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-21 13:05:33 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,5 +1,3 @@
-# handlers/__init__.py
-
 import logging
 
 logger = logging.getLogger(__name__)
@@ -9,6 +7,10 @@ try:
     from .start_command import router as start_router
     from .menu_handlers import router as menu_router
     from .message_handlers import router as message_router
+    from .error_handler import router as error_router
+    from .hero_class_handlers import router as hero_class_router
+    from .hero_handlers import router as hero_router
+    from .navigation_handlers import router as navigation_router
     logger.debug("Імпорти роутерів успішно виконано у handlers/__init__.py")
 except ImportError as e:
     logger.exception(f"Помилка імпорту роутерів у handlers/__init__.py: {e}")
