15792b0d36806b00e65a065188ad5700a67a57cd
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-21 15:31:52 +0200

    Update navigation_menu.py

--- keyboards/navigation_menu.py
+++ keyboards/navigation_menu.py
@@ -5,27 +5,17 @@ from aiogram.types import ReplyKeyboardMarkup
 
 class NavigationKeyboard(BaseKeyboard):
     @classmethod
-    def main_menu(cls) -> ReplyKeyboardMarkup:
-        """Main navigation menu"""
+    def get_keyboard(cls) -> ReplyKeyboardMarkup:
+        """Navigation menu keyboard"""
         buttons = [
-            "buttons.navigation.main.team",
-            "buttons.navigation.main.tournaments",
-            "buttons.navigation.main.rating",
-            "buttons.navigation.main.screenshots",
-            "buttons.navigation.main.achievements",
-            "buttons.navigation.main.notes"
+            "buttons.guides",
+            "buttons.characters",
+            "buttons.counter_picks",
+            "buttons.builds",
+            "buttons.voting"
         ]
-        return cls.create_keyboard(buttons, row_width=3, add_back=False)
-
-    @classmethod
-    def heroes_menu(cls) -> ReplyKeyboardMarkup:
-        """Heroes selection menu"""
-        buttons = [
-            "buttons.navigation.heroes.tank",
-            "buttons.navigation.heroes.fighter", 
-            "buttons.navigation.heroes.assassin",
-            "buttons.navigation.heroes.mage",
-            "buttons.navigation.heroes.marksman",
-            "buttons.navigation.heroes.support"
-        ]
-        return cls.create_keyboard(buttons, row_width=3)
+        return cls.create_keyboard(
+            buttons, 
+            row_width=3,
+            back_key="buttons.back"
+        )
