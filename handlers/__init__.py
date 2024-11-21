07420ef60ef8172dafd9e9aca51eb76fd04d951d
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-21 04:06:30 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,8 +2,8 @@
 
 from .start_command import router as start_router
 from .hero_commands import router as hero_router
+from .navigation_handlers import router as navigation_router
 from .message_handlers import router as message_router
-from .menu_handlers import router as menu_router
 from .error_handler import router as error_router
 
-__all__ = ['start_router', 'hero_router', 'message_router', 'menu_router', 'error_router']
+__all__ = ['start_router', 'hero_router', 'navigation_router', 'message_router', 'error_router']
