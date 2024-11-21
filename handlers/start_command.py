04b8f331a8efe037f31faa581d5d9a8b43dd8444
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-21 07:59:06 +0200

    Update start_command.py

--- handlers/start_command.py
+++ handlers/start_command.py
@@ -3,22 +3,22 @@
 from aiogram import Router, F
 from aiogram.types import Message
 from keyboards.main_menu import MainMenu
-from utils.localization_instance import loc
+from utils.localization import loc
 import logging
 
 logger = logging.getLogger(__name__)
 router = Router()
 
-@router.message(commands=['start'])
+@router.message(F.text.startswith('/start'))
 async def cmd_start(message: Message):
-    logger.info(f"Користувач {message.from_user.id} запустив команду /start")
+    logger.info(f"Користувач {message.from_user.id} запустив бота")
     try:
         await message.answer(
             loc.get_message("messages.start_command"),
             reply_markup=MainMenu().get_main_menu()
         )
     except Exception as e:
-        logger.exception(f"Помилка при виконанні команди /start: {e}")
+        logger.exception(f"Помилка в команді /start: {e}")
         await message.answer(
             loc.get_message("errors.general")
         )
