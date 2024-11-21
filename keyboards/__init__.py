e1b15a15834a833810bc7553e30acdb9449be5f3
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-21 18:43:13 +0200

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,5 +1,13 @@
 # keyboards/__init__.py
+
+# Імпортуємо необхідні класи з відповідних модулів
 from .base_keyboard import BaseKeyboard
+from .keyboard_buttons import Buttons
 from .navigation_keyboard import NavigationKeyboard
 
-__all__ = ['BaseKeyboard', 'NavigationKeyboard']
+# Визначаємо, які імена будуть доступні при імпорті з пакету keyboards
+__all__ = [
+    'BaseKeyboard',
+    'Buttons',
+    'NavigationKeyboard'
+]
