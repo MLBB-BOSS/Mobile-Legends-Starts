ac85439536535eecbb5f0a1ef11f8041327c7279
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-21 02:29:39 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,4 +1,9 @@
-from .start_command import router as start_router
-# Імпортуйте інші роутери, якщо вони є
+# File: handlers/__init__.py
 
-__all__ = ['start_router']  # Додайте інші роутери, якщо потрібно
+from .start_command import router as start_router
+from .hero_commands import router as hero_router
+from .message_handlers import router as message_router
+from .menu_handlers import router as menu_router
+from .error_handler import router as error_router
+
+__all__ = ['start_router', 'hero_router', 'message_router', 'menu_router', 'error_router']
