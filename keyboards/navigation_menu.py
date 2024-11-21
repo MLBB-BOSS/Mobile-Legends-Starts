2db93c43a8ca000507ad78eb8cb60b5f5bb1bcb6
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-21 05:49:13 +0200

    Update navigation_menu.py

--- keyboards/navigation_menu.py
+++ keyboards/navigation_menu.py
@@ -1,45 +1,12 @@
-# File: keyboards/navigation_menu.py
-
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-from utils.localization_instance import loc  # Updated import
-import logging
-
-logger = logging.getLogger(__name__)
-
 class NavigationMenu:
-    def get_main_navigation(self) -> ReplyKeyboardMarkup:
-        keyboard = ReplyKeyboardMarkup(
-            keyboard=[
-                [
-                    KeyboardButton(text=loc.get_message("buttons.guides")),
-                    KeyboardButton(text=loc.get_message("buttons.characters"))
-                ],
-                [
-                    KeyboardButton(text=loc.get_message("buttons.counter_picks")),
-                    KeyboardButton(text=loc.get_message("buttons.builds"))
-                ],
-                [
-                    KeyboardButton(text=loc.get_message("buttons.back"))
-                ]
-            ],
-            resize_keyboard=True
-        )
-        return keyboard
+    # Existing methods...
 
-    def get_heroes_menu(self) -> ReplyKeyboardMarkup:
+    def get_builds_menu(self) -> ReplyKeyboardMarkup:
         keyboard = ReplyKeyboardMarkup(
             keyboard=[
                 [
-                    KeyboardButton(text=loc.get_message("buttons.tanks")),
-                    KeyboardButton(text=loc.get_message("buttons.fighters"))
-                ],
-                [
-                    KeyboardButton(text=loc.get_message("buttons.assassins")),
-                    KeyboardButton(text=loc.get_message("buttons.mages"))
-                ],
-                [
-                    KeyboardButton(text=loc.get_message("buttons.marksmen")),
-                    KeyboardButton(text=loc.get_message("buttons.supports"))
+                    KeyboardButton(text=loc.get_message("buttons.build1")),
+                    KeyboardButton(text=loc.get_message("buttons.build2"))
                 ],
                 [
                     KeyboardButton(text=loc.get_message("buttons.back_to_navigation"))
