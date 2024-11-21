4f6307d5bb4b162c39cb271703e3577b15745a23
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-21 02:26:56 +0200

    Update keyboard_utils.py

--- keyboards/keyboard_utils.py
+++ keyboards/keyboard_utils.py
@@ -1,11 +1,12 @@
 # File: keyboards/keyboard_utils.py
+
 from aiogram.types import InlineKeyboardButton
 from typing import List, Union, Dict
 
 def create_keyboard_row(*buttons: Union[Dict, InlineKeyboardButton]) -> List[InlineKeyboardButton]:
     """
     Створює ряд кнопок з словників або готових кнопок
-    
+
     :param buttons: Кнопки у вигляді словників або об'єктів InlineKeyboardButton
     :return: Список кнопок InlineKeyboardButton
     """
