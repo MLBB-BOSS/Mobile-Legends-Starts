3ad2f5345d403f0f0aa4e87e11b00b7ab29b4a5a
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-21 19:01:31 +0200

    Update menu_keyboard.py

--- keyboards/menu_keyboard.py
+++ keyboards/menu_keyboard.py
@@ -1,4 +1,3 @@
-# keyboards/menu_keyboard.py
 from .base_keyboard import BaseKeyboard
 from .keyboard_buttons import Buttons, MenuLevel
 
@@ -6,45 +5,53 @@ class MenuKeyboard(BaseKeyboard):
     def __init__(self):
         super().__init__()
         self._current_level = MenuLevel.MAIN
-        
+
     def get_main_menu(self):
         self._current_level = MenuLevel.MAIN
         buttons = [
-            [Buttons.NAVIGATION, Buttons.HEROES],
-            [Buttons.TOURNAMENTS, Buttons.PROFILE],
-            [Buttons.SETTINGS]
+            [Buttons.NAVIGATION.value, Buttons.HEROES.value],
+            [Buttons.TOURNAMENTS.value, Buttons.PROFILE.value],
+            [Buttons.SETTINGS.value]
         ]
         return self.create_keyboard(buttons, add_back=False, add_main=False)
-        
+
     def get_navigation_menu(self):
         self._current_level = MenuLevel.NAVIGATION
         buttons = [
-            [Buttons.CHARACTERS, Buttons.MAPS],
-            [Buttons.GUIDES]
+            [Buttons.CHARACTERS.value, Buttons.MAPS.value],
+            [Buttons.GUIDES.value]
         ]
         return self.create_keyboard(buttons)
-        
+
     def get_heroes_menu(self):
         self._current_level = MenuLevel.HEROES
         buttons = [
-            [Buttons.TANK, Buttons.FIGHTER],
-            [Buttons.ASSASSIN, Buttons.MAGE],
-            [Buttons.MARKSMAN, Buttons.SUPPORT]
+            [Buttons.TANK.value, Buttons.FIGHTER.value],
+            [Buttons.ASSASSIN.value, Buttons.MAGE.value],
+            [Buttons.MARKSMAN.value, Buttons.SUPPORT.value]
         ]
         return self.create_keyboard(buttons)
-        
+
     def get_tournaments_menu(self):
         self._current_level = MenuLevel.TOURNAMENTS
         buttons = [
-            [Buttons.ACTIVE, Buttons.UPCOMING],
-            [Buttons.PAST, Buttons.CREATE]
+            [Buttons.ACTIVE.value, Buttons.UPCOMING.value],
+            [Buttons.PAST.value, Buttons.CREATE.value]
         ]
         return self.create_keyboard(buttons)
-        
+
     def get_profile_menu(self):
         self._current_level = MenuLevel.PROFILE
         buttons = [
-            [Buttons.STATS, Buttons.ACHIEVEMENTS],
-            [Buttons.INVENTORY]
+            [Buttons.STATS.value, Buttons.ACHIEVEMENTS.value],
+            [Buttons.INVENTORY.value]
+        ]
+        return self.create_keyboard(buttons)
+
+    def get_settings_menu(self):
+        self._current_level = MenuLevel.SETTINGS
+        buttons = [
+            ["Мова", "Сповіщення"],
+            ["Тема", "Приватність"]
         ]
         return self.create_keyboard(buttons)
