baffbe5049cbddc550cbcfe0fa9ded785e23aab2
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-21 18:12:39 +0200

    Update base_keyboard.py

--- keyboards/base_keyboard.py
+++ keyboards/base_keyboard.py
@@ -1,9 +1,5 @@
-from aiogram.types import (
-    ReplyKeyboardMarkup,
-    KeyboardButton,
-    InlineKeyboardMarkup,
-    InlineKeyboardButton
-)
+# keyboards/base_keyboard.py
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 class BaseKeyboard:
     """Base class for all keyboards"""
@@ -14,14 +10,6 @@ class BaseKeyboard:
         resize_keyboard: bool = True,
         one_time_keyboard: bool = False
     ) -> ReplyKeyboardMarkup:
-        """
-        Creates a reply keyboard markup from a list of button texts
-        
-        Args:
-            keyboard: List of lists of button texts
-            resize_keyboard: Whether to resize the keyboard
-            one_time_keyboard: Whether to hide keyboard after first use
-        """
         markup = ReplyKeyboardMarkup(
             resize_keyboard=resize_keyboard,
             one_time_keyboard=one_time_keyboard
@@ -29,39 +17,5 @@ class BaseKeyboard:
         
         for row in keyboard:
             markup.row(*[KeyboardButton(text=button) for button in row])
-            
-        return markup
-
-    def create_inline_markup(self, buttons: list[list[tuple[str, str]]]) -> InlineKeyboardMarkup:
-        """
-        Creates an inline keyboard markup from a list of button tuples
-        
-        Args:
-            buttons: List of lists of (text, callback_data) tuples
-        """
-        markup = InlineKeyboardMarkup()
-        
-        for row in buttons:
-            markup.row(*[InlineKeyboardButton(
-                text=button[0],
-                callback_data=button[1]
-            ) for button in row])
-            
-        return markup
-
-    def create_url_markup(self, buttons: list[list[tuple[str, str]]]) -> InlineKeyboardMarkup:
-        """
-        Creates an inline keyboard markup with URL buttons
-        
-        Args:
-            buttons: List of lists of (text, url) tuples
-        """
-        markup = InlineKeyboardMarkup()
-        
-        for row in buttons:
-            markup.row(*[InlineKeyboardButton(
-                text=button[0],
-                url=button[1]
-            ) for button in row])
             
         return markup
