dd805dcfc583431c65644d90080507ecf49f4ba9
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-21 04:06:58 +0200

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,8 +1,8 @@
 # File: keyboards/__init__.py
 
 from .main_menu import MainMenu
-from .hero_menu import HeroMenu
 from .navigation_menu import NavigationMenu
 from .profile_menu import ProfileMenu
+from .hero_menu import HeroMenu
 
-__all__ = ['MainMenu', 'HeroMenu', 'NavigationMenu', 'ProfileMenu']
+__all__ = ['MainMenu', 'NavigationMenu', 'ProfileMenu', 'HeroMenu']
