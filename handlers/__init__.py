27972754e6051291dc7ca85d7f6d0694b7eecbf6
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-21 11:17:03 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -3,4 +3,4 @@
 from .start_command import router as start_router
 from .menu_handlers import router as menu_router
 from .message_handlers import router as message_router
-# Додайте інші роутери за потреби
+# Якщо у вас є інші роутери, імпортуйте їх тут
