158d74c21009df9bed90e619672135221572b32e
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-22 01:40:04 +0200

    Update main_menu.py

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -3,6 +3,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 class MainMenu:
     @staticmethod
     def get_main_menu():
+        """Генерація головної клавіатури"""
         return ReplyKeyboardMarkup(
             keyboard=[
                 [KeyboardButton(text="Навігація"), KeyboardButton(text="Профіль")]
