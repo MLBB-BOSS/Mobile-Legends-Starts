15002c8f2693873f9c8312f4ba7948911b4179ef
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-21 12:44:01 +0200

    Update bot_runner.py

--- core/bot_runner.py
+++ core/bot_runner.py
@@ -5,14 +5,14 @@ from aiogram.fsm.storage.memory import MemoryStorage
 import logging
 import aiogram
 
-# Встановлюємо рівень логування
+# Set logging level
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-# Перевірка версії aiogram
+# Check aiogram version
 logger.info(f"aiogram version: {aiogram.__version__}")
 
-# Імпортуємо роутери
+# Import routers
 from handlers.start_command import router as start_router
 from handlers.menu_handlers import router as menu_router
 from handlers.message_handlers import router as message_router
@@ -20,25 +20,25 @@ from handlers.message_handlers import router as message_router
 API_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 
 if not API_TOKEN:
-    logger.critical("Не встановлено змінну середовища TELEGRAM_BOT_TOKEN")
-    raise ValueError("Не встановлено змінну середовища TELEGRAM_BOT_TOKEN")
+    logger.critical("Environment variable TELEGRAM_BOT_TOKEN is not set")
+    raise ValueError("Environment variable TELEGRAM_BOT_TOKEN is not set")
 
 async def main():
     try:
-        # Створюємо об'єкт бота
+        # Create bot object
         bot = Bot(token=API_TOKEN, parse_mode='HTML')
         storage = MemoryStorage()
         dp = Dispatcher(storage=storage)
 
-        # Реєструємо роутери
+        # Register routers
         dp.include_router(start_router)
         dp.include_router(menu_router)
         dp.include_router(message_router)
 
-        logger.info("Бот стартував.")
+        logger.info("Bot started.")
         await dp.start_polling(bot)
     except Exception as e:
-        logger.exception(f"Помилка під час запуску бота: {e}")
+        logger.exception(f"Error while running the bot: {e}")
     finally:
         await bot.session.close()
 
