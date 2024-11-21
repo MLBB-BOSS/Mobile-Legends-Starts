39847fb3b6d568fe8b185452d1d6d8958453fdc8
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-22 00:11:27 +0200

    Update start_command.py

--- handlers/start_command.py
+++ handlers/start_command.py
@@ -1,16 +1,15 @@
 from aiogram import Router
 from aiogram.types import Message
-from aiogram.utils.markdown import hbold
+from keyboards.main_menu import MainMenuKeyboard
 
 router = Router()
 
-
 @router.message(commands=["start"])
-async def command_start_handler(message: Message) -> None:
-    """Handle /start command."""
-    user = message.from_user
-    text_lines = [
-        f"Hi, {hbold(user.full_name)}!",
-        "Try to send me something...",
-    ]
-    await message.answer("\n".join(text_lines))
+async def start_command(message: Message):
+    """
+    Обробник команди /start
+    """
+    await message.answer(
+        text="Ласкаво просимо до бота! Оберіть дію:",
+        reply_markup=MainMenuKeyboard.get_keyboard()
+    )
