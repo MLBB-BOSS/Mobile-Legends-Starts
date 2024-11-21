e777230b933a94a73cb8932140de23dee065bf7e
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-22 00:33:01 +0200

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,17 +1,3 @@
 from .start_command import router as start_router
-from .menu_handlers import router as menu_router
-from .message_handlers import router as message_router
-from .error_handler import router as error_router
-from .hero_class_handlers import router as hero_class_router
-from .hero_handlers import router as hero_router
-from .navigation_handlers import router as navigation_router
 
-__all__ = [
-    "start_router",
-    "menu_router",
-    "message_router",
-    "error_router",
-    "hero_class_router",
-    "hero_router",
-    "navigation_router"
-]
+__all__ = ["start_router"]
