9002e18be80dbe4f854791054ad0f78126cdac3c
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-21 23:32:53 +0200

    Update base_keyboard.py

--- keyboards/base_keyboard.py
+++ keyboards/base_keyboard.py
@@ -1,35 +1,6 @@
-# keyboards/base_keyboard.py
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-from typing import List
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 class BaseKeyboard:
-    def __init__(self):
-        self._current_level = None
-
-    def create_keyboard(
-        self,
-        buttons: List[List[str]],
-        add_back: bool = True,
-        add_main: bool = True
-    ) -> ReplyKeyboardMarkup:
-        keyboard = []
-        
-        # Додаємо основні кнопки
-        for row in buttons:
-            keyboard.append([KeyboardButton(text=str(btn)) for btn in row])
-        
-        # Додаємо навігаційні кнопки
-        nav_row = []
-        if add_back and self._current_level != MenuLevel.MAIN:
-            nav_row.append(KeyboardButton(text=Buttons.BACK))
-        if add_main and self._current_level != MenuLevel.MAIN:
-            nav_row.append(KeyboardButton(text=Buttons.MAIN_MENU))
-        
-        if nav_row:
-            keyboard.append(nav_row)
-            
-        return ReplyKeyboardMarkup(
-            keyboard=keyboard,
-            resize_keyboard=True,
-            one_time_keyboard=False
-        )
+    @staticmethod
+    def create_inline_keyboard(buttons: list[list[InlineKeyboardButton]]) -> InlineKeyboardMarkup:
+        return InlineKeyboardMarkup(inline_keyboard=buttons)
