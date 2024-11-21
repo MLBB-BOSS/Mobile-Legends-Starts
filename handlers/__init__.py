829e15c51af443738d822eb3350927f3d131a381
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-21 19:06:26 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,22 +1,18 @@
-import logging
-
-logger = logging.getLogger(__name__)
-logger.debug("Початок обробки імпортів у handlers/__init__.py")
-
+# handlers/__init__.py
 from .start_command import router as start_router
 from .menu_handlers import router as menu_router
-from .navigation_handlers import router as navigation_router
-from .hero_handlers import router as hero_router
-from .hero_class_handlers import router as hero_class_router
 from .message_handlers import router as message_router
 from .error_handler import router as error_router
+from .hero_class_handlers import router as hero_class_router
+from .hero_handlers import router as hero_router
+from .navigation_handlers import router as navigation_router
 
 __all__ = [
     'start_router',
     'menu_router',
-    'navigation_router',
-    'hero_router',
-    'hero_class_handlers',
     'message_router',
-    'error_router'
+    'error_router',
+    'hero_class_router',
+    'hero_router',
+    'navigation_router'
 ]
