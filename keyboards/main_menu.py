42fc497e3fb878748aef05429f3d2692303570d3
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-22 01:54:47 +0200

    Update main_menu.py

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -1,4 +1,15 @@
+# File: keyboards/main_menu.py
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+class MainMenu:
+    @staticmethod
+    def get_main_menu():
+        return ReplyKeyboardMarkup(
+            keyboard=[
+                [KeyboardButton(text="Навігація"), KeyboardButton(text="Профіль")]
+            ],
+            resize_keyboard=True
+        )from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 class MainMenu:
     @staticmethod
