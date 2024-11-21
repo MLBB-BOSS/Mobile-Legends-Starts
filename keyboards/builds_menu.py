325378b771765a7a2d1c21b64ce359bd1d4fd3cc
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-21 22:09:47 +0200

    Update builds_menu.py

--- keyboards/builds_menu.py
+++ keyboards/builds_menu.py
@@ -8,6 +8,6 @@ class BuildsMenuKeyboard(BaseKeyboard):
             [InlineKeyboardButton(text="⚙️ Створити Білд", callback_data="create_build")],
             [InlineKeyboardButton(text="📜 Мої Білди", callback_data="my_builds")],
             [InlineKeyboardButton(text="🔥 Популярні Білди", callback_data="popular_builds")],
-            [InlineKeyboardButton(text="🔄 Назад", callback_data="back_to_navigation")],
+            [InlineKeyboardButton(text="🔄 Назад", callback_data="back_to_main")],
         ]
         return BaseKeyboard.create_inline_keyboard(buttons)
