f785427e8b5ed990ee81f8686bc7f1048704f5e7
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-21 06:46:03 +0200

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,5 @@
 # File: utils/__init__.py
-
 from .localization import LocalizationManager
-from .localization_instance import loc
+from .localization_setup import loc
 
 __all__ = ['LocalizationManager', 'loc']
