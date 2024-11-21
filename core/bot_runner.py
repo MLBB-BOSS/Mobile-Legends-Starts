1091b6e63bfa1d67fa080df4f87f11cce8fe6645
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-21 10:56:42 +0200

    Update bot_runner.py

--- core/bot_runner.py
+++ core/bot_runner.py
@@ -12,7 +12,6 @@ from handlers.start_command import router as start_router
 from handlers.menu_handlers import router as menu_router
 from handlers.message_handlers import router as message_router
 
-# Зчитуємо токен бота з змінної середовища
 API_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 
 if not API_TOKEN:
@@ -22,7 +21,6 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 async def main():
-    # Використовуємо DefaultBotProperties для встановлення parse_mode
     default_properties = DefaultBotProperties(parse_mode='HTML')
     bot = Bot(token=API_TOKEN, default=default_properties)
     storage = MemoryStorage()
