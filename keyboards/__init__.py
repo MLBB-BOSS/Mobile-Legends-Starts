b3a6147be88d86d06423303fd60437aa17570083
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-22 23:29:10 +0200

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,8 +1,6 @@
+# Імпорти доступних меню
 from .achievements_menu import get_achievements_menu
 from .builds_menu import get_builds_menu
-# keyboards/counter_picks_menu.py
-def get_counter_picks_menu():
-    return None  # Тимчасова заглушка
 from .hero_menu import get_hero_menu
 from .main_menu import get_main_menu
 from .navigation_menu import get_navigation_menu
@@ -11,11 +9,16 @@ from .settings_menu import get_settings_menu
 from .statistics_menu import get_statistics_menu
 from .voting_menu import get_voting_menu
 
+# Заглушка для функцій, які ще не реалізовані
+def get_counter_picks_menu():
+    return None  # Тимчасова заглушка
+
+# Визначаємо, які функції можна імпортувати за допомогою `from keyboards import *`
 __all__ = [
+    "get_achievements_menu",
     "get_builds_menu",
     "get_hero_menu",
     "get_main_menu",
-    "get_map_menu",
     "get_navigation_menu",
     "get_profile_menu",
     "get_settings_menu",
