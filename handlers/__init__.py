372b753770359b5b044efd8203b99ccd5423f2a6
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-23 00:36:19 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,17 +1,15 @@
+# handlers/__init__.py
+
 from .router import router
-from .start_command import start_command
-from .menu_handlers import menu_handlers
-from .navigation_handlers import navigation_handlers
-from .profile_handlers import profile_handlers
-from .settings_handlers import settings_handlers
-from .statistics_handlers import statistics_handlers
+from .start_command import start_router
+from .menu_handlers import menu_router
+from .navigation_handlers import navigation_router
+from .profile_handlers import profile_router
 
 __all__ = [
     "router",
-    "start_command",
-    "menu_handlers",
-    "navigation_handlers",
-    "profile_handlers",
-    "settings_handlers",
-    "statistics_handlers",
+    "start_router",
+    "menu_router",
+    "navigation_router",
+    "profile_router",
 ]
