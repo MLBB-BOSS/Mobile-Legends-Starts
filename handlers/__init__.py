d9b3f68318cdebbf5f82c964479dabfd0cbeb87b
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-22 07:54:56 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,9 +1,5 @@
-# handlers/__init__.py
-
 from .start_command import router as start_router
 from .navigation_handlers import router as navigation_router
+from .profile_handlers import router as profile_router
 
-__all__ = [
-    "start_router",
-    "navigation_router"
-]
+__all__ = ['start_router', 'navigation_router', 'profile_router']
