73485d6f6ed51863b0f7999c31ae7667cdb1882a
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-22 12:21:15 +0200

    Update start_command.py

--- handlers/start_command.py
+++ handlers/start_command.py
@@ -1,11 +1,10 @@
 # handlers/start_command.py
 
+import logging
 from aiogram import Router, F
 from aiogram.types import Message
-from keyboards.start_command import StartMenu
-import logging
+from keyboards.start_command import StartMenu  # Імпортуємо StartMenu замість router
 
-# Ініціалізація логування
 logger = logging.getLogger(__name__)
 
 # Створення екземпляра Router
@@ -13,6 +12,5 @@ router = Router()
 
 @router.message(F.text == "/start")
 async def handle_start_command(message: Message):
-    logger.info("Отримано команду /start")
-    keyboard = StartMenu.get_start_menu()
-    await message.answer("Вітаємо! Оберіть опцію:", reply_markup=keyboard)
+    start_menu = StartMenu.get_start_menu()
+    await message.answer("Вітаю! Це стартова команда.", reply_markup=start_menu)
