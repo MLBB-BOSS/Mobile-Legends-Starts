b22c8ed97cb1b17a171cbdfb7aedf153e54bbe5b
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-22 22:53:03 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,26 +1,17 @@
-from .help_menu import get_help_menu
-from .hero_menu import get_hero_menu
-from .main_menu import get_main_menu
-from .map_menu import get_map_menu
-from .navigation_menu import get_navigation_menu
-from .profile_menu import get_profile_menu
-from .settings_menu import get_settings_menu
-from .statistics_menu import get_statistics_menu
-from .voting_menu import get_voting_menu
+from .router import router
+from .start_command import start_command
+from .menu_handlers import menu_handlers
+from .navigation_handlers import navigation_handlers
+from .profile_handlers import profile_handlers
+from .settings_handlers import settings_handlers
+from .statistics_handlers import statistics_handlers
 
 __all__ = [
-    "BaseKeyboard",
-    "get_builds_menu",
-    "get_characters_menu",
-    "get_counter_picks_menu",  # Додайте функцію до __all__
-    "get_game_modes_menu",
-    "get_help_menu",
-    "get_hero_menu",
-    "get_main_menu",
-    "get_map_menu",
-    "get_navigation_menu",
-    "get_profile_menu",
-    "get_settings_menu",
-    "get_statistics_menu",
-    "get_voting_menu",
+    "router",
+    "start_command",
+    "menu_handlers",
+    "navigation_handlers",
+    "profile_handlers",
+    "settings_handlers",
+    "statistics_handlers",
 ]
