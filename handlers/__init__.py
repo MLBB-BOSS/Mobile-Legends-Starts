eafbe55cc7768380db67a90cbc73487a51a6acae
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-22 11:01:43 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -4,11 +4,11 @@ from .navigation_handlers import router as navigation_router
 from .profile_handlers import router as profile_router
 from .characters_handlers import router as characters_router
 from .statistics_handlers import router as statistics_router
-from .achievements_handlers import achievements_router  # Зверніть увагу на ім'я
+from .achievements_handlers import router as achievements_router
 from .settings_handlers import router as settings_router
 from .feedback_handlers import router as feedback_router
 from .help_handlers import router as help_router
-from .heroes_handlers import router as heroes_router
+from .heroes_handlers import heroes_router  # Переконайтеся, що ім'я відповідає
 from .guides_handlers import router as guides_router
 from .counter_picks_handlers import router as counter_picks_router
 from .builds_handlers import router as builds_router
@@ -23,11 +23,11 @@ __all__ = [
     'profile_router',
     'characters_router',
     'statistics_router',
-    'achievements_router',  # Переконайтеся, що ім'я відповідає
+    'achievements_router',
     'settings_router',
     'feedback_router',
     'help_router',
-    'heroes_router',
+    'heroes_router',  # Переконайтеся, що ім'я відповідає
     'guides_router',
     'counter_picks_router',
     'builds_router',
