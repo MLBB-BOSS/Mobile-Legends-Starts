0d72c19c18d1bdd251d949ce8c32d0bd331dbc4d
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-22 05:39:26 +0200

    Update profile_handlers.py

--- handlers/profile_handlers.py
+++ handlers/profile_handlers.py
@@ -1,20 +1,13 @@
+# handlers/profile_handlers.py
 from aiogram import Router, F
 from aiogram.types import Message
 from keyboards.profile_menu import ProfileMenu
-from keyboards.statistics_menu import StatisticsMenu
 
 router = Router()
 
-@router.message(F.text == "📊 Статистика")
-async def handle_statistics(message: Message):
+@router.message(F.text == "🪪 Мій профіль")
+async def handle_my_profile(message: Message):
     await message.reply(
-        "Меню 'Статистика'. Оберіть опцію:",
-        reply_markup=StatisticsMenu.get_statistics_menu()
-    )
-
-@router.message(F.text == "🔄 Назад")
-async def handle_back_to_profile(message: Message):
-    await message.reply(
-        "Повернення до меню профілю. Оберіть опцію:",
+        "Це меню вашого профілю. Оберіть опцію:",
         reply_markup=ProfileMenu.get_profile_menu()
     )
