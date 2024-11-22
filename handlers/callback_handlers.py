7f57988a2d916de344011f2fcecc5098b0a865fd
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-22 23:36:39 +0200

    Delete handlers/callback_handlers.py

--- handlers/callback_handlers.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Router, F, types
-from aiogram.types import CallbackQuery
-
-router = Router()
-
-@router.callback_query(F.data == "example_callback")
-async def example_callback_handler(callback: CallbackQuery):
-    await callback.message.answer("Оброблено!")
-    await callback.answer()
