146abe7a2e5193ce56404299e8d32bfc6e41aec9
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-23 00:59:08 +0200

    Update menus.py

--- keyboards/menus.py
+++ keyboards/menus.py
@@ -3,6 +3,9 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 def get_main_menu():
+    """
+    Повертає головну клавіатуру бота з кнопками.
+    """
     return ReplyKeyboardMarkup(
         keyboard=[
             [KeyboardButton(text="Навігація"), KeyboardButton(text="Мій профіль")]
