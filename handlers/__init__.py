38b455d80efe8d0d08d269a8ab6299821705c221
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-22 13:10:58 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,18 +1,38 @@
-# handlers/start_command.py
+# handlers/__init__.py
+from .start_command import router as start_router
+from .navigation_handlers import router as navigation_router
+from .profile_handlers import router as profile_router
+from .characters_handlers import router as characters_router
+from .statistics_handlers import router as statistics_router
+from .achievements_handlers import router as achievements_router
+from .settings_handlers import router as settings_router
+from .feedback_handlers import router as feedback_router
+from .help_handlers import router as help_router
+from .heroes_handlers import router as heroes_router
+from .guides_handlers import router as guides_router
+from .counter_picks_handlers import router as counter_picks_router
+from .builds_handlers import router as builds_router
+from .voting_handlers import router as voting_router
+from .map_handlers import router as map_router
+from .game_modes_handlers import router as game_modes_router
+# Додайте інші хендлери тут
 
-from aiogram import Router, F
-from aiogram.types import Message
-from keyboards.start_command import StartMenu
-import logging
-
-# Ініціалізація логування
-logger = logging.getLogger(__name__)
-
-# Створення екземпляра Router
-router = Router()
-
-@router.message(F.text == "/start")
-async def handle_start_command(message: Message):
-    logger.info("Отримано команду /start")
-    keyboard = StartMenu.get_start_menu()
-    await message.answer("Вітаємо! Оберіть опцію:", reply_markup=keyboard)
+__all__ = [
+    'start_router',
+    'navigation_router',
+    'profile_router',
+    'characters_router',
+    'statistics_router',
+    'achievements_router',
+    'settings_router',
+    'feedback_router',
+    'help_router',
+    'heroes_router',
+    'guides_router',
+    'counter_picks_router',
+    'builds_router',
+    'voting_router',
+    'map_router',
+    'game_modes_router',
+    # Додайте інші хендлери тут
+]
