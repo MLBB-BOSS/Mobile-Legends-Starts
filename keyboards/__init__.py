ca487d4302d64a88e2c9023ca19ea44f93e9a3d9
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-22 13:06:10 +0200

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,1 +1,17 @@
 # keyboards/__init__.py
+
+from .start_command import StartMenu
+from .menus import MainMenu
+from .heroes_menu import HeroesMenu
+from .help_menu import HelpMenu
+from .profile_menu import ProfileMenu
+# Додайте інші клавіатури тут
+
+__all__ = [
+    'StartMenu',
+    'MainMenu',
+    'HeroesMenu',
+    'HelpMenu',
+    'ProfileMenu',
+    # Додайте інші клавіатури тут
+]
