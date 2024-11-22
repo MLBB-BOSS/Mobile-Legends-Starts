a46923c6d313384ddaaf964c4f33df944c25f4d1
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-22 15:38:21 +0200

    Update __init__.py

--- .github/workflows/__init__.py
+++ .github/workflows/__init__.py
@@ -1,1 +1,34 @@
+name: Run Python Script
 
+on:
+  push:
+    branches:
+      - main
+  pull_request:
+    branches:
+      - main
+
+jobs:
+  run-script:
+    runs-on: ubuntu-latest
+    steps:
+      # Клонуйте репозиторій
+      - name: Checkout code
+        uses: actions/checkout@v3
+
+      # Встановіть Python
+      - name: Setup Python
+        uses: actions/setup-python@v4
+        with:
+          python-version: 3.10  # Вкажіть версію Python
+
+      # Встановіть залежності
+      - name: Install dependencies
+        run: |
+          python -m pip install --upgrade pip
+          pip install -r requirements.txt
+
+      # Запустіть скрипт
+      - name: Run the script
+        run: |
+          python core/bot_runner.py
