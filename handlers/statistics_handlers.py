af6f3d05cde5885d03f7087bee216157a76eeba1
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-22 07:41:38 +0200

    Update statistics_handlers.py

--- handlers/statistics_handlers.py
+++ handlers/statistics_handlers.py
@@ -1,1 +1,9 @@
+# handlers/statistics_handlers.py
+from aiogram import Router, F
+from aiogram.types import Message
+from keyboards.statistics_menu import StatisticsMenu
+from keyboards.profile_menu import ProfileMenu
 
+router = Router()
+
+@router.message(F.text == "📊 Загальна Актив
