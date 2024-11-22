0cceb9904d0df8777949089f0ea33a171f7b4b8f
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-23 00:23:10 +0200

    Update menu_handlers.py

--- handlers/menu_handlers.py
+++ handlers/menu_handlers.py
@@ -1,17 +1,16 @@
 # handlers/menu_handlers.py
 
-from aiogram import Router
-from aiogram.filters import Text
+from aiogram import Router, F
 from aiogram.types import Message
 from keyboards.menus import get_main_menu
 from keyboards.navigation_menu import get_navigation_menu
 
 menu_router = Router()
 
-@menu_router.message(Text("Головне меню"))
+@menu_router.message(F.text == "Головне меню")
 async def show_main_menu(message: Message):
     await message.answer("Оберіть розділ:", reply_markup=get_main_menu())
 
-@menu_router.message(Text("Навігація"))
+@menu_router.message(F.text == "Навігація")
 async def show_navigation_menu(message: Message):
     await message.answer("Оберіть категорію:", reply_markup=get_navigation_menu())
