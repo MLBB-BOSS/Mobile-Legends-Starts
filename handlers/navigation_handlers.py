b8979d7d0517fcad81c828e8a4c36d32a964b67c
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-22 05:23:34 +0200

    Update navigation_handlers.py

--- handlers/navigation_handlers.py
+++ handlers/navigation_handlers.py
@@ -1,22 +1,20 @@
 from aiogram import Router, F
 from aiogram.types import Message
 from keyboards.navigation_menu import NavigationMenu
-from keyboards.main_menu import MainMenu
+from keyboards.characters_menu import CharactersMenu
 
 router = Router()
 
-@router.message(F.text == "🧭 Навігація")
-async def handle_navigation(message: Message):
-    """Обробка кнопки 'Навігація'."""
+@router.message(F.text == "🛡️ Персонажі")
+async def handle_characters(message: Message):
     await message.reply(
-        "Це розділ навігації. Оберіть опцію:",
-        reply_markup=NavigationMenu.get_navigation_menu()
+        "Це меню 'Персонажі'. Оберіть категорію:",
+        reply_markup=CharactersMenu.get_characters_menu()
     )
 
-@router.message(F.text == "🔙 Назад")
+@router.message(F.text == "🔄 Назад")
 async def handle_back_to_main_menu(message: Message):
-    """Обробка кнопки 'Назад' для повернення до головного меню."""
     await message.reply(
         "Повернення до головного меню. Оберіть дію:",
-        reply_markup=MainMenu.get_main_menu()
+        reply_markup=NavigationMenu.get_navigation_menu()
     )
