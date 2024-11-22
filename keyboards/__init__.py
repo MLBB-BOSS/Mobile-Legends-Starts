4e872834dc002ad4c3d3a7d893e52cd8fcfb0fcc
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-22 13:28:58 +0200

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,17 +1,41 @@
 # keyboards/__init__.py
 
+from .achievements_menu import get_achievements_menu
+from .base_keyboard import BaseKeyboard
+from .builds_menu import get_builds_menu
+from .characters_menu import get_characters_menu
+from .counter_picks_menu import get_counter_picks_menu
+from .game_modes_menu import get_game_modes_menu
+from .help_menu import get_help_menu
+from .hero_menu import get_hero_menu
+from .inline_keyboard import InlineKeyboard
+from .keyboard_buttons import KeyboardButtons
+from .main_menu import get_main_menu
+from .map_menu import get_map_menu
+from .navigation_menu import get_navigation_menu
+from .profile_menu import get_profile_menu
+from .settings_menu import get_settings_menu
 from .start_command import StartMenu
-from .menus import MainMenu
-from .heroes_menu import HeroesMenu
-from .help_menu import HelpMenu
-from .profile_menu import ProfileMenu
-# Додайте інші клавіатури тут
+from .statistics_menu import get_statistics_menu
+from .voting_menu import get_voting_menu
 
 __all__ = [
-    'StartMenu',
-    'MainMenu',
-    'HeroesMenu',
-    'HelpMenu',
-    'ProfileMenu',
-    # Додайте інші клавіатури тут
+    "get_achievements_menu",
+    "BaseKeyboard",
+    "get_builds_menu",
+    "get_characters_menu",
+    "get_counter_picks_menu",
+    "get_game_modes_menu",
+    "get_help_menu",
+    "get_hero_menu",
+    "InlineKeyboard",
+    "KeyboardButtons",
+    "get_main_menu",
+    "get_map_menu",
+    "get_navigation_menu",
+    "get_profile_menu",
+    "get_settings_menu",
+    "StartMenu",
+    "get_statistics_menu",
+    "get_voting_menu",
 ]
