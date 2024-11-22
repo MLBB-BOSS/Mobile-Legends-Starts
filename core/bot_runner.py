dd8b199b0c2fd0eb3327312bbdd84a8c776a9837
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-22 10:48:40 +0200

    Update bot_runner.py

--- core/bot_runner.py
+++ core/bot_runner.py
@@ -7,7 +7,24 @@ from aiogram.types import BotCommand
 from aiogram.client.session.aiohttp import AiohttpSession
 from aiogram.client.bot import DefaultBotProperties
 
-from handlers import start_router, navigation_router, profile_router, characters_router, statistics_router
+from handlers import (
+    start_router,
+    navigation_router,
+    profile_router,
+    characters_router,
+    statistics_router,
+    achievements_router,
+    settings_router,
+    feedback_router,
+    help_router,
+    heroes_router,
+    guides_router,
+    counter_picks_router,
+    builds_router,
+    voting_router,
+    map_router,
+    game_modes_router
+)
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -41,6 +58,17 @@ async def main():
     dp.include_router(profile_router)
     dp.include_router(characters_router)
     dp.include_router(statistics_router)
+    dp.include_router(achievements_router)
+    dp.include_router(settings_router)
+    dp.include_router(feedback_router)
+    dp.include_router(help_router)
+    dp.include_router(heroes_router)
+    dp.include_router(guides_router)
+    dp.include_router(counter_picks_router)
+    dp.include_router(builds_router)
+    dp.include_router(voting_router)
+    dp.include_router(map_router)
+    dp.include_router(game_modes_router)
     # Додайте інші роутери тут
 
     dp.startup.register(on_startup)
