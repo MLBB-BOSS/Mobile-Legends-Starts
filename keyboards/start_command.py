9d7085db65bd23f0e9f855f96e50f722aff5461c
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-22 12:20:44 +0200

    Update start_command.py

--- keyboards/start_command.py
+++ keyboards/start_command.py
@@ -1,16 +1,1 @@
-# handlers/start_command.py
 
-import logging
-from aiogram import Router, F
-from aiogram.types import Message
-from keyboards.start_command import StartMenu  # Імпортуємо StartMenu замість router
-
-logger = logging.getLogger(__name__)
-
-# Створення екземпляра Router
-router = Router()
-
-@router.message(F.text == "/start")
-async def handle_start_command(message: Message):
-    start_menu = StartMenu.get_start_menu()
-    await message.answer("Вітаю! Це стартова команда.", reply_markup=start_menu)
