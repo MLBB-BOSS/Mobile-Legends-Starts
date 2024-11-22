914ddbcfa598a208e5cafa2b92cef0abfdc8c4c1
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-22 10:53:15 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -4,6 +4,17 @@ from .navigation_handlers import router as navigation_router
 from .profile_handlers import router as profile_router
 from .characters_handlers import router as characters_router
 from .statistics_handlers import router as statistics_router
+from .achievements_handlers import achievements_router  # Зверніть увагу на ім'я
+from .settings_handlers import router as settings_router
+from .feedback_handlers import router as feedback_router
+from .help_handlers import router as help_router
+from .heroes_handlers import router as heroes_router
+from .guides_handlers import router as guides_router
+from .counter_picks_handlers import router as counter_picks_router
+from .builds_handlers import router as builds_router
+from .voting_handlers import router as voting_router
+from .map_handlers import router as map_router
+from .game_modes_handlers import router as game_modes_router
 # Додайте інші хендлери тут
 
 __all__ = [
@@ -12,5 +23,16 @@ __all__ = [
     'profile_router',
     'characters_router',
     'statistics_router',
+    'achievements_router',  # Переконайтеся, що ім'я відповідає
+    'settings_router',
+    'feedback_router',
+    'help_router',
+    'heroes_router',
+    'guides_router',
+    'counter_picks_router',
+    'builds_router',
+    'voting_router',
+    'map_router',
+    'game_modes_router',
     # Додайте інші хендлери тут
 ]
