e3c306235a58f2b787dcc535f18aab6175906944
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-22 14:28:23 +0200

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,41 +1,11 @@
-# keyboards/__init__.py
-
-from .achievements_menu import get_achievements_menu
-from .base_keyboard import BaseKeyboard
-from .builds_menu import get_builds_menu
-from .characters_menu import get_characters_menu
-from .counter_picks_menu import get_counter_picks_menu
-from .game_modes_menu import get_game_modes_menu
-from .help_menu import get_help_menu
-from .hero_menu import get_hero_menu
-from .inline_keyboard import InlineKeyboard
-from .keyboard_buttons import KeyboardButtons
-from .main_menu import get_main_menu
-from .map_menu import get_map_menu
-from .navigation_menu import get_navigation_menu
-from .profile_menu import get_profile_menu
-from .settings_menu import get_settings_menu
-from .start_command import StartMenu
-from .statistics_menu import get_statistics_menu
-from .voting_menu import get_voting_menu
+from .menus import *
+from .base import BaseKeyboard, InlineKeyboard
+from .utils import KeyboardButtons, StartMenu
 
 __all__ = [
-    "get_achievements_menu",
+    *menus.__all__,
     "BaseKeyboard",
-    "get_builds_menu",
-    "get_characters_menu",
-    "get_counter_picks_menu",
-    "get_game_modes_menu",
-    "get_help_menu",
-    "get_hero_menu",
     "InlineKeyboard",
     "KeyboardButtons",
-    "get_main_menu",
-    "get_map_menu",
-    "get_navigation_menu",
-    "get_profile_menu",
-    "get_settings_menu",
     "StartMenu",
-    "get_statistics_menu",
-    "get_voting_menu",
 ]
