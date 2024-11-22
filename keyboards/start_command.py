22592466de11333f1152c6a9d632d0accdb627d7
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-22 14:00:09 +0200

    Update start_command.py

--- keyboards/start_command.py
+++ keyboards/start_command.py
@@ -1,14 +1,22 @@
 # keyboards/start_command.py
+
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 class StartMenu:
-    """Клавіатура для стартового меню"""
+    """Клавіатура для стартової команди."""
+
     @staticmethod
     def get_start_menu() -> ReplyKeyboardMarkup:
+        """
+        Створює стартову клавіатуру з основними опціями.
+        
+        Returns:
+            ReplyKeyboardMarkup: Клавіатура з основними опціями.
+        """
         return ReplyKeyboardMarkup(
             keyboard=[
                 [KeyboardButton(text="🧭 Навігація"), KeyboardButton(text="🪪 Профіль")],
-                [KeyboardButton(text="❓ Допомога")],
+                [KeyboardButton(text="📚 Довідка"), KeyboardButton(text="⚙️ Налаштування")],
             ],
             resize_keyboard=True
         )
