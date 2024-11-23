a550bbd61935646e92e8fca816a71975dc6f0fc5
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-23 02:58:59 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,11 +1,9 @@
-# handlers/__init__.py
-
 from .menu import menu_router
 from .navigation import navigation_router
-from .heroes import hero_router
+from .heroes import heroes_router  # Додати цей імпорт
 
 __all__ = [
     "menu_router",
     "navigation_router",
-    "hero_router",
+    "heroes_router",
 ]
