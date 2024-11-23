fc9d14a0d6240a815bbf963d7088a59936a468bb
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-23 02:42:47 +0200

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -2,7 +2,7 @@
 
 from .main_menu import get_main_menu
 from .navigation_menu import get_navigation_menu
-from .heroes_menu import get_hero_class_menu
+from .heroes_menu import get_hero_class_menu  # Увага на цей імпорт
 
 __all__ = [
     "get_main_menu",
