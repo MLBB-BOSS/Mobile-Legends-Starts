405019cd4e24aad51726f768d64f0f05207eec27
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-23 09:14:35 +0200

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,9 +1,11 @@
 from .main_menu import get_main_menu
 from .navigation_menu import get_navigation_menu
-from .heroes_menu import get_hero_class_menu
+from .hero_menu import get_hero_class_menu
+from .start_command import StartMenu
 
 __all__ = [
     "get_main_menu",
     "get_navigation_menu",
     "get_hero_class_menu",
+    "StartMenu"
 ]
