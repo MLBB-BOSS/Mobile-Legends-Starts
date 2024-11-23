c648a0a585a063d16b77d51054ed5591571097c3
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-23 11:16:38 +0200

    Update hero_menu.py

--- keyboards/hero_menu.py
+++ keyboards/hero_menu.py
@@ -1,5 +1,4 @@
 # keyboards/heroes_menu.py
-
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 def get_hero_class_menu():
@@ -8,8 +7,9 @@ def get_hero_class_menu():
     """
     return ReplyKeyboardMarkup(
         keyboard=[
-            [KeyboardButton(text="🛡️ Танк"), KeyboardButton(text="🔮 Маг"), KeyboardButton(text="🏹 Стрілець")],
-            [KeyboardButton(text="⚔️ Асасін"), KeyboardButton(text="🧬 Підтримка")],
+            [KeyboardButton(text="🛡️ Танк"), KeyboardButton(text="🔮 Маг")],
+            [KeyboardButton(text="🏹 Стрілець"), KeyboardButton(text="⚔️ Асасін")],
+            [KeyboardButton(text="🧬 Підтримка"), KeyboardButton(text="🤺 Боєць")],
             [KeyboardButton(text="🔄 Назад")]
         ],
         resize_keyboard=True
