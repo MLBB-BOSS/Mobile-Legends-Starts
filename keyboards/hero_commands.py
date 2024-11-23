08a0d2543e99ec95b29f971a9f5f614f12096209
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-23 09:50:41 +0200

    Update hero_commands.py

--- keyboards/hero_commands.py
+++ keyboards/hero_commands.py
@@ -1,17 +1,4 @@
-# keyboards/hero_menu.py
-
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-def get_hero_class_menu() -> ReplyKeyboardMarkup:
-    """
-    Повертає клавіатуру для вибору класу героїв.
-    """
-    return ReplyKeyboardMarkup(
-        keyboard=[
-            [KeyboardButton(text="🛡️ Танк"), KeyboardButton(text="🔮 Маг")],
-            [KeyboardButton(text="🏹 Стрілець"), KeyboardButton(text="⚔️ Асасін")],
-            [KeyboardButton(text="🧬 Підтримка"), KeyboardButton(text="🤺 Боєць")],
-            [KeyboardButton(text="🔄 Назад")]
-        ],
-        resize_keyboard=True
-    )
+def get_hero_class_menu(hero_classes: list[str]) -> ReplyKeyboardMarkup:
+    keyboard = [[KeyboardButton(text=hero)] for hero in hero_classes]
+    keyboard.append([KeyboardButton(text="🔄 Назад")])
+    return ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True)
