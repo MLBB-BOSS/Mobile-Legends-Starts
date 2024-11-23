d84417f08d2fd277e93379a7d4659c39e6251471
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-23 10:27:43 +0200

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,11 +1,19 @@
-from .main_menu import get_main_menu
-from .navigation_menu import get_navigation_menu
-from .hero_menu import get_hero_class_menu
-from .start_command import StartMenu
+import logging
+
+logger = logging.getLogger(__name__)
+
+try:
+    from .main_menu import get_main_menu
+    from .navigation_menu import get_navigation_menu
+    from .hero_menu import get_hero_class_menu
+    from .start_command import StartMenu
+except ImportError as e:
+    logger.error(f"Помилка імпорту: {e}")
+    raise
 
 __all__ = [
     "get_main_menu",
     "get_navigation_menu",
     "get_hero_class_menu",
-    "StartMenu"
+    "StartMenu",
 ]
