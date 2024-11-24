96c00d55de9bd9ab8467768a2c3d1411bec1362f
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-24 13:35:30 +0200

    Delete handlers directory

--- handlers/heroes_handlers .py
+++
@@ -1,8 +0,0 @@
-from aiogram import Router
-from aiogram.types import CallbackQuery
-
-router = Router()
-
-@router.callback_query(lambda c: c.data == "menu_navigation")
-async def navigation_menu(callback: CallbackQuery):
-    await callback.message.edit_text("🧭 Ви обрали Навігацію.")
