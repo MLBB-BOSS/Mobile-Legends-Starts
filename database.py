ef5c77131a8645134b882f673ff18db94a3a6ea2
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-24 15:59:24 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import declarative_base
 from config import settings
+from typing import AsyncGenerator
 
 DATABASE_URL = settings.async_database_url
 
@@ -8,6 +9,17 @@ engine = create_async_engine(DATABASE_URL, echo=False)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 Base = declarative_base()
 
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
+
 async def create_db_and_tables():
     async with engine.begin() as conn:
+        # Імпортуємо моделі тут, щоб уникнути циклічних імпортів
+        from models import User  # noqa
+        
+        # Створюємо таблиці
         await conn.run_sync(Base.metadata.create_all)
