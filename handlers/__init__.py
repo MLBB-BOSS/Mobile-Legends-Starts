a04fc211398d92cc1a6821e55bf875757b70b3d1
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-24 22:46:32 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,5 +1,10 @@
-from aiogram import Dispatcher
+# UTC:20:42
+# 2024-11-24
+# handlers/__init__.py
+# Author: MLBB-BOSS
+# Description: Handlers module initialization
+# The era of artificial intelligence.
+
 from .main_menu import router as main_menu_router
 
-def register_handlers(dp: Dispatcher) -> None:
-    dp.include_router(main_menu_router)
+__all__ = ['main_menu_router']
