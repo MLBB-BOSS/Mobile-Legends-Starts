b3a555898273962035c50bb5cfb6af46642c3ee8
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-24 23:36:04 +0200

    Update navigation_menu.py

--- keyboards/navigation_menu.py
+++ keyboards/navigation_menu.py
@@ -1,10 +1,42 @@
+# UTC:21:32
+# 2024-11-24
+# keyboards/navigation_menu.py
+# Author: MLBB-BOSS
+# Description: Navigation menu keyboard layouts
+# The era of artificial intelligence.
+
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-def navigation_menu_keyboard():
+def get_navigation_keyboard() -> ReplyKeyboardMarkup:
+    """Підменю Навігація"""
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(
-        KeyboardButton("Опція 1"),
-        KeyboardButton("Опція 2")
+    keyboard.row(
+        KeyboardButton("🛡️ Персонажі"),
+        KeyboardButton("📚 Гайди")
     )
-    keyboard.add(KeyboardButton("Повернутися до головного меню"))
+    keyboard.row(
+        KeyboardButton("⚖️ Контр-піки"),
+        KeyboardButton("⚜️ Білди")
+    )
+    keyboard.row(
+        KeyboardButton("📊 Голосування"),
+        KeyboardButton("🔙 Назад до Головного")
+    )
+    return keyboard
+
+def get_profile_keyboard() -> ReplyKeyboardMarkup:
+    """Підменю Профіль"""
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.row(
+        KeyboardButton("📈 Статистика"),
+        KeyboardButton("🏆 Досягнення")
+    )
+    keyboard.row(
+        KeyboardButton("⚙️ Налаштування"),
+        KeyboardButton("💌 Зворотний Зв'язок")
+    )
+    keyboard.row(
+        KeyboardButton("❓ Допомога"),
+        KeyboardButton("🔙 Назад до Головного")
+    )
     return keyboard
