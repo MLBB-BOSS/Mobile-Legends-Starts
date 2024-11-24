d05fb22dc235e54459398b09cbb13dd14d9ccd6c
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-24 21:14:07 +0200

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,3 +1,4 @@
-from .main_menu import NavigationKeyboards, ProfileKeyboards
+from .main_menu import main_menu_keyboard
+from .navigation_menu import navigation_menu_keyboard
 
-__all__ = ["NavigationKeyboards", "ProfileKeyboards"]
+__all__ = ["main_menu_keyboard", "navigation_menu_keyboard"]
