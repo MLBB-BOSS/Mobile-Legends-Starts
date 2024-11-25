1a8fbe90b919f580ff0ed6978b8e12a4e9b4c24e
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-25 17:48:59 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,7 +1,7 @@
 # handlers/__init__.py
 from .main_menu import router as main_menu_router
 from .navigation import router as navigation_router
-from .profile import router as profile_handlers_router  # Змініть імпорт на .profile
+from .profile_handlers import router as profile_handlers_router  # Змініть імпорт на .profile_handlers
 
 __all__ = [
     'main_menu_router',
