e7350603cfbb54358eff06e92ac7ccb5fc0f2edc
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-25 17:51:44 +0200

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,16 +1,8 @@
-# UTC:22:06
-# 2024-11-24
 # keyboards/__init__.py
-# Author: MLBB-BOSS
-# Description: Keyboards module initialization
-# The era of artificial intelligence.
-
-from .main_menu import get_main_keyboard as main_menu_keyboard
-from .navigation_menu import get_navigation_keyboard as navigation_keyboard
-from .profile_menu import get_profile_keyboard as profile_keyboard
+from .main_menu import get_main_keyboard
+from .profile_menu import get_profile_keyboard
 
 __all__ = [
-    'main_menu_keyboard',
-    'navigation_keyboard', 
-    'profile_keyboard'
+    'get_main_keyboard',
+    'get_profile_keyboard'
 ]
