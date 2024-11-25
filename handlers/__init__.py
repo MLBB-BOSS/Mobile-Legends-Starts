bbe06fac35e794ff736f9d30c6db40b88dfabf6c
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-25 17:26:49 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,13 +1,7 @@
-# UTC:22:00
-# 2024-11-25
 # handlers/__init__.py
-# Author: MLBB-BOSS
-# Description: Handlers module initialization
-# The era of artificial intelligence.
-
 from .main_menu import router as main_menu_router
 from .navigation import router as navigation_router
-from .profile_handlers import router as profile_handlers_router  # Додано профільні хендлери
+from .profile import router as profile_handlers_router  # Змініть імпорт на .profile
 
 __all__ = [
     'main_menu_router',
