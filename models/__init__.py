a4b05474412c10df78db2007a5d28b7f0722180f
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-25 16:58:41 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,5 @@
-# UTC:22:42
-# 2024-11-24
 # models/__init__.py
-# Author: MLBB-BOSS
-# Description: Models initialization
-# The era of artificial intelligence.
-
 from .base import Base
+from .user import User
 
 __all__ = ['Base', 'User']
