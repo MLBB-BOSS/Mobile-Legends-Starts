fa3c1d1a5972a1e59351d1e318902185a81c2427
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-25 15:34:29 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,5 +1,5 @@
-# UTC:21:50
-# 2024-11-24
+# UTC:22:00
+# 2024-11-25
 # handlers/__init__.py
 # Author: MLBB-BOSS
 # Description: Handlers module initialization
@@ -8,9 +8,11 @@
 from .main_menu import router as main_menu_router
 from .navigation import router as navigation_router
 from .user_handlers import router as user_handlers_router
+from .profile_handlers import router as profile_handlers_router  # Import profile handlers
 
 __all__ = [
     'main_menu_router',
-    'navigation_router', 
-    'user_handlers_router'
+    'navigation_router',
+    'user_handlers_router',
+    'profile_handlers_router'  # Add profile handlers to the exported module
 ]
