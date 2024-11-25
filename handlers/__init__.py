2d8710d0f759baab6586b7dff3e7e9ecf14b231d
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-26 00:02:46 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,5 +1,5 @@
 # handlers/__init__.py
-# UTC:21:56
+# UTC:22:01
 # 2024-11-25
 # Author: MLBB-BOSS
 # Description: Handlers initialization
@@ -7,22 +7,12 @@
 
 from .main_menu import router as main_menu_router
 from .navigation import router as navigation_router
-from .profile import router as profile_router
-from .characters import router as characters_router
-from .guides import router as guides_router
-from .counterpicks import router as counterpicks_router
-from .builds import router as builds_router
-from .voting import router as voting_router
-from .help import router as help_router
+from .profile_menu import router as profile_router  # змінено з profile на profile_menu
+from .characters_menu import router as characters_router  # змінено з characters на characters_menu
 
 __all__ = [
     'main_menu_router',
     'navigation_router',
     'profile_router',
-    'characters_router',
-    'guides_router',
-    'counterpicks_router',
-    'builds_router',
-    'voting_router',
-    'help_router'
+    'characters_router'
 ]
