b8d4060e84432a7db1a925c44a5369537a36ac96
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-26 00:22:35 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,5 +1,5 @@
 # handlers/__init__.py
-# UTC:22:10
+# UTC:22:20
 # 2024-11-25
 # Author: MLBB-BOSS
 # Description: Handlers initialization
@@ -7,13 +7,10 @@
 
 from .main_menu import router as main_menu_router
 from .navigation import router as navigation_router
-# Змінюємо імпорт на існуючий файл
 from .profile_handlers import router as profile_router
-from .characters_menu import router as characters_router
 
 __all__ = [
     'main_menu_router',
     'navigation_router',
-    'profile_router',
-    'characters_router'
+    'profile_router'
 ]
