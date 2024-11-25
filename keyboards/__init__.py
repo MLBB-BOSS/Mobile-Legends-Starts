150761bac280a84c19af728781ecfc8fd1879d45
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-26 01:55:04 +0200

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -9,10 +9,12 @@ from .main_menu import get_main_keyboard
 from .profile_menu import get_profile_keyboard
 from .navigation_menu import get_navigation_keyboard
 from .characters_menu import get_characters_keyboard
+from .mp3_player_menu import get_mp3_player_keyboard  # Додано нову клавіатуру для MP3 плеєра
 
 __all__ = [
     'get_main_keyboard',
     'get_profile_keyboard',
     'get_navigation_keyboard',
-    'get_characters_keyboard'
+    'get_characters_keyboard',
+    'get_mp3_player_keyboard'  # Додано нову клавіатуру для MP3 плеєра
 ]
