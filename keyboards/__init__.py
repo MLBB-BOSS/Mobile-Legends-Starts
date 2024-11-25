f27687fcbbb72617fd2e599ff2c6dad83f8f2399
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-25 22:25:16 +0200

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,10 +1,12 @@
 # keyboards/__init__.py
 from .main_menu import get_main_keyboard
 from .profile_menu import get_profile_keyboard
-from .navigation_menu import get_navigation_keyboard  # Додано, якщо існує
+from .navigation_menu import get_navigation_keyboard
+from .characters_menu import get_characters_keyboard  # Додаємо нову клавіатуру
 
 __all__ = [
     'get_main_keyboard',
     'get_profile_keyboard',
-    'get_navigation_keyboard'  # Додано, якщо існує
+    'get_navigation_keyboard',
+    'get_characters_keyboard'  # Додаємо в експорт
 ]
