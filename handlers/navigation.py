b314688848b2439b6235f390a895788af997d358
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-27 01:34:19 +0200

    Update navigation.py

--- handlers/navigation.py
+++ handlers/navigation.py
@@ -1,12 +1,13 @@
 # /handlers/navigation.py
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.types import CallbackQuery
 from keyboards.level2.navigation_menu import get_navigation_menu
 
 router = Router()
 
-@router.callback_query(lambda c: c.data == "navigation_menu")
+@router.callback_query(F.data == "navigation_menu")
 async def navigation_menu_handler(callback: CallbackQuery):
+    """Обробник для меню Навігація"""
     await callback.message.edit_text(
         "🧭 Навігація: Оберіть потрібний розділ:",
         reply_markup=get_navigation_menu()
