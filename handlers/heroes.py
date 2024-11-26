85983914c647eaf1933eaab3d5736a4846fe213a
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-27 01:34:49 +0200

    Update heroes.py

--- handlers/heroes.py
+++ handlers/heroes.py
@@ -1,13 +1,14 @@
 # /handlers/heroes.py
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.types import CallbackQuery
 from keyboards.level3.heroes_menu import get_heroes_menu
 
 router = Router()
 
-@router.callback_query(lambda c: c.data == "heroes_menu")
+@router.callback_query(F.data == "heroes_menu")
 async def heroes_menu_handler(callback: CallbackQuery):
+    """Обробник для меню Персонажі"""
     await callback.message.edit_text(
-        "🛡️ Персонажі: Оберіть клас:",
+        "🛡️ Персонажі: Оберіть клас персонажів:",
         reply_markup=get_heroes_menu()
     )
