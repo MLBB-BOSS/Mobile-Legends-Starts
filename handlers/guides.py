2c3fbf3e03a712aa2aa3f973bb547fdf269ff690
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-27 01:47:55 +0200

    Update guides.py

--- handlers/guides.py
+++ handlers/guides.py
@@ -7,8 +7,8 @@ router = Router()
 
 @router.callback_query(F.data == "guides_menu")
 async def guides_menu_handler(callback: CallbackQuery):
-    """Обробник для меню Гайди"""
+    """Меню гайдів"""
     await callback.message.edit_text(
-        "📚 Гайди: Оберіть гайди:",
+        "📚 Гайди: Оберіть потрібний гайди:",
         reply_markup=get_guides_menu()
     )
