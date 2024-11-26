ab0d923c377fd035c7d5bceabffffae558935246
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-26 02:01:59 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -8,9 +8,11 @@
 from .main_menu import router as main_menu_router
 from .navigation import router as navigation_router
 from .profile_handlers import router as profile_router
+from .mp3_player import router as mp3_player_router  # Додано новий обробник для MP3 плеєра
 
 __all__ = [
     'main_menu_router',
     'navigation_router',
-    'profile_router'
+    'profile_router',
+    'mp3_player_router'  # Додано новий обробник для MP3 плеєра
 ]
