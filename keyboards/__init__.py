b33cfcddb6b9c598a43e8de0b64e7bb48f5c6301
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-26 03:05:54 +0200

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,11 +1,14 @@
-# keyboards/__init__.py
-
 from .main_menu import (
     get_main_keyboard,
     get_profile_keyboard,
     get_navigation_keyboard,
-    get_characters_keyboard
+    get_guides_keyboard,
+    get_counterpicks_keyboard,
+    get_builds_keyboard,
+    get_voting_keyboard,
+    get_help_keyboard
 )
+from .characters_menu import get_characters_keyboard
 from .mp3_player_menu import get_mp3_player_keyboard
 
 __all__ = [
@@ -13,5 +16,10 @@ __all__ = [
     'get_profile_keyboard',
     'get_navigation_keyboard',
     'get_characters_keyboard',
+    'get_guides_keyboard',
+    'get_counterpicks_keyboard',
+    'get_builds_keyboard',
+    'get_voting_keyboard',
+    'get_help_keyboard',
     'get_mp3_player_keyboard'
 ]
