d587c5baf2d94c4d6346987f9540bc843d8d9e95
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-27 01:46:27 +0200

    Update main_menu.py

--- handlers/main_menu.py
+++ handlers/main_menu.py
@@ -1,17 +1,9 @@
 # /handlers/main_menu.py
 from aiogram import Router, F
-from aiogram.types import CallbackQuery, Message
+from aiogram.types import CallbackQuery
 from keyboards.level1.main_menu import get_main_menu
 
 router = Router()
-
-@router.message(F.text == "/start")
-async def start_handler(message: Message):
-    """Обробник команди /start"""
-    await message.answer(
-        "Привіт! Ласкаво просимо до бота. Оберіть опцію:",
-        reply_markup=get_main_menu()
-    )
 
 @router.callback_query(F.data == "main_menu")
 async def main_menu_handler(callback: CallbackQuery):
