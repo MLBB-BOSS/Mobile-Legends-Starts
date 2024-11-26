2a2da6e26ffca9e6dc3c6507a935309ebe536ab5
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-27 01:07:57 +0200

    Update heroes_menu.py

--- keyboards/level3/heroes_menu.py
+++ keyboards/level3/heroes_menu.py
@@ -1,1 +1,17 @@
+# /keyboards/level3/heroes_menu.py
+#  Персонажі - третій рівень
+
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+def get_heroes_menu():
+    builder = InlineKeyboardBuilder()
+    builder.button(text="🔎 Пошук Персонажа", callback_data="search_hero")
+    builder.button(text="🛡️ Танк", callback_data="tank_heroes")
+    builder.button(text="🔮 Маг", callback_data="mage_heroes")
+    builder.button(text="🏹 Стрілець", callback_data="marksman_heroes")
+    builder.button(text="⚔️ Асасін", callback_data="assassin_heroes")
+    builder.button(text="🧬 Підтримка", callback_data="support_heroes")
+    builder.button(text="🔄 Назад", callback_data="navigation_menu")
+    builder.adjust(2)
+    return builder.as_markup()
 
