878382539a61e2f2b83d741a2498ff7d6b14b248
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-26 02:30:09 +0200

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,20 +1,17 @@
 # keyboards/__init__.py
-# UTC:22:03
-# 2024-11-25
-# Author: MLBB-BOSS
-# Description: Keyboards initialization
-# The era of artificial intelligence.
 
-from .main_menu import get_main_keyboard
-from .profile_menu import get_profile_keyboard
-from .navigation_menu import get_navigation_keyboard
-from .characters_menu import get_characters_keyboard
-from .mp3_player_menu import get_mp3_player_keyboard  # Додано нову клавіатуру для MP3 плеєра
+from .main_menu import (
+    get_main_keyboard,
+    get_profile_keyboard,
+    get_navigation_keyboard,
+    get_characters_keyboard  # Імпортуємо get_characters_keyboard з main_menu.py
+)
+from .mp3_player_menu import get_mp3_player_keyboard
 
 __all__ = [
     'get_main_keyboard',
     'get_profile_keyboard',
     'get_navigation_keyboard',
     'get_characters_keyboard',
-    'get_mp3_player_keyboard'  # Додано нову клавіатуру для MP3 плеєра
+    'get_mp3_player_keyboard'
 ]
