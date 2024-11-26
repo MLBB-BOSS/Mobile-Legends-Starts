042d8b394862dba52cf21c1206804a4ab7c8a152
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-27 01:05:29 +0200

    Update main_menu.py

--- keyboards/level1/main_menu.py
+++ keyboards/level1/main_menu.py
@@ -1,1 +1,12 @@
+# /keyboards/level1/main_menu.py
+# Головне меню - перший рівень
+
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+def get_main_menu():
+    builder = InlineKeyboardBuilder()
+    builder.button(text="🧭 Навігація", callback_data="navigation_menu")
+    builder.button(text="🪪 Профіль", callback_data="profile_menu")
+    builder.adjust(2)  # Дві кнопки в рядку
+    return builder.as_markup()
 
