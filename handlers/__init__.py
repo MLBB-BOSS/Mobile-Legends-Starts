8a2eec47d77bae93cc3d30be4029d462a1db9745
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-27 01:50:20 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,21 +1,16 @@
-# /handlers/__init__.py
 from .start import router as start_router
 from .main_menu import router as main_menu_router
 from .navigation import router as navigation_router
 from .heroes import router as heroes_router
-from .guides import router as guides_router
-from .help import router as help_router
 
 routers = [
     start_router,
     main_menu_router,
     navigation_router,
-    heroes_router,
-    guides_router,
-    help_router
+    heroes_router
 ]
 
 def setup_handlers(dp):
-    """Функція для підключення всіх хендлерів до диспетчера"""
+    """Підключаємо всі хендлери до Dispatcher"""
     for router in routers:
         dp.include_router(router)
