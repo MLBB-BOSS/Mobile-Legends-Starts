2cad79930adf33679af7e23a204c71822cbaa119
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-27 01:06:44 +0200

    Update navigation_menu.py

--- keyboards/level2/navigation_menu.py
+++ keyboards/level2/navigation_menu.py
@@ -1,1 +1,16 @@
+# /keyboards/level2/navigation_menu.py
+# Навігація - другий рівень
+
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+def get_navigation_menu():
+    builder = InlineKeyboardBuilder()
+    builder.button(text="🛡️ Персонажі", callback_data="heroes_menu")
+    builder.button(text="📚 Гайди", callback_data="guides_menu")
+    builder.button(text="⚖️ Контр-піки", callback_data="counter_picks_menu")
+    builder.button(text="⚜️ Білди", callback_data="builds_menu")
+    builder.button(text="📊 Голосування", callback_data="votes_menu")
+    builder.button(text="🔄 Назад", callback_data="main_menu")
+    builder.adjust(2)
+    return builder.as_markup()
 
