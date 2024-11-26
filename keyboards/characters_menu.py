39b113931c823e02b34b8e8749b052dcdf86cb0b
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-26 03:17:14 +0200

    Update characters_menu.py

--- keyboards/characters_menu.py
+++ keyboards/characters_menu.py
@@ -26,3 +26,21 @@ def get_characters_keyboard() -> ReplyKeyboardMarkup:
     )
     
     return keyboard
+    
+    from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+def create_buttons(button_texts):
+    return [KeyboardButton(text=text) for text in button_texts]
+
+def get_characters_keyboard() -> ReplyKeyboardMarkup:
+    buttons = [
+        create_buttons(["🗡️ Бійці", "🏹 Стрільці", "🔮 Маги"]),
+        create_buttons(["🛡️ Танки", "🏥 Саппорти", "🗲 Гібриди"]),
+        create_buttons(["🔥 Метові", "◀️ Назад до Навігації"])
+    ]
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=buttons,
+        resize_keyboard=True,
+        input_field_placeholder="Оберіть тип героя"
+    )
+    return keyboard
