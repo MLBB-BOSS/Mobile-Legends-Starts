112b0fc7eb55306d47d53c9a2a184142d36610ab
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-26 02:34:46 +0200

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -4,7 +4,7 @@ from .main_menu import (
     get_main_keyboard,
     get_profile_keyboard,
     get_navigation_keyboard,
-    get_characters_keyboard  # Імпортуємо get_characters_keyboard з main_menu.py
+    get_characters_keyboard
 )
 from .mp3_player_menu import get_mp3_player_keyboard
 
