57d2dd8031a7ee7d83a09f74c6fc27d402a5a096
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-27 02:09:24 +0200

    Create back.py

---
+++ handlers/back.py
@@ -0,0 +1,13 @@
+from aiogram import Router, F
+from aiogram.types import Message
+from keyboards.level1.main_menu import get_main_menu
+
+router = Router()
+
+@router.message(F.text == "🔄 Назад")
+async def back_to_main_menu(message: Message):
+    """Повернення до головного меню"""
+    await message.answer(
+        "Ви повернулися до головного меню.",
+        reply_markup=get_main_menu()
+    )
