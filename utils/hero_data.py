137b7feaa871ffc3413d4f3e9c0b64d93f5b37b8
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-27 02:44:23 +0200

    Create hero_data.py

---
+++ utils/hero_data.py
@@ -0,0 +1,16 @@
+HERO_CLASSES = {
+    "Fighter": [
+        "Balmond", "Alucard", "Bane", "Zilong", "Freya", "Alpha", "Ruby", "Roger", 
+        "Gatotkaca", "Grock", "Jawhead", "Martis", "Aldous", "Minsitthar", 
+        "Terizla", "X.Borg", "Dyroth", "Masha", "Silvanna", "Yu Zhong", 
+        "Khaleed", "Barats", "Paquito", "Phoveus", "Aulus", "Fiddrin", 
+        "Arlott", "Cici", "Kaja", "Leomord", "Thamuz", "Badang", "Guinevere"
+    ],
+    "Tank": [
+        "Alice", "Tigreal", "Akai", "Franco", "Minotaur", "Lolia", "Gatotkaca", 
+        "Grock", "Hylos", "Uranus", "Belerick", "Khufra", "Esmeralda", "Terizla", 
+        "Baxia", "Masha", "Atlas", "Barats", "Edith", "Fredrinn", "Johnson", 
+        "Hilda", "Carmilla", "Gloo", "Chip"
+    ],
+    # Додайте інші класи
+}
