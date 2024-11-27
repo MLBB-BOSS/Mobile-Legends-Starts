9bda06131a8430ffefa8f265f7bd426503401d56
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-27 02:09:59 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,17 +2,17 @@ from .start import router as start_router
 from .main_menu import router as main_menu_router
 from .navigation import router as navigation_router
 from .heroes import router as heroes_router
-from .guides import router as guides_router
+from .back import router as back_router  # Додаємо новий хендлер
 
 routers = [
     start_router,
     main_menu_router,
     navigation_router,
     heroes_router,
-    guides_router
+    back_router  # Реєструємо хендлер "Назад"
 ]
 
 def setup_handlers(dp):
-    """Реєстрація всіх хендлерів"""
+    """Підключаємо всі хендлери"""
     for router in routers:
         dp.include_router(router)
