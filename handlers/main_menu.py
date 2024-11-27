2c82c1e43b6645365d33d99b1fc3eee27d1281af
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-27 02:02:00 +0200

    Update main_menu.py

--- handlers/main_menu.py
+++ handlers/main_menu.py
@@ -1,4 +1,3 @@
-# /handlers/main_menu.py
 from aiogram import Router, F
 from aiogram.types import CallbackQuery
 from keyboards.level1.main_menu import get_main_menu
@@ -7,7 +6,6 @@ router = Router()
 
 @router.callback_query(F.data == "main_menu")
 async def main_menu_handler(callback: CallbackQuery):
-    """Повернення до головного меню"""
     await callback.message.edit_text(
         "Головне меню. Оберіть опцію:",
         reply_markup=get_main_menu()
