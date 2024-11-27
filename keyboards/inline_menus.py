e99bf0ef5861442feda201b350d755ec174a51e7
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-27 13:45:06 +0200

    Update inline_menus.py

--- keyboards/inline_menus.py
+++ keyboards/inline_menus.py
@@ -5,8 +5,8 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 def get_generic_inline_keyboard():
     inline_keyboard = [
         [
-            InlineKeyboardButton(text="Button 1", callback_data="button1"),
-            InlineKeyboardButton(text="Button 2", callback_data="button2")
+            InlineKeyboardButton(text="Кнопка 1", callback_data="button1"),
+            InlineKeyboardButton(text="Кнопка 2", callback_data="button2")
         ]
     ]
     return InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
