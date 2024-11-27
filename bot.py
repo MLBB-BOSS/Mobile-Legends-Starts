20d921c64ad22037f68ba776067ee03010c0b074
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-27 03:53:51 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,33 +1,20 @@
 # bot.py
+
 import os
-import logging
 import asyncio
 from aiogram import Bot, Dispatcher
+from handlers.menu_handlers import router as menu_router
 
-# Ініціалізація бота та диспетчера
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 if not TOKEN:
     raise ValueError("TELEGRAM_BOT_TOKEN не встановлено!")
+
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-# Імпорт ваших обробників
-from handlers.start import router as start_router
-from handlers.heroes import router as heroes_router
-from handlers.tank import router as tank_router
-from handlers.fighter import router as fighter_router
-from handlers.guides import router as guides_router
-from handlers.back import router as back_router
-# Додайте інші обробники за потребою
-
-# Реєстрація обробників у диспетчері
-dp.include_router(start_router)
-dp.include_router(heroes_router)
-dp.include_router(tank_router)
-dp.include_router(fighter_router)
-dp.include_router(guides_router)
-dp.include_router(back_router)
-# Додайте інші маршрутизатори
+# Реєстрація маршрутизаторів
+dp.include_router(menu_router)
+# Додайте інші маршрутизатори за потребою
 
 async def main():
     await dp.start_polling(bot)
@@ -36,4 +23,4 @@ if __name__ == "__main__":
     try:
         asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
-        logging.info("Бот зупинено.")
+        print("Бот зупинено.")
