c028fd0db0959a3a7682b230d6cc258badb267de
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-27 02:55:07 +0200

    Update guides_menu.py

--- keyboards/level3/guides_menu.py
+++ keyboards/level3/guides_menu.py
@@ -1,15 +1,11 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 def get_guides_menu():
-    """
-    Створює клавіатуру для меню гайдів.
-
-    :return: ReplyKeyboardMarkup
-    """
     return ReplyKeyboardMarkup(
         keyboard=[
-            [KeyboardButton(text="🆕 Нові Гайди"), KeyboardButton(text="🌟 Популярні Гайди"), KeyboardButton(text="📘 Для Початківців")],
-            [KeyboardButton(text="🧙 Просунуті Техніки"), KeyboardButton(text="🛡️ Командна Гра"), KeyboardButton(text="🔄 Назад")],
+            [KeyboardButton(text="🆕 Нові Гайди"), KeyboardButton(text="🌟 Популярні Гайди")],
+            [KeyboardButton(text="📘 Для Початківців"), KeyboardButton(text="🧙 Просунуті Техніки")],
+            [KeyboardButton(text="🔄 Назад")],
         ],
         resize_keyboard=True
     )
