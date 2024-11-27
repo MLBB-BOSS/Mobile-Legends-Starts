5c6a803d89cf1775f8317f98108dfaadca557fea
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-27 02:53:31 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,10 +1,16 @@
 from .navigation import router as navigation_router
 from .heroes import router as heroes_router
+from .guides import router as guides_router
+from .counter_picks import router as counter_picks_router
+from .hero_classes import router as hero_classes_router
 from .back import router as back_router
 
 routers = [
     navigation_router,
     heroes_router,
+    guides_router,
+    counter_picks_router,
+    hero_classes_router,
     back_router,
 ]
 
