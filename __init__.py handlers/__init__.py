fa194a38c872c0049f7a5976638ecb538b48be3b
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-27 02:46:32 +0200

    Create __init__.py

---
+++ __init__.py handlers/__init__.py
@@ -0,0 +1,10 @@
+from .fighter import router as fighter_router
+from .tank import router as tank_router
+
+routers = [
+    navigation_router,
+    heroes_router,
+    back_router,
+    fighter_router,
+    tank_router,
+]
