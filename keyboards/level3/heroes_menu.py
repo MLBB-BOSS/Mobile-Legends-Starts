25d872475c57e7d156dab3542a8a49e759f868bc
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-27 02:06:50 +0200

    Update heroes_menu.py

--- keyboards/level3/heroes_menu.py
+++ keyboards/level3/heroes_menu.py
@@ -1,35 +1,18 @@
-# /keyboards/level3/heroes_menu.py
-from aiogram.utils.keyboard import InlineKeyboardBuilder
-from aiogram.types import InlineKeyboardMarkup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-TEXTS = {
-    "search": "🔎 Пошук Персонажа",
-    "tank": "🛡️ Танк",
-    "mage": "🔮 Маг",
-    "marksman": "🏹 Стрілець",
-    "assassin": "⚔️ Асасін",
-    "support": "🧬 Підтримка",
-    "back": "🔄 Назад"
-}
-
-def get_heroes_menu(row_width: int = 2) -> InlineKeyboardMarkup:
+def get_heroes_menu():
     """
-    Створює меню для персонажів.
+    Створює клавіатуру для меню Персонажів.
 
-    :param row_width: Кількість кнопок у рядку (за замовчуванням 2).
-    :return: InlineKeyboardMarkup
+    :return: ReplyKeyboardMarkup
     """
-    builder = InlineKeyboardBuilder()
-    buttons = [
-        {"text": TEXTS["search"], "callback_data": "search_hero"},
-        {"text": TEXTS["tank"], "callback_data": "tank_heroes"},
-        {"text": TEXTS["mage"], "callback_data": "mage_heroes"},
-        {"text": TEXTS["marksman"], "callback_data": "marksman_heroes"},
-        {"text": TEXTS["assassin"], "callback_data": "assassin_heroes"},
-        {"text": TEXTS["support"], "callback_data": "support_heroes"},
-        {"text": TEXTS["back"], "callback_data": "navigation_menu"}
-    ]
-    for button in buttons:
-        builder.button(text=button["text"], callback_data=button["callback_data"])
-    builder.adjust(row_width)
-    return builder.as_markup()
+    return ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="🔎 Пошук Персонажа")],
+            [KeyboardButton(text="🛡️ Танк"), KeyboardButton(text="🔮 Маг")],
+            [KeyboardButton(text="🏹 Стрілець"), KeyboardButton(text="⚔️ Асасін")],
+            [KeyboardButton(text="🧬 Підтримка")],
+            [KeyboardButton(text="🔄 Назад")]
+        ],
+        resize_keyboard=True
+    )
