effaf1dc5d0ded2c33ed912a656c168f6e214973
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-11-29 16:21:35 +0200

    Update inline_menus.py

--- keyboards/inline_menus.py
+++ keyboards/inline_menus.py
@@ -11,3 +11,22 @@ def get_generic_inline_keyboard() -> InlineKeyboardMarkup:
             InlineKeyboardButton(text="---MLS---", callback_data="mls_button")
         ]
     ])
+
+def get_welcome_keyboard(page: int) -> InlineKeyboardMarkup:
+    if page == 1:
+        buttons = [
+            InlineKeyboardButton(text="Продовжити", callback_data="welcome_continue_1")
+        ]
+    elif page == 2:
+        buttons = [
+            InlineKeyboardButton(text="Продовжити", callback_data="welcome_continue_2")
+        ]
+    elif page == 3:
+        buttons = [
+            InlineKeyboardButton(text="Розпочати", callback_data="welcome_start")
+        ]
+    else:
+        buttons = []
+    
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[buttons])
+    return keyboard
