38b00e1c3d24d38461d80a75bc9fc13fb0d9ae94
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-02 15:47:31 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,10 +1,21 @@
 from .base import router as base_router
+from .callbacks import callbacks_router
+from .heroes import heroes_router
+from .main_menu import menu_router
+from .start_handler import start_router
 
+# Список усіх роутерів
 routers = [
+    start_router,
+    menu_router,
+    heroes_router,
+    callbacks_router,
     base_router,
 ]
 
 def setup_handlers(dp):
-    """Реєстрація всіх хендлерів"""
+    """
+    Реєстрація всіх хендлерів у диспетчері.
+    """
     for router in routers:
         dp.include_router(router)
