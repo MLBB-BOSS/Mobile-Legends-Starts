82207307eba9b2779ab8bcbe5ee375773e348450
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-06 23:15:23 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,10 +1,8 @@
-from .base import router as base_router
+# handlers/__init__.py
 
-routers = [
-    base_router,
-]
+from .base import router as base_router
+from .ai_handler import router as ai_router
 
 def setup_handlers(dp):
-    """Реєстрація всіх хендлерів"""
-    for router in routers:
-        dp.include_router(router)
+    dp.include_router(base_router)
+    dp.include_router(ai_router)
