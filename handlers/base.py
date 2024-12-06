2591efb3fc8d4d70da558766c2e0b1f060a7ac89
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-07 00:49:47 +0200

    Update base.py

--- handlers/base.py
+++ handlers/base.py
@@ -12,21 +12,7 @@ from keyboards.inline_menus import (
     get_generic_inline_keyboard,
     get_intro_page_1_keyboard,
     get_intro_page_2_keyboard,
-    get_intro_page_3_keyboard,
-    get_main_menu,
-    get_navigation_menu,
-    get_profile_menu,
-    get_heroes_menu,
-    get_hero_class_menu,
-    get_guides_menu,
-    get_counter_picks_menu,
-    get_builds_menu,
-    get_voting_menu,
-    get_statistics_menu,
-    get_achievements_menu,
-    get_settings_menu,
-    get_feedback_menu,
-    get_help_menu
+    get_intro_page_3_keyboard
 )
 
 # Імпортуємо тексти
