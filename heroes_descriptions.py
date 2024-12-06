ba024ac8e1af20d2566c830aa11d28d67a29bab9
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-06 17:25:38 +0200

    Create heroes_descriptions.py

---
+++ heroes_descriptions.py
@@ -0,0 +1,231 @@
+# heroes_descriptions.py
+
+# Константи для кожного героя
+HERO_BALMOND = "Balmond"
+HERO_ALUCARD = "Alucard"
+HERO_BANE = "Bane"
+HERO_ZILONG = "Zilong"
+HERO_FREYA = "Freya"
+HERO_ALPHA = "Alpha"
+HERO_RUBY = "Ruby"
+HERO_ROGER = "Roger"
+HERO_GATOTKACA = "Gatotkaca"
+HERO_JAWHEAD = "Jawhead"
+HERO_MARTIS = "Martis"
+HERO_ALDOUS = "Aldous"
+HERO_MINSITTHAR = "Minsitthar"
+HERO_TERIZLA = "Terizla"
+HERO_X_BORG = "X.Borg"
+HERO_DYROTH = "Dyroth"
+HERO_MASHA = "Masha"
+HERO_SILVANNA = "Silvanna"
+HERO_YU_ZHONG = "Yu Zhong"
+HERO_KHALEED = "Khaleed"
+HERO_BARATS = "Barats"
+HERO_PAQUITO = "Paquito"
+HERO_PHOVEUS = "Phoveus"
+HERO_AULUS = "Aulus"
+HERO_FIDDRIN = "Fiddrin"
+HERO_ARLOTT = "Arlott"
+HERO_CICI = "Cici"
+HERO_KAJA = "Kaja"
+HERO_LEOMORD = "Leomord"
+HERO_THAMUZ = "Thamuz"
+HERO_BADANG = "Badang"
+HERO_GUINEVERE = "Guinevere"
+
+HERO_ALICE = "Alice"
+HERO_TIGREAL = "Tigreal"
+HERO_AKAI = "Akai"
+HERO_FRANCO = "Franco"
+HERO_MINOTAUR = "Minotaur"
+HERO_LOLIA = "Lolia"
+HERO_GROCK = "Grock"
+HERO_HYLOS = "Hylos"
+HERO_URANUS = "Uranus"
+HERO_BELERICK = "Belerick"
+HERO_KHUFRA = "Khufra"
+HERO_ESMERALDA = "Esmeralda"
+HERO_BAXIA = "Baxia"
+HERO_ATLAS = "Atlas"
+HERO_EDITH = "Edith"
+HERO_FREDRINN = "Fredrinn"
+HERO_JOHNSON = "Johnson"
+HERO_HILDA = "Hilda"
+HERO_CARMILLA = "Carmilla"
+HERO_GLOO = "Gloo"
+HERO_CHIP = "Chip"
+
+HERO_SABER = "Saber"
+HERO_FANNY = "Fanny"
+HERO_NATALIA = "Natalia"
+HERO_YI_SUN_SHIN = "Yi Sun-shin"
+HERO_LANCELOT = "Lancelot"
+HERO_HELCURT = "Helcurt"
+HERO_LESLEY = "Lesley"
+HERO_SELENA = "Selena"
+HERO_MATHILDA = "Mathilda"
+HERO_YIN = "Yin"
+HERO_HARLEY = "Harley"
+HERO_SUYOU = "Suyou"
+
+HERO_POPOL_AND_KUPA = "Popol and Kupa"
+HERO_BRODY = "Brody"
+HERO_BEATRIX = "Beatrix"
+HERO_NATAN = "Natan"
+HERO_MELISSA = "Melissa"
+HERO_IXIA = "Ixia"
+HERO_HANABI = "Hanabi"
+HERO_CLAUDE = "Claude"
+HERO_KIMMY = "Kimmy"
+HERO_GRANGER = "Granger"
+HERO_WANWAN = "Wanwan"
+HERO_MIYA = "Miya"
+HERO_BRUNO = "Bruno"
+HERO_CLINT = "Clint"
+HERO_LAYLA = "Layla"
+HERO_MOSKOV = "Moskov"
+HERO_KARRIE = "Karrie"
+HERO_IRITHEL = "Irithel"
+
+HERO_VALE = "Vale"
+HERO_LUNOX = "Lunox"
+HERO_KADITA = "Kadita"
+HERO_CECILLION = "Cecillion"
+HERO_LUO_YI = "Luo Yi"
+HERO_XAVIER = "Xavier"
+HERO_NOVARIA = "Novaria"
+HERO_ZHUXIN = "Zhuxin"
+HERO_YVE = "Yve"
+HERO_AURORA = "Aurora"
+HERO_FARAMIS = "Faramis"
+HERO_KAGURA = "Kagura"
+HERO_CYCLOPS = "Cyclops"
+HERO_VEXANA = "Vexana"
+HERO_ODETTE = "Odette"
+HERO_ZHASK = "Zhask"
+
+HERO_RAFAELA = "Rafaela"
+HERO_LOLITA = "Lolita"
+HERO_ESTES = "Estes"
+HERO_ANGELA = "Angela"
+HERO_FLORIN = "Florin"
+
+HERO_META_HERO1 = "MetaHero1"
+HERO_META_HERO2 = "MetaHero2"
+
+# Словник з описами героїв
+HERO_DESCRIPTIONS = {
+    HERO_BALMOND: "Опис героя Balmond...",
+    HERO_ALUCARD: "Опис героя Alucard...",
+    HERO_BANE: "Опис героя Bane...",
+    HERO_ZILONG: "Опис героя Zilong...",
+    HERO_FREYA: "Опис героя Freya...",
+    HERO_ALPHA: "Опис героя Alpha...",
+    HERO_RUBY: "Опис героя Ruby...",
+    HERO_ROGER: "Опис героя Roger...",
+    HERO_GATOTKACA: "Опис героя Gatotkaca...",
+    HERO_JAWHEAD: "Опис героя Jawhead...",
+    HERO_MARTIS: "Опис героя Martis...",
+    HERO_ALDOUS: "Опис героя Aldous...",
+    HERO_MINSITTHAR: "Опис героя Minsitthar...",
+    HERO_TERIZLA: "Опис героя Terizla...",
+    HERO_X_BORG: "Опис героя X.Borg...",
+    HERO_DYROTH: "Опис героя Dyroth...",
+    HERO_MASHA: "Опис героя Masha...",
+    HERO_SILVANNA: "Опис героя Silvanna...",
+    HERO_YU_ZHONG: "Опис героя Yu Zhong...",
+    HERO_KHALEED: "Опис героя Khaleed...",
+    HERO_BARATS: "Опис героя Barats...",
+    HERO_PAQUITO: "Опис героя Paquito...",
+    HERO_PHOVEUS: "Опис героя Phoveus...",
+    HERO_AULUS: "Опис героя Aulus...",
+    HERO_FIDDRIN: "Опис героя Fiddrin...",
+    HERO_ARLOTT: "Опис героя Arlott...",
+    HERO_CICI: "Опис героя Cici...",
+    HERO_KAJA: "Опис героя Kaja...",
+    HERO_LEOMORD: "Опис героя Leomord...",
+    HERO_THAMUZ: "Опис героя Thamuz...",
+    HERO_BADANG: "Опис героя Badang...",
+    HERO_GUINEVERE: "Опис героя Guinevere...",
+
+    HERO_ALICE: "Опис героя Alice...",
+    HERO_TIGREAL: "Опис героя Tigreal...",
+    HERO_AKAI: "Опис героя Akai...",
+    HERO_FRANCO: "Опис героя Franco...",
+    HERO_MINOTAUR: "Опис героя Minotaur...",
+    HERO_LOLIA: "Опис героя Lolia...",
+    HERO_GROCK: "Опис героя Grock...",
+    HERO_HYLOS: "Опис героя Hylos...",
+    HERO_URANUS: "Опис героя Uranus...",
+    HERO_BELERICK: "Опис героя Belerick...",
+    HERO_KHUFRA: "Опис героя Khufra...",
+    HERO_ESMERALDA: "Опис героя Esmeralda...",
+    HERO_BAXIA: "Опис героя Baxia...",
+    HERO_ATLAS: "Опис героя Atlas...",
+    HERO_EDITH: "Опис героя Edith...",
+    HERO_FREDRINN: "Опис героя Fredrinn...",
+    HERO_JOHNSON: "Опис героя Johnson...",
+    HERO_HILDA: "Опис героя Hilda...",
+    HERO_CARMILLA: "Опис героя Carmilla...",
+    HERO_GLOO: "Опис героя Gloo...",
+    HERO_CHIP: "Опис героя Chip...",
+
+    HERO_SABER: "Опис героя Saber...",
+    HERO_FANNY: "Опис героя Fanny...",
+    HERO_NATALIA: "Опис героя Natalia...",
+    HERO_YI_SUN_SHIN: "Опис героя Yi Sun-shin...",
+    HERO_LANCELOT: "Опис героя Lancelot...",
+    HERO_HELCURT: "Опис героя Helcurt...",
+    HERO_LESLEY: "Опис героя Lesley...",
+    HERO_SELENA: "Опис героя Selena...",
+    HERO_MATHILDA: "Опис героя Mathilda...",
+    HERO_YIN: "Опис героя Yin...",
+    HERO_HARLEY: "Опис героя Harley...",
+    HERO_SUYOU: "Опис героя Suyou...",
+
+    HERO_POPOL_AND_KUPA: "Опис героя Popol and Kupa...",
+    HERO_BRODY: "Опис героя Brody...",
+    HERO_BEATRIX: "Опис героя Beatrix...",
+    HERO_NATAN: "Опис героя Natan...",
+    HERO_MELISSA: "Опис героя Melissa...",
+    HERO_IXIA: "Опис героя Ixia...",
+    HERO_HANABI: "Опис героя Hanabi...",
+    HERO_CLAUDE: "Опис героя Claude...",
+    HERO_KIMMY: "Опис героя Kimmy...",
+    HERO_GRANGER: "Опис героя Granger...",
+    HERO_WANWAN: "Опис героя Wanwan...",
+    HERO_MIYA: "Опис героя Miya...",
+    HERO_BRUNO: "Опис героя Bruno...",
+    HERO_CLINT: "Опис героя Clint...",
+    HERO_LAYLA: "Опис героя Layla...",
+    HERO_MOSKOV: "Опис героя Moskov...",
+    HERO_KARRIE: "Опис героя Karrie...",
+    HERO_IRITHEL: "Опис героя Irithel...",
+
+    HERO_VALE: "Опис героя Vale...",
+    HERO_LUNOX: "Опис героя Lunox...",
+    HERO_KADITA: "Опис героя Kadita...",
+    HERO_CECILLION: "Опис героя Cecillion...",
+    HERO_LUO_YI: "Опис героя Luo Yi...",
+    HERO_XAVIER: "Опис героя Xavier...",
+    HERO_NOVARIA: "Опис героя Novaria...",
+    HERO_ZHUXIN: "Опис героя Zhuxin...",
+    HERO_YVE: "Опис героя Yve...",
+    HERO_AURORA: "Опис героя Aurora...",
+    HERO_FARAMIS: "Опис героя Faramis...",
+    HERO_KAGURA: "Опис героя Kagura...",
+    HERO_CYCLOPS: "Опис героя Cyclops...",
+    HERO_VEXANA: "Опис героя Vexana...",
+    HERO_ODETTE: "Опис героя Odette...",
+    HERO_ZHASK: "Опис героя Zhask...",
+
+    HERO_RAFAELA: "Опис героя Rafaela...",
+    HERO_LOLITA: "Опис героя Lolita...",
+    HERO_ESTES: "Опис героя Estes...",
+    HERO_ANGELA: "Опис героя Angela...",
+    HERO_FLORIN: "Опис героя Florin...",
+
+    HERO_META_HERO1: "Опис метового героя MetaHero1...",
+    HERO_META_HERO2: "Опис метового героя MetaHero2...",
+}
