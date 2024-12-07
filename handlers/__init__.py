683697635ca229d15daff48715183ce72cf15116
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-07 15:17:58 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,8 +1,8 @@
 # handlers/__init__.py
 
 from .base import router as base_router
-from .ai_handler import router as ai_router
+# from .ai_handler import router as ai_router  # Видалено або закоментовано
 
 def setup_handlers(dp):
     dp.include_router(base_router)
-    dp.include_router(ai_router)
+    # dp.include_router(ai_router)  # Видалено або закоментовано
