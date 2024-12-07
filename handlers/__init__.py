9f61b81edc4498a16f964824246cda37889efcd2
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-08 01:23:48 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,10 +1,10 @@
 # handlers/__init__.py
 
-from .visual_handler import router as visual_router
+from .start_handler import router as start_router
 # Імпортуйте інші роутери тут, наприклад:
-# from .openai_handler import router as openai_router
+# from .visual_handler import router as visual_router
 
 def setup_handlers(dispatcher):
-    dispatcher.include_router(visual_router)
-    # Додайте інші роутери тут, наприклад:
-    # dispatcher.include_router(openai_router)
+    dispatcher.include_router(start_router)
+    # dispatcher.include_router(visual_router)
+    # Додайте інші роутери тут
