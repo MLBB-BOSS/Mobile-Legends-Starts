9a1f21dacfe71b455c157b2b75761f79771ff577
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-07 12:30:24 +0200

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,1 +1,25 @@
 # keyboards/__init__.py
+
+from .keyboards import (
+    get_main_menu,
+    get_generic_inline_keyboard,
+    get_navigation_menu,
+    get_profile_menu,
+    get_meta_menu,
+    get_m6_menu,
+    get_gpt_menu,
+    get_heroes_menu,
+    get_hero_class_menu,
+    get_guides_menu,
+    get_counter_picks_menu,
+    get_builds_menu,
+    get_voting_menu,
+    get_statistics_menu,
+    get_achievements_menu,
+    get_settings_menu,
+    get_feedback_menu,
+    get_help_menu,
+    get_intro_page_1_keyboard,
+    get_intro_page_2_keyboard,
+    get_intro_page_3_keyboard
+)# keyboards/__init__.py
