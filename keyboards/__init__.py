a8e2fe94af43c4deb1ff3420ca416d37bb971b39
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-07 12:51:14 +0200

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,25 +1,14 @@
-# keyboards/__init__.py
-
-from .keyboards import (
-    get_main_menu,
-    get_generic_inline_keyboard,
-    get_navigation_menu,
-    get_profile_menu,
-    get_meta_menu,
-    get_m6_menu,
-    get_gpt_menu,
-    get_heroes_menu,
-    get_hero_class_menu,
-    get_guides_menu,
-    get_counter_picks_menu,
-    get_builds_menu,
-    get_voting_menu,
-    get_statistics_menu,
-    get_achievements_menu,
-    get_settings_menu,
-    get_feedback_menu,
-    get_help_menu,
-    get_intro_page_1_keyboard,
-    get_intro_page_2_keyboard,
-    get_intro_page_3_keyboard
-)# keyboards/__init__.py
+mlbb.boss.ua@gmail.com
+2024-12-07T10:50:09.855395+00:00 heroku[worker.1]: State changed from crashed to starting
+2024-12-07T10:50:16.974163+00:00 heroku[worker.1]: Starting process with command `python bot.py`
+2024-12-07T10:50:17.561289+00:00 heroku[worker.1]: State changed from starting to up
+2024-12-07T10:50:20.282729+00:00 app[worker.1]: Traceback (most recent call last):
+2024-12-07T10:50:20.282754+00:00 app[worker.1]:   File "/app/bot.py", line 12, in <module>
+2024-12-07T10:50:20.282786+00:00 app[worker.1]:     from handlers import setup_handlers
+2024-12-07T10:50:20.282803+00:00 app[worker.1]:   File "/app/handlers/__init__.py", line 3, in <module>
+2024-12-07T10:50:20.282834+00:00 app[worker.1]:     from .base import setup_handlers
+2024-12-07T10:50:20.282846+00:00 app[worker.1]:   File "/app/handlers/base.py", line 34, in <module>
+2024-12-07T10:50:20.282898+00:00 app[worker.1]:     from texts import (
+2024-12-07T10:50:20.282907+00:00 app[worker.1]: ImportError: cannot import name 'PROFILE_MENU_TEXT' from 'texts' (/app/texts.py)
+2024-12-07T10:50:20.562334+00:00 heroku[worker.1]: Process exited with status 1
+2024-12-07T10:50:20.580295+00:00 heroku[worker.1]: State changed from up to crashed
