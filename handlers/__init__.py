f759d2844e93bc0fe65ba6deadf82d6d09564c0b
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-08 02:42:02 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,6 +1,6 @@
 from .start_handler import router as start_router
-from .base import router as base_router  # Імпортуємо базовий роутер
+from .base import router as base_router
 
 def setup_handlers(dispatcher):
     dispatcher.include_router(start_router)
-    dispatcher.include_router(base_router)  # Додаємо базовий роутер
+    dispatcher.include_router(base_router)
