45f92a94a0a9e76249d8f8d45575d698d257f6f9
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-10 20:23:57 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,10 +1,5 @@
 from .base import router as base_router
 
-routers = [
-    base_router,
-]
-
-def setup_handlers(dp):
-    """Реєстрація всіх хендлерів"""
-    for router in routers:
-        dp.include_router(router)
+def setup_handlers(dispatcher):
+    dispatcher.include_router(start_router)
+    dispatcher.include_router(base_router)
