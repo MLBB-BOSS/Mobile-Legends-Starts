58476acae8f93d123572f4fe0a13be8637d00b9f
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-10 11:28:28 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,6 +1,4 @@
 from .start_handler import router as start_router
-from .base import router as base_router
 
 def setup_handlers(dispatcher):
     dispatcher.include_router(start_router)
-    dispatcher.include_router(base_router)
