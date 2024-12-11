204a21f675bcdf0ab14a0456152012055a6578f1
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-12 01:37:04 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,4 +1,7 @@
-from .base import router as base_router
+from .base import router as base_router, register_handlers as register_base_handlers
+from .tournaments import register_handlers as register_tournaments_handlers
+from .meta import register_handlers as register_meta_handlers
+from .m6 import register_handlers as register_m6_handlers
 
 routers = [
     base_router,
