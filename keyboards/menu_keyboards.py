657c8dc12101ce81b3ad87763e4adcab734784a1
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-11 21:44:48 +0200

    Delete keyboards/menu_keyboards.py

--- keyboards/menu_keyboards.py
+++
@@ -1,7 +0,0 @@
-from aiogram.utils.keyboard import InlineKeyboardBuilder
-
-def get_main_menu():
-    builder = InlineKeyboardBuilder()
-    builder.button(text="📩 Керувати повідомленнями", callback_data="manage_messages")
-    builder.adjust(1)
-    return builder.as_markup()
