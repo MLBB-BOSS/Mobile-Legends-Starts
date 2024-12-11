78af34fde4c9bbf0aaf60bf851aee6ea548c6ac3
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-12 01:11:56 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,22 +1,24 @@
-# handlers/__init__.py
-
 from .base import register_handlers as register_base_handlers
 from .tournaments import register_handlers as register_tournaments_handlers
 from .meta import register_handlers as register_meta_handlers
 from .m6 import register_handlers as register_m6_handlers
 from .base import router as base_router
 
+# Список роутерів для реєстрації
+routers = [
+    base_router,  # Головний роутер
+]
+
 def setup_handlers(dp):
+    """
+    Реєстрація всіх хендлерів
+    """
+    # Виклик функцій для реєстрації обробників
     register_base_handlers(dp)
     register_tournaments_handlers(dp)
     register_meta_handlers(dp)
     register_m6_handlers(dp)
 
-routers = [
-    base_router,
-]
-
-def setup_handlers(dp):
-    """Реєстрація всіх хендлерів"""
+    # Інтеграція роутерів
     for router in routers:
         dp.include_router(router)
