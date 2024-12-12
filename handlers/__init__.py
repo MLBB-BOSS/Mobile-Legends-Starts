7207c4e081d56377095b845895e4e4d325a92583
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-12 12:04:24 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,13 +1,7 @@
 from .base import router as base_router
-from .m6 import router as m6_router
-from .meta import router as meta_router
-from .tournaments import router as tournaments_router
 
 routers = [
     base_router,
-    m6_router,
-    meta_router,
-    tournaments_router,
 ]
 
 def setup_handlers(dp):
