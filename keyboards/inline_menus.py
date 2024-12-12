013f2c3fd09ff3fe97405632dbf7c395b5893ac9
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-12 03:48:12 +0200

    Update inline_menus.py

--- keyboards/inline_menus.py
+++ keyboards/inline_menus.py
@@ -1,43 +1,50 @@
 # keyboards/inline_menus.py
 
+from aiogram.utils.callback_data import CallbackData
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+# Визначення CallbackData
+callback_data = CallbackData("action", "page")
+
 def get_intro_page_1_keyboard() -> InlineKeyboardMarkup:
-    """
-    Створює інлайн-клавіатуру для першої сторінки вступу з кнопкою 'Далі'.
-    """
     return InlineKeyboardMarkup(inline_keyboard=[
         [
-            InlineKeyboardButton(text="Далі", callback_data="intro_next_1")
+            InlineKeyboardButton(text="Далі", callback_data=callback_data.new(action="intro_next", page=1))
         ]
     ])
 
 def get_intro_page_2_keyboard() -> InlineKeyboardMarkup:
-    """
-    Створює інлайн-клавіатуру для другої сторінки вступу з кнопкою 'Далі'.
-    """
     return InlineKeyboardMarkup(inline_keyboard=[
         [
-            InlineKeyboardButton(text="Далі", callback_data="intro_next_2")
+            InlineKeyboardButton(text="Далі", callback_data=callback_data.new(action="intro_next", page=2))
         ]
     ])
 
 def get_intro_page_3_keyboard() -> InlineKeyboardMarkup:
-    """
-    Створює інлайн-клавіатуру для третьої сторінки вступу з кнопкою 'Розпочати'.
-    """
     return InlineKeyboardMarkup(inline_keyboard=[
         [
-            InlineKeyboardButton(text="Розпочати", callback_data="intro_start")
+            InlineKeyboardButton(text="Розпочати", callback_data=callback_data.new(action="intro_start", page=3))
         ]
     ])
 
 def get_generic_inline_keyboard() -> InlineKeyboardMarkup:
-    """
-    Створює інлайн-клавіатуру з однією кнопкою '---MLS---'.
-    """
     return InlineKeyboardMarkup(inline_keyboard=[
         [
-            InlineKeyboardButton(text="---MLS---", callback_data="mls_button")
+            InlineKeyboardButton(text="---MLS---", callback_data=callback_data.new(action="mls_button", page=0))
+        ]
+    ])
+
+def get_guides_inline_keyboard() -> InlineKeyboardMarkup:
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Нові Гайди", callback_data=callback_data.new(action="guides_new", page=1)),
+            InlineKeyboardButton(text="Популярні Гайди", callback_data=callback_data.new(action="guides_popular", page=1))
+        ],
+        [
+            InlineKeyboardButton(text="Для Початківців", callback_data=callback_data.new(action="guides_beginner", page=1)),
+            InlineKeyboardButton(text="Стратегії гри", callback_data=callback_data.new(action="guides_advanced", page=1))
+        ],
+        [
+            InlineKeyboardButton(text="Командна Гра", callback_data=callback_data.new(action="guides_teamplay", page=1))
         ]
     ])
