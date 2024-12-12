c404ff70ecba777cc18f91c55dba42e4f64ca71b
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-12 04:06:19 +0200

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,10 +1,1 @@
 # keyboards/__init__.py
-
-from keyboards import (
-    get_intro_page_1_keyboard,
-    get_intro_page_2_keyboard,
-    get_intro_page_3_keyboard,
-    get_generic_inline_keyboard,
-    get_guides_inline_keyboard,
-    callback_data
-)
