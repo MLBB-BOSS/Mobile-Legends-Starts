3f40af2f3f1060ce9c3a9380011a43c91ef78802
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-14 22:46:20 +0200

    Update user_badges.py

--- models/user_badges.py
+++ models/user_badges.py
@@ -1,14 +1,10 @@
-# models/user_stats.py
-from sqlalchemy import Column, Integer, ForeignKey
-from sqlalchemy.orm import relationship
+# models/user_badges.py
+from sqlalchemy import Table, Column, Integer, ForeignKey
 from .base import Base
 
-class UserStats(Base):
-    __tablename__ = 'user_stats'
-
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'), unique=True)
-    # Додайте інші поля статистики за потребою
-
-    # Відношення до користувача
-    user = relationship('User', back_populates='stats')
+user_badges = Table(
+    'user_badges',
+    Base.metadata,
+    Column('user_id', Integer, ForeignKey('users.id'), primary_key=True),
+    Column('badge_id', Integer, ForeignKey('badges.id'), primary_key=True)
+)
