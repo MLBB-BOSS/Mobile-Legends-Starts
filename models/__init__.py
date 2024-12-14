4ab09092bd1dc2ed0469bc7e1c40648c813c32bf
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-14 19:10:22 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,8 @@
 # models/__init__.py
 from .base import Base
 from .user import User
+from .badge import Badge
+from .rating_history import RatingHistory
+from .user_stats import UserStats
 
-__all__ = ['Base', 'User']
+__all__ = ["Base", "User", "Badge", "RatingHistory", "UserStats"]
