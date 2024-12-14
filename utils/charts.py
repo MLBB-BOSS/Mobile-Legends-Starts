945b4bb4d38c05e07cefb74f14b426e1aecbd6c3
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-14 12:46:53 +0200

    Update charts.py

--- utils/charts.py
+++ utils/charts.py
@@ -1,19 +1,8 @@
-import io
-import matplotlib.pyplot as plt
+from aiogram.types import InputFile
+from utils.charts import generate_rating_chart
 
-def generate_rating_chart(rating_history: list[int]) -> io.BytesIO:
-    """
-    rating_history - список рейтингів по часу.
-    Для прикладу: [100, 200, 250, 300]
-    """
-    plt.figure(figsize=(4,4))
-    plt.plot(rating_history, marker='o')
-    plt.title("Графік зміни рейтингу")
-    plt.xlabel("Сеанс")
-    plt.ylabel("Рейтинг")
+rating_history = [100, 200, 250, 300]
+chart = generate_rating_chart(rating_history)
+photo_file = InputFile(chart, filename="chart.png")
 
-    buf = io.BytesIO()
-    plt.savefig(buf, format='png')
-    buf.seek(0)
-    plt.close()  # очищаємо, щоб уникнути витоків пам'яті
-    return buf
+await message.answer_photo(photo=photo_file, caption="Ваш прогрес за останні сеанси")
