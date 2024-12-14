07b378226bb27f8cb0bc7febbd2355cddf11e2ee
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-14 23:56:40 +0200

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,9 +1,22 @@
 # utils/db.py
 from sqlalchemy.ext.asyncio import AsyncSession
-from database import async_session
-from contextlib import asynccontextmanager
+from sqlalchemy.future import select
+from models.user import Badge, User
 
-@asynccontextmanager
-async def get_db_session():
-    async with async_session() as session:
-        yield session
+async def get_all_badges(db: AsyncSession):
+    """
+    Повертає список усіх бейджів, доступних у боті.
+    Це бейджі, які можуть бути присвоєні, але не обов'язково вже присвоєні користувачам.
+    """
+    result = await db.execute(select(Badge))
+    badges = result.scalars().all()
+    return badges
+
+async def get_user_by_telegram_id(db: AsyncSession, telegram_id: int) -> User:
+    """
+    Отримує користувача за telegram_id.
+    Повертає об’єкт User або None, якщо користувача не знайдено.
+    """
+    result = await db.execute(select(User).where(User.telegram_id == telegram_id))
+    user = result.scalars().first()
+    return user
