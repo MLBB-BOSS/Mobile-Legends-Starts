c6c8aa2ea4a885d7d4b38c76ab7f3d3db27cd76f
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-14 11:09:35 +0200

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -8,7 +8,5 @@ engine = create_async_engine(DATABASE_URL, echo=False)
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 async def get_db_session() -> AsyncSession:
-    # Повертає асинхронну сесію. 
-    # Залежно від вашої логіки, можливо вам потрібно використати контекстний менеджер async with:
-    session = async_session()
-    return session
+    # Повертає нову сесію БД
+    return async_session()
