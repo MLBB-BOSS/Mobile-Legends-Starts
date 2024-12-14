270804aa0057b58bd735b7299c178cfed2534a99
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-14 19:20:53 +0200

    Update base.py

--- models/base.py
+++ models/base.py
@@ -1,17 +1,9 @@
-# models/badge.py
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
-from models.base import Base
+# models/base.py
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import AsyncAttrs
 
-class Badge(Base):
-    __tablename__ = 'badges'
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, nullable=False)
-    description = Column(String, nullable=True)
-    icon = Column(String, nullable=True)  # Припустимо, що тут зберігаються емодзі або URL до іконки
-
-    users = relationship("User", secondary="user_badges", back_populates="badges")
-
-    def __repr__(self):
-        return f"<Badge(id={self.id}, name={self.name})>"
+class Base(AsyncAttrs, DeclarativeBase):
+    """Base class for all models"""
+    
+    def to_dict(self):
+        return {c.key: getattr(self, c.key) for c in self.__table__.columns}
