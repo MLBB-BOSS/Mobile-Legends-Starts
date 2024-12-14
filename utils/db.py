6921a34b4e0f922d7a5a6adcb1fefaa24111e9ba
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-14 23:36:37 +0200

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,20 +1,7 @@
 # utils/db.py
-from sqlalchemy.future import select
-from sqlalchemy.orm import selectinload
-from models.user import User, Badge
 from sqlalchemy.ext.asyncio import AsyncSession
+from database import async_session
 
-async def get_user_by_telegram_id(db: AsyncSession, telegram_id: int) -> User | None:
-    # Використовуємо selectinload для жадного завантаження бейджів
-    stmt = (
-        select(User)
-        .where(User.telegram_id == telegram_id)
-        .options(selectinload(User.badges))
-    )
-    result = await db.execute(stmt)
-    return result.scalars().first()
-
-async def get_all_badges(db: AsyncSession) -> list[Badge]:
-    stmt = select(Badge)
-    result = await db.execute(stmt)
-    return result.scalars().all()
+async def get_db_session() -> AsyncSession:
+    async with async_session() as session:
+        yield session
