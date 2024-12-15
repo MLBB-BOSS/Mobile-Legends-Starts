648116d84034ca70cdc447266d6cc73c653f7d08
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-15 04:15:37 +0200

    Update badge.py

--- models/badge.py
+++ models/badge.py
@@ -1,10 +1,15 @@
-# models/badge.py
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy.orm import relationship
 from models.base import Base
 
 class Badge(Base):
-    __tablename__ = "badges"
+    __tablename__ = "badges"  # Назва таблиці
+    __table_args__ = {"extend_existing": True}  # Додаємо цей параметр
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
-    description = Column(String, nullable=True)
+    category = Column(String, nullable=False)
+    level = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="badges")
