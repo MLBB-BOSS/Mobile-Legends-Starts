b5564c8e22b07f75fe90f46de7a882b54d123502
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-15 04:40:59 +0200

    Delete test_models.py

--- test_models.py
+++
@@ -1,7 +0,0 @@
-# models/__init__.py
-from .base import Base
-from .user import User
-from .badge import Badge
-from .user_badges import user_badges
-from .user_stats import UserStats
-# Додайте інші моделі за потребою
