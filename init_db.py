9621cb7790e3c08434136e8ae026016cde1a304a
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-15 14:33:45 +0200

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,11 +1,9 @@
 import asyncio
-from sqlalchemy.ext.asyncio import create_async_engine
+from database import engine
 from models.base import Base
-from config import settings
 
 async def init_db():
     """Ініціалізація бази даних: створення таблиць."""
-    engine = create_async_engine(settings.db_url, echo=True)
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     print("Database initialized successfully.")
