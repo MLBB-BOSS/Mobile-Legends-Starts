c594565a65b0de5c971ec4dc64a15245511ea0e2
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-15 12:50:22 +0200

    Update base.py

--- models/base.py
+++ models/base.py
@@ -2,6 +2,9 @@
 
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
 
 class Base(AsyncAttrs, DeclarativeBase):
     """Base class for all models"""
