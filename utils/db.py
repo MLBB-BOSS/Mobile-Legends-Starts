a1c727f510fa6d895c959b79bea072ce6790ecd2
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-15 12:46:02 +0200

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -4,10 +4,13 @@ from sqlalchemy.orm import sessionmaker
 from models.base import Base
 
 # URL бази даних
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://user:password@host:port/dbname")
+DATABASE_URL = os.getenv("AS_BASE", "postgresql+asyncpg://user:password@host:port/dbname")
+
+if not DATABASE_URL:
+    raise ValueError("AS_BASE is not set or invalid")
 
 # Створення асинхронного двигуна
-engine = create_async_engine(DATABASE_URL, echo=False)
+engine = create_async_engine(DATABASE_URL, echo=True)
 
 # Фабрика асинхронних сесій
 async_session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
