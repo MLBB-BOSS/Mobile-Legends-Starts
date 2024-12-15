51057216cefd6d38d9e29712ab0aa188258ccff8
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-15 12:23:43 +0200

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -2,6 +2,23 @@
 import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
+from aiogram import BaseMiddleware
+
+class DatabaseMiddleware(BaseMiddleware):
+    def __init__(self, session_factory):
+        super().__init__()
+        self.session_factory = session_factory
+
+    async def __call__(self, handler, event, data):
+        async with self.session_factory() as session:
+            data['db'] = session
+            try:
+                return await handler(event, data)
+            except Exception as e:
+                await session.rollback()
+                raise
+            finally:
+                await session.close()
 
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
