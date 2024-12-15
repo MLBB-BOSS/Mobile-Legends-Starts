3107765e6fde8b52def63f3a1ecedca8acded65e
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-15 14:32:31 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,10 +1,13 @@
-from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from models.base import Base
 
-class UserStats(Base):
-    __tablename__ = 'user_stats'
-    
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    rating = Column(Integer, nullable=False)
-    # Додайте інші поля за необхідності
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    telegram_id = Column(Integer, unique=True, nullable=False)
+    username = Column(String(50), nullable=True)
+    fullname = Column(String(100), nullable=True)
+
+    stats = relationship("UserStats", back_populates="user")
