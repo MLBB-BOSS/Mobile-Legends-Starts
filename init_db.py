cc1e2f67ada204bb72412907c7de45ed6991194c
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-15 04:37:02 +0200

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -2,25 +2,21 @@ import asyncio
 import logging
 from utils.db import engine
 from models.base import Base
-import models.user  # Імпортуємо модель User
-import models.user_stats  # Імпортуємо модель UserStats
-import models.badge  # Імпортуємо модель Badge
+import models.user  # Модель користувача
+import models.badge  # Модель бейджів
+import models.user_stats  # Модель статистики
 
 # Налаштування логування
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 async def init_db():
-    """Ініціалізація бази даних"""
+    """Створення всіх таблиць"""
     logger.info("Initializing the database...")
-    try:
-        async with engine.begin() as conn:
-            # Створення всіх таблиць, якщо вони ще не існують
-            await conn.run_sync(Base.metadata.create_all, checkfirst=True)
-        logger.info("Database initialized successfully.")
-    except Exception as e:
-        logger.error(f"Error initializing the database: {e}")
-        raise
+    async with engine.begin() as conn:
+        # Перевірка та створення таблиць
+        await conn.run_sync(Base.metadata.create_all, checkfirst=True)
+    logger.info("Database initialized successfully.")
 
 if __name__ == "__main__":
     try:
