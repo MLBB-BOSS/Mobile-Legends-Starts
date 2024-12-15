d7a76c511e5405e564ec3de2a1f772042d3db365
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-15 20:44:05 +0200

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,22 +1,15 @@
-Ю# init_db.py
+# init_db.py
 
 import asyncio
 import logging
-from config import engine, settings
-from sqlalchemy.ext.asyncio import AsyncEngine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-# Імпорт моделей
-import models.user  # Модель User
-import models.user_stats  # Модель UserStats
+from utils.db import engine
+from models.base import Base
+import models.user  # Імпортуємо модель User
+import models.user_stats  # Імпортуємо модель UserStats
 
 # Налаштування логування
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-# Ініціалізація бази
-Base = declarative_base()
 
 async def init_db():
     logger.info("Initializing the database...")
