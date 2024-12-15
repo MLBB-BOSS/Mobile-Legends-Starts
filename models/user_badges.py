56ee4b5e28d7a7a650f197a7d2450ca8f6767dbc
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-15 05:33:50 +0200

    Update user_badges.py

--- models/user_badges.py
+++ models/user_badges.py
@@ -1,10 +1,9 @@
-# models/user_badges.py
 from sqlalchemy import Table, Column, Integer, ForeignKey
 from models.base import Base
 
 user_badges = Table(
     'user_badges',
     Base.metadata,
-    Column('user_id', Integer, ForeignKey('users.id', ondelete='CASCADE'), primary_key=True),
-    Column('badge_id', Integer, ForeignKey('badges.id', ondelete='CASCADE'), primary_key=True)
+    Column('user_id', Integer, ForeignKey('users.id'), primary_key=True),
+    Column('badge_id', Integer, ForeignKey('badges.id'), primary_key=True)
 )
