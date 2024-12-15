cc92bd245dc2c5978e707d081a89039b9e5b012f
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-15 17:12:50 +0200

    Update base.py

--- models/base.py
+++ models/base.py
@@ -1,9 +1,10 @@
 # models/base.py
+
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.ext.asyncio import AsyncAttrs
 
 class Base(AsyncAttrs, DeclarativeBase):
     """Base class for all models"""
-    
+
     def to_dict(self):
         return {c.key: getattr(self, c.key) for c in self.__table__.columns}
