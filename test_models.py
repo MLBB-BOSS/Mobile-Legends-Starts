6f55f486e8e282a6a526f7b6020c1044a830b89f
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-15 20:45:52 +0200

    Update

---
+++ test_models.py
@@ -0,0 +1,7 @@
+# models/__init__.py
+from .base import Base
+from .user import User
+from .badge import Badge
+from .user_badges import user_badges
+from .user_stats import UserStats
+# Додайте інші моделі за потребою
