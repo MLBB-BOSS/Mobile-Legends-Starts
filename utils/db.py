0a1f64eca98eb2766c14e61755c2b3b0e312cd45
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-15 14:16:50 +0200

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,17 +1,12 @@
-
 import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-
-# Завантажуємо змінні оточення
-load_dotenv()
 
 # Базовий клас для моделей
 from models.base import Base
 
 # URL бази даних
-DATABASE_URL = os.getenv("AS_BASE")
+DATABASE_URL = os.getenv("AS_BASE", "postgresql+asyncpg://ufk3frgco7l9d1:p7aad477be5e7c084f8d9c2e9998fdfd75ed3eb573c808a6b3db95bbdb221b234@ccaml3dimis7eh.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/d7rglea9jc6ggd")
 
 if not DATABASE_URL:
     raise ValueError("AS_BASE is not set or invalid")
