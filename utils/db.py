e4ebe1c82fc2bc2a94c08ef5014679839b66eb26
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-15 09:38:35 +0200

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
 # Змінна середовища або хардкоджений URL — краще зберігати у змінних середовища
-AS_BASE = os.getenv("AS_BASE", "postgresql+asyncpg://udoepvnsfd1v4p:p06d554a757b594fc448b0fe17f59b24af6e1ed553f9cd262a36d4e56fd87a37f@c9tiftt16dc3eo.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/d76pc5iknkd84")
+AS_BASE = os.getenv("AS_BASE", "-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/d7rglea9jc6ggd")
 
 if not AS_BASE:
     raise ValueError("AS_BASE is not set or invalid")
