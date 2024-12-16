b3375c8f0dc394cc24d484efb7a1ca52f2290c64
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-16 16:15:15 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,7 +1,9 @@
 from .base import router as base_router
+from .profile import profile_router  # Додано імпорт profile_router
 
 routers = [
     base_router,
+    profile_router,  # Додаємо profile_router до списку
 ]
 
 def setup_handlers(dp):
