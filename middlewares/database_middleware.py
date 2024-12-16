e8bee928adec64b62db85e23e958ab03b09d46b2
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-16 20:46:57 +0200

    Update database_middleware.py

--- middlewares/database_middleware.py
+++ middlewares/database_middleware.py
@@ -7,13 +7,19 @@ logger = logging.getLogger(__name__)
 
 class DatabaseMiddleware(BaseMiddleware):
     async def __call__(self, handler, event, data):
+        logger.info("Initializing database session")
         async with async_session() as session:
             data['db'] = session
             try:
-                return await handler(event, data)
+                response = await handler(event, data)
+                await session.commit()  # Комміт змін, якщо все успішно
+                logger.info("Session committed successfully")
+                return response
             except Exception as e:
-                logger.error(f"Database error: {e}")
-                await session.rollback()
+                logger.error(f"Database error occurred: {e}")
+                await session.rollback()  # Відкат змін у разі помилки
+                logger.info("Rolled back session due to error")
                 raise
             finally:
                 await session.close()
+                logger.info("Database session closed")
