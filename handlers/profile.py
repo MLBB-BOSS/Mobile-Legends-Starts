8790149efadbc190b052fad657cf71af3ebc57da
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-16 14:59:40 +0200

    Update profile.py

--- handlers/profile.py
+++ handlers/profile.py
@@ -6,8 +6,9 @@ from sqlalchemy.orm import Session
 from io import BytesIO
 
 from utils.db import get_db_session
-from services.user_service import get_user_profile_text
+from services.user_service import get_user_profile_text, update_mlbb_id
 from utils.charts import generate_rating_chart
+
 
 # Middleware для отримання сесії бази даних
 class DbSessionMiddleware(BaseMiddleware):
@@ -21,9 +22,11 @@ class DbSessionMiddleware(BaseMiddleware):
         data["db"] = db_session
         return await handler(event, data)
 
+
 # Створюємо роутер для профілю
 profile_router = Router()
 profile_router.message.middleware(DbSessionMiddleware())
+
 
 # Обробник для команди /profile
 @profile_router.message(Command("profile"))
@@ -46,3 +49,15 @@ async def show_profile(message: Message, db: Session):
 
     # Відправляємо графік користувачу разом із текстом профілю
     await message.answer_photo(photo=input_file, caption=profile_text)
+
+
+# Обробник для команди /add_mlbb
+@profile_router.message(Command("add_mlbb"))
+async def add_mlbb_id(message: Message, db: Session):
+    args = message.get_args()
+    if not args:
+        await message.answer("Будь ласка, введіть ваш MLBB ID: /add_mlbb <ваш_id>")
+        return
+    
+    response = await update_mlbb_id(db, message.from_user.id, args)
+    await message.answer(response)
