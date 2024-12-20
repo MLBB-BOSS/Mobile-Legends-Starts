5cf8a6e4ab6e8ada7d918d9c3691da52ee73dae3
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-20 22:38:14 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,7 +1,9 @@
 from .base import router as base_router
+from .missing_handlers import router as missing_router  # Новий хендлер
 
 routers = [
     base_router,
+    missing_router,  # Додано новий маршрутизатор
 ]
 
 def setup_handlers(dp):
