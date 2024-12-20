1ddd96a4661a4da511286b764887af0c78c327fb
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-20 02:56:41 +0200

    Update

---
+++ utils/text_formatter.py
@@ -0,0 +1,15 @@
+# utils/text_formatter.py
+
+def format_profile_text(template: str, data: dict) -> str:
+    """
+    Форматує текст профілю, замінюючи плейсхолдери на реальні дані користувача.
+    
+    :param template: Шаблонний текст з плейсхолдерами.
+    :param data: Словник з даними користувача.
+    :return: Відформатований текст.
+    """
+    try:
+        return template.format(**data)
+    except KeyError as e:
+        missing_key = e.args[0]
+        raise ValueError(f"Відсутній ключ для форматування тексту профілю: {missing_key}") from e
