7a84e28246879df51d4e736de59b31b5f01634e4
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-21 15:35:23 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
-# models/__init__.py
 from .base import Base
 from .user import User
+from .feedback import Feedback
+from .user_stats import UserStats
 
-__all__ = ['Base', 'User']
+__all__ = ['Base', 'User', 'Feedback', 'UserStats']
