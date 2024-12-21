bc291429c0b5245d404558134cc405bb1a7de4ce
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-21 03:40:22 +0200

    Create menu_messages.py

---
+++ utils/menu_messages.py
@@ -0,0 +1,8 @@
+class MenuMessages:
+    @staticmethod
+    def get_heroes_menu_text():
+        return {"title": "Персонажі", "description": "Оберіть героя"}
+
+    @staticmethod
+    def get_guides_menu_text():
+        return {"title": "Гайди", "description": "Оберіть гайд"}
