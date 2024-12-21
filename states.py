0b90c84cbeb007851deb6f4ece3b6ee5cfe0f349
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-22 01:42:16 +0200

    Update states.py

--- states.py
+++ states.py
@@ -1,3 +1,5 @@
+# states.py
+
 from aiogram.fsm.state import StatesGroup, State
 
 class MenuStates(StatesGroup):
@@ -17,31 +19,15 @@ class MenuStates(StatesGroup):
     ACHIEVEMENTS_MENU = State()
     SETTINGS_MENU = State()
     FEEDBACK_MENU = State()
-    HELP_MENU = State()
-    SEARCH_HERO = State()
-    SEARCH_TOPIC = State()
-    CHANGE_USERNAME = State()
     RECEIVE_FEEDBACK = State()
     REPORT_BUG = State()
+    HELP_MENU = State()
     TOURNAMENTS_MENU = State()
     META_MENU = State()
     M6_MENU = State()
     GPT_MENU = State()
-
-    # Додаткові стани
-    CHALLENGES_MENU = State()
-    BUST_MENU = State()
-    TEAMS_MENU = State()
-    TRADING_MENU = State()
-    SETTINGS_SUBMENU = State()
-    HELP_SUBMENU = State()
-    MY_TEAM_MENU = State()
-    SELECT_LANGUAGE = State()
-
-async def increment_step(state):
-    data = await state.get_data()
-    step_count = data.get("step_count", 0) + 1
-    if step_count >= 3:
-        await state.clear()
-        step_count = 0
-    await state.update_data(step_count=step_count)
+    SEARCH_HERO = State()
+    SEARCH_TOPIC = State()
+    CHANGE_USERNAME = State()
+    COMPARISON_STEP_1 = State()
+    COMPARISON_STEP_2 = State()
