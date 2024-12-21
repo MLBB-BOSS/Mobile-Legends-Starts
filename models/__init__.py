5069294641799d0ef731b613ac8d9fc09804b009
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-21 14:57:28 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,11 @@
 # models/__init__.py
-from .base import Base
-from .user import User
 
-__all__ = ['Base', 'User']
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+from .user import User
+from .user_stats import UserStats
+from .feedback import Feedback
+from .bug_report import BugReport
+# Import other models here
