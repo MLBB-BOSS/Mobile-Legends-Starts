cd746336f37f632136eecc8aa9de61667e5c6eea
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-21 17:06:34 +0200

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,5 +1,6 @@
 #utils/db.py
 
+from utils.charts import create_chart
 from utils.charts import charts_router
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
