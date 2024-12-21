2a97d6092b0aa2b714919a57f2c7905c3c929b38
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-21 16:30:18 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,12 +1,17 @@
+# handlers/__init__.py
+
 from .base import router as base_router
-from .missing_handlers import router as missing_router  # Новий хендлер
+# Імпортуйте інші маршрутизатори з інших файлів, наприклад:
+# from .progress import router as progress_router
+# from .feedback import router as feedback_router
 
 routers = [
     base_router,
-    missing_router,  # Додано новий маршрутизатор
+    # progress_router,
+    # feedback_router,
+    # Додайте інші маршрутизатори тут
 ]
 
-def setup_handlers(dp):
-    """Реєстрація всіх хендлерів"""
+def setup_handlers(dp: Dispatcher):
     for router in routers:
         dp.include_router(router)
