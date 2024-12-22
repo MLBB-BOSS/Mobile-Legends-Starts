0f1aef344040edc41db82eda56cc4aad37344872
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-22 19:58:40 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,10 +1,12 @@
+
 from aiogram import Dispatcher
 from .base import router as base_router
 from .missing_handlers import router as missing_handlers_router
 
 def setup_handlers(dp: Dispatcher):
+    # Список роутерів для підключення
     routers = [base_router, missing_handlers_router]
 
+    # Підключення всіх роутерів до диспетчера
     for router in routers:
-        if router not in dp.routers:
-            dp.include_router(router)
+        dp.include_router(router)
