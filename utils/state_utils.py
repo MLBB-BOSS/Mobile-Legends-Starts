59f5eb750efe8899ba5c6aa516ec50a0c7df4f06
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-22 02:12:42 +0200

    Update state_utils.py

--- utils/state_utils.py
+++ utils/state_utils.py
@@ -1,12 +1,11 @@
 # utils/state_utils.py
 
 from aiogram.fsm.context import FSMContext
-from states import MenuStates
-import logging
 
-logger = logging.getLogger(__name__)
-
-async def transition_state(state: FSMContext, new_state: MenuStates):
-    await state.clear()
-    await state.set_state(new_state)
-    logger.info(f"Transitioned to state: {new_state.name}")
+async def increment_step(state: FSMContext):
+    data = await state.get_data()
+    step_count = data.get("step_count", 0) + 1
+    if step_count >= 3:
+        await state.clear()
+        step_count = 0
+    await state.update_data(step_count=step_count)
