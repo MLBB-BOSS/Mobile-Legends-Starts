7c11602fb25084a99026cdde97577a2ba1e4ddb8
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-27 18:22:06 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 #config.py
 import logging
-from pydantic import BaseSettings, Field
+from pydantic_settings import BaseSettings
+from pydantic import Field
 
 # Налаштування логування
 logging.basicConfig(level=logging.INFO)
