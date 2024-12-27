1242e67ec173e357de7df7d1050b40d2d7560f5c
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-27 17:21:57 +0200

    Update states.py

--- states.py
+++ states.py
@@ -1,56 +1,24 @@
+# states.py
+
 from aiogram.fsm.state import StatesGroup, State
 
 class MenuStates(StatesGroup):
-    # Головне меню та підменю
-    INTRO_PAGE_1 = State()
-    INTRO_PAGE_2 = State()
-    INTRO_PAGE_3 = State()
     MAIN_MENU = State()
-    NAVIGATION_MENU = State()
-    PROFILE_MENU = State()
+    CHALLENGES_MENU = State()
+    GUIDES_MENU = State()
+    BUST_MENU = State()
+    TEAMS_MENU = State()
+    TRADING_MENU = State()
     SETTINGS_MENU = State()
     SETTINGS_SUBMENU = State()
-    HELP_MENU = State()
-    HELP_SUBMENU = State()
-
-    # Меню персонажів
-    HEROES_MENU = State()
-    HERO_CLASS_MENU = State()
-    SEARCH_HERO = State()
-    CHALLENGES_MENU = State()
-    COMPARISON_STEP_1 = State()
-    COMPARISON_STEP_2 = State()
-
-    # Гайди, контр-піки та білди
-    GUIDES_MENU = State()
-    COUNTER_PICKS_MENU = State()
-    BUILDS_MENU = State()
-    VOTING_MENU = State()
-    
-    # Меню статистики
-    STATISTICS_MENU = State()
-    ACHIEVEMENTS_MENU = State()
-
-    # Турніри та команди
-    TOURNAMENTS_MENU = State()
-    TEAMS_MENU = State()
-    MY_TEAM_MENU = State()
-
-    # Meta, M6, GPT
-    META_MENU = State()
-    M6_MENU = State()
-    GPT_MENU = State()
-
-    # Торгівля, бафи та додаткові функції
-    TRADING_MENU = State()
-    BUST_MENU = State()
-
-    # Інтерактивні стани
-    SEARCH_TOPIC = State()
-    CHANGE_USERNAME = State()
-    RECEIVE_FEEDBACK = State()
-    REPORT_BUG = State()
     SELECT_LANGUAGE = State()
-
-    # Виправлення: додано стан FEEDBACK_MENU
+    PROFILE_MENU = State()
+    STATS_MENU = State()
+    ACHIEVEMENTS_MENU = State()
     FEEDBACK_MENU = State()
+    HELP_MENU = State()
+    MY_TEAM_MENU = State()
+    GPT_MENU = State()
+    HELP_SUBMENU = State()
+    CHANGE_USERNAME = State()
+    # Додайте інші необхідні стани...
