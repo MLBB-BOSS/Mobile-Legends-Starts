fbb104084bffb939b1881bfece5581b19504e8a6
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-28 14:49:53 +0200

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,1 +1,11 @@
 # keyboards/__init__.py
+
+from .menus import *
+from .inline_keyboards import *
+
+__all__ = (
+    'get_main_menu',
+    'get_heroes_menu',
+    'get_generic_inline_keyboard',
+    # ... інші функції ...
+)# keyboards/__init__.py
