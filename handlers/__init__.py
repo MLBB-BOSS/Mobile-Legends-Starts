b8f73647b50378357274147030206bc701b01352
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-28 02:38:10 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,6 +2,10 @@
 from aiogram import Dispatcher
 from .base import router as base_router
 from .missing_handlers import router as missing_handlers_router
+from .base import router as base_router
+from .hero_handlers import router as hero_router
+
+__all__ = ['base_router', 'hero_router']
 
 def setup_handlers(dp: Dispatcher):
     # Список роутерів для підключення
