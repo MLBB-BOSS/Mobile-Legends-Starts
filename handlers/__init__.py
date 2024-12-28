3fef2a1ffb32f32606b392de8c11e36efeeab037
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-29 01:45:28 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,14 +1,12 @@
-# handlers/__init__.py
-
-from aiogram import Dispatcher
-from .start_intro import router as start_intro_router
-from .main_menu import router as main_menu_router
-
-# Не підключаємо все інше. Коли доробимо – тоді додаємо.
-# from .navigation.menu import router as navigation_router
-# from .profile.menu import router as profile_router
+from aiogram import Router
+from .menu import router as navigation_menu_router
+from .heroes import router as heroes_router
+# from .builds import router as builds_router
+# from .guides import router as guides_router
 # ...
 
-def setup_handlers(dp: Dispatcher):
-    dp.include_router(start_intro_router)
-    dp.include_router(main_menu_router)
+router = Router()
+router.include_router(navigation_menu_router)
+router.include_router(heroes_router)
+# router.include_router(builds_router)
+# router.include_router(guides_router)
