907412d1aa8f5bc1689d396f1e144c8d35350b7c
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-29 01:54:49 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,12 +1,27 @@
-from aiogram import Router
-from .menu import router as navigation_menu_router
-from .heroes import router as heroes_router
-# from .builds import router as builds_router
-# from .guides import router as guides_router
-# ...
+# handlers/__init__.py
+from aiogram import Dispatcher
 
-router = Router()
-router.include_router(navigation_menu_router)
-router.include_router(heroes_router)
-# router.include_router(builds_router)
-# router.include_router(guides_router)
+# Підключаємо хендлери інтро (якщо є)
+from .start_intro import router as start_intro_router
+
+# Підключаємо хендлер головного меню
+from .main_menu import router as main_menu_router
+
+# Підключаємо навігацію
+from .navigation import router as navigation_router
+
+# Підключаємо профіль
+from .profile import router as profile_router
+
+def setup_handlers(dp: Dispatcher):
+    # Підключаємо інтро (якщо треба)
+    dp.include_router(start_intro_router)
+
+    # Головне меню
+    dp.include_router(main_menu_router)
+
+    # Навігація
+    dp.include_router(navigation_router)
+
+    # Профіль
+    dp.include_router(profile_router)
