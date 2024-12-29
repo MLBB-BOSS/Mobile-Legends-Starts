e2f7152fd58c468e3a844fa563472a78a2f25a42
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-29 21:58:26 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,12 +1,13 @@
 from aiogram import Dispatcher
 from .main_menu import MainMenuHandler
+from .intro_handler import IntroHandler
 
 def setup_handlers(dp: Dispatcher) -> None:
     """Реєстрація всіх обробників"""
+    # Створюємо екземпляр обробника інтро
+    intro_handler = IntroHandler()
+    dp.include_router(intro_handler.router)
+
     # Створюємо екземпляр обробника головного меню
     main_menu = MainMenuHandler()
-    
-    # Реєструємо роутер головного меню
     dp.include_router(main_menu.router)
-
-__all__ = ['setup_handlers']
