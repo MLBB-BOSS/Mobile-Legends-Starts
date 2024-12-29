64029f3757013f8c970ed63e1c88e0af98eb3b53
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-29 19:53:20 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,27 +1,10 @@
-# handlers/__init__.py
 from aiogram import Dispatcher
+from .main_menu import MainMenuHandler
 
-# Підключаємо хендлери інтро (якщо є)
-from .start_intro import router as start_intro_router
-
-# Підключаємо хендлер головного меню
-from .main_menu import router as main_menu_router
-
-# Підключаємо навігацію
-from .navigation import router as navigation_router
-
-# Підключаємо профіль
-from .profile import router as profile_router
-
-def setup_handlers(dp: Dispatcher):
-    # Підключаємо інтро (якщо треба)
-    dp.include_router(start_intro_router)
-
-    # Головне меню
-    dp.include_router(main_menu_router)
-
-    # Навігація
-    dp.include_router(navigation_router)
-
-    # Профіль
-    dp.include_router(profile_router)
+def setup_handlers(dp: Dispatcher) -> None:
+    """Реєстрація всіх обробників"""
+    # Створюємо екземпляр обробника головного меню
+    main_menu = MainMenuHandler()
+    
+    # Реєструємо роутер головного меню
+    dp.include_router(main_menu.router)
