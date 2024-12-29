433d74d412fd1438f1710be66aeed2b980fd651e
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2024-12-29 02:25:13 +0200

    Create menu_states.py

---
+++ states/menu_states.py
@@ -0,0 +1,25 @@
+# states/menu_states.py
+from aiogram.fsm.state import State, StatesGroup
+
+class MenuStates(StatesGroup):
+    MAIN_MENU = State()
+    NAVIGATION_MENU = State()
+    HEROES_MENU = State()
+    PROFILE_MENU = State()
+    SETTINGS_MENU = State()
+    STATISTICS_MENU = State()
+    ACHIEVEMENTS_MENU = State()
+    FEEDBACK_MENU = State()
+    HELP_MENU = State()
+    GUIDES_MENU = State()
+    TOURNAMENTS_MENU = State()
+    TEAMS_MENU = State()
+    TRADING_MENU = State()
+    CHALLENGES_MENU = State()
+    BUST_MENU = State()
+    GPT_MENU = State()
+    M6_MENU = State()
+    META_MENU = State()
+    BUILDS_MENU = State()
+    VOTING_MENU = State()
+    COUNTER_PICKS_MENU = State()
