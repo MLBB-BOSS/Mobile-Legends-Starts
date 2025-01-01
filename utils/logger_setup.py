f53a5e31d9ba7dd39e4d0d080c999349fda21821
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-02 00:57:35 +0200

    Update logger_setup.py

--- utils/logger_setup.py
+++ utils/logger_setup.py
@@ -1,15 +1,21 @@
+# utils/logger_setup.py
+
 import logging
 
-def setup_logger(name='bot_logger', level=logging.INFO):
+def setup_logger(name: str) -> logging.Logger:
+    """
+    Налаштовує та повертає логгер з заданим ім'ям.
+
+    :param name: Ім'я логгера.
+    :return: Налаштований логгер.
+    """
     logger = logging.getLogger(name)
-    logger.setLevel(level)
-    
-    # Create console handler with formatting
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    
-    # Add handler to logger
-    logger.addHandler(handler)
-    
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
     return logger
