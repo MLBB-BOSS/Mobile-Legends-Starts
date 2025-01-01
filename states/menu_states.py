88b20dbda0b7d865a4f7262c51f624aa8939183e
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-01 23:10:15 +0200

    Update menu_states.py

--- states/menu_states.py
+++ states/menu_states.py
@@ -1,5 +1,16 @@
-from enum import auto
 from aiogram.fsm.state import State, StatesGroup
+
+class IntroState(StatesGroup):
+    """Intro states"""
+    page_1 = State()
+    page_2 = State()
+    page_3 = State()
+
+class MainMenuState(StatesGroup):
+    """Main menu states"""
+    main = State()
+    navigation = State()
+    profile = State()
 
 class IntroState(StatesGroup):
     """Intro section states"""
