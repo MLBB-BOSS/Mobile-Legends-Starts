0eeac59da8807f01ebcfcda517840816ca79c663
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-02 00:48:32 +0200

    Update menus.py

--- keyboards/menus.py
+++ keyboards/menus.py
@@ -1,5 +1,6 @@
 # keyboards/menus.py
 
+from typing import List, Union
 from utils.logger_setup import setup_logger
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from texts.enums import MenuButton, LanguageButton
