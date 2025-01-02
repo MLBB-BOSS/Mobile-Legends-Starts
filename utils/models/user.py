02bf24deccd43d27bffb53618a2bb7cd02ae8885
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-03 01:08:02 +0200

    Create user.py

---
+++ utils/models/user.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String
+from utils.db import Base
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    telegram_id = Column(Integer, unique=True, nullable=False)
