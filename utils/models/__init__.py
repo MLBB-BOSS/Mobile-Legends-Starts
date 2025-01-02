e1753bc0ac61fe3ed62d7e3bc44940c3f1b7fcd5
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-03 01:31:24 +0200

    Update __init__.py

--- utils/models/__init__.py
+++ utils/models/__init__.py
@@ -1,7 +1,6 @@
+# utils/models/__init__.py
 from .user import User
 from .item import Item
+# Додайте імпорти інших моделей тут
 
-__all__ = [
-    "User",
-    "Item",
-]
+__all__ = ['User', 'Item']  # Додайте інші моделі до цього списку
