785aafdcd9446e1668671fd476400cefd515e886
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-03 01:47:19 +0200

    Update item.py

--- utils/models/item.py
+++ utils/models/item.py
@@ -1,6 +1,6 @@
 # utils/models/item.py
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, Mapped, mapped_column
 from utils.base import Base  # Імпорт базового класу
 
 class Item(Base):
@@ -9,13 +9,13 @@ class Item(Base):
     """
     __tablename__ = 'items'
 
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    description = Column(String, nullable=True)
-    owner_id = Column(Integer, ForeignKey('users.id'))
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(String, index=True)
+    description: Mapped[Optional[str]] = mapped_column(String, nullable=True)
+    owner_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'))
 
     # Встановлення зв'язку з моделлю User
-    owner = relationship("User", back_populates="items")
+    owner: Mapped["User"] = relationship("User", back_populates="items")
 
     def __repr__(self):
         return f"<Item(id={self.id}, title='{self.title}', owner_id={self.owner_id})>"
