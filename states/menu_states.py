619aa48cfabbb6b53754ab39e533296bb142e951
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-02 06:09:01 +0200

    Update menu_states.py

--- states/menu_states.py
+++ states/menu_states.py
@@ -1,17 +1,15 @@
 from aiogram.fsm.state import State, StatesGroup
 
 class MainMenuState(StatesGroup):
-    """Станів для головного меню"""
     main = State()
     settings = State()
     profile = State()
     tournaments = State()
     screenshots = State()
 
-class IntroState(StatesGroup):
-    """Станів для вступного меню"""
-    INTRO_PAGE_1 = State()
-    INTRO_PAGE_2 = State()
-    INTRO_PAGE_3 = State()
+class ProfileState(StatesGroup):
+    stats = State()
+    settings = State()
+    achievements = State()
 
-__all__ = ['MainMenuState', 'IntroState']
+__all__ = ['MainMenuState', 'ProfileState']
