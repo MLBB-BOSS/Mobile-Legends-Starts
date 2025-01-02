5c8a2c340a53b71acebc6524f01a1258566d31df
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-02 08:26:47 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,12 +1,10 @@
 # handlers/__init__.py
 
 from aiogram import Dispatcher
+from .start import router as start_router
+from .navigation import router as navigation_router
 
 def setup_handlers(dp: Dispatcher):
-    from .start import router as start_router
-    from .navigation import router as navigation_router
-
-    # Include both routers
+    # Include routers
     dp.include_router(start_router)
     dp.include_router(navigation_router)
-    # Додайте інші Routers за необхідністю
