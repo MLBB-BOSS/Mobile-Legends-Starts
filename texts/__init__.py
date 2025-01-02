9139a3d761c78f36c54cd20dd69b8277024868c3
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-02 10:19:41 +0200

    Update __init__.py

--- texts/__init__.py
+++ texts/__init__.py
@@ -1,45 +1,5 @@
 # texts/__init__.py
+NAVIGATION_MENU_TEXT = "Тут якийсь текст..."
 
-from .messages import Messages
-
-__all__ = ['Messages']
-
-from .enums import MenuButton, LanguageButton
-from .messages_intro import (
-    WELCOME_NEW_USER_TEXT,
-    INTRO_PAGE_1_TEXT,
-    INTRO_PAGE_2_TEXT,
-    INTRO_PAGE_3_TEXT,
-    # Додайте інші текстові константи
-)
-from .messages_main_menu import (
-    MAIN_MENU_TEXT,
-    MAIN_MENU_DESCRIPTION,
-    MAIN_MENU_ERROR_TEXT,
-    # Додайте інші текстові константи
-)
-from .messages_profile import (
-    PROFILE_MENU_TEXT,
-    PROFILE_INTERACTIVE_TEXT,
-    VIEW_PROFILE_TEXT,
-    EDIT_PROFILE_TEXT,
-    # Додайте інші текстові константи
-)
-# Імпортуйте інші текстові модулі аналогічно
-
-__all__ = [
-    "MenuButton",
-    "LanguageButton",
-    "WELCOME_NEW_USER_TEXT",
-    "INTRO_PAGE_1_TEXT",
-    "INTRO_PAGE_2_TEXT",
-    "INTRO_PAGE_3_TEXT",
-    "MAIN_MENU_TEXT",
-    "MAIN_MENU_DESCRIPTION",
-    "MAIN_MENU_ERROR_TEXT",
-    "PROFILE_MENU_TEXT",
-    "PROFILE_INTERACTIVE_TEXT",
-    "VIEW_PROFILE_TEXT",
-    "EDIT_PROFILE_TEXT",
-    # Додайте інші текстові константи
-]
+# або імпорт з іншого файлу
+from .some_texts import NAVIGATION_MENU_TEXT
