080275eaa0a8749eb5582500c02c4255133bcb0a
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-02 11:16:55 +0200

    Update __init__.py

--- states/__init__.py
+++ states/__init__.py
@@ -1,11 +1,13 @@
 # states/__init__.py
 
-from .intro import IntroState
-from .main_menu import MainMenuState
-# Імпортуйте інші стани тут
+from .intro_state import IntroState
+from .main_menu_state import MainMenuState
+from .navigation_state import NavigationState
+from .profile_state import ProfileState
 
 __all__ = [
     "IntroState",
     "MainMenuState",
-    # Додайте інші стани тут
-] 
+    "NavigationState",
+    "ProfileState",
+]
