d7c6bf4425cfeb790e08f729beab98ba12c97eca
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-02 08:41:55 +0200

    Update navigation.py

--- handlers/navigation.py
+++ handlers/navigation.py
@@ -1,70 +1,27 @@
 # handlers/navigation.py
 
-from aiogram import Router, F, Bot
+from aiogram import Router, F
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
-import logging
-
-from states.states import MenuStates
-from utils.constants import NAVIGATION_MENU_TEXT, NAVIGATION_INTERACTIVE_TEXT
-from utils.keyboards import get_navigation_menu, get_generic_inline_keyboard, get_main_keyboard
-from utils.message_utils import safe_delete_message
+from states.menu_states import MenuStates
+from utils.keyboards import get_navigation_menu, get_main_keyboard
 
 router = Router()
-logger = logging.getLogger(__name__)
 
-@router.message(MenuStates.MAIN_MENU, F.text == "🧭 Навігація")
-async def handle_navigation_transition(message: Message, state: FSMContext, bot: Bot):
-    # Видалення повідомлення користувача
-    await safe_delete_message(bot, message.chat.id, message.message_id)
-    
-    # Отримання даних стану
-    data = await state.get_data()
-    old_message_id = data.get('bot_message_id')
-    interactive_message_id = data.get('interactive_message_id')
-    
-    # Видалення старого "пульта"
-    if old_message_id:
-        await safe_delete_message(bot, message.chat.id, old_message_id)
-    
-    # Відправка нового "пульта"
-    new_message = await bot.send_message(
-        chat_id=message.chat.id,
-        text=NAVIGATION_MENU_TEXT,
+@router.message(F.text == "🧭 Навігація")
+async def handle_navigation_transition(message: Message, state: FSMContext):
+    await message.answer(
+        "🧭 Навігаційне меню\n\n"
+        "Оберіть розділ, який вас цікавить:",
         reply_markup=get_navigation_menu()
-    )
-    
-    # Оновлення або створення "екрану"
-    if interactive_message_id:
-        try:
-            await bot.edit_message_text(
-                chat_id=message.chat.id,
-                message_id=interactive_message_id,
-                text=NAVIGATION_INTERACTIVE_TEXT,
-                reply_markup=get_generic_inline_keyboard()
-            )
-        except TelegramBadRequest:
-            interactive_message = await bot.send_message(
-                chat_id=message.chat.id,
-                text=NAVIGATION_INTERACTIVE_TEXT,
-                reply_markup=get_generic_inline_keyboard()
-            )
-            interactive_message_id = interactive_message.message_id
-    else:
-        interactive_message = await bot.send_message(
-            chat_id=message.chat.id,
-            text=NAVIGATION_INTERACTIVE_TEXT,
-            reply_markup=get_generic_inline_keyboard()
-        )
-        interactive_message_id = interactive_message.message_id
-    
-    # Збереження нового стану
-    await state.update_data(
-        bot_message_id=new_message.message_id,
-        interactive_message_id=interactive_message_id,
-        last_text=NAVIGATION_MENU_TEXT,
-        last_keyboard=get_navigation_menu()
     )
     await state.set_state(MenuStates.NAVIGATION_MENU)
 
-# Додайте інші хендлери для навігаційного меню тут
+@router.message(MenuStates.NAVIGATION_MENU)
+async def handle_navigation_menu(message: Message, state: FSMContext):
+    if message.text == "🔙 Головне меню":
+        await message.answer(
+            "Ви повернулися до головного меню",
+            reply_markup=get_main_keyboard()
+        )
+        await state.set_state(MenuStates.MAIN_MENU)
