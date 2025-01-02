4ab09ccf3b121342029011895002fdabe9759d1f
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-02 08:21:24 +0200

    Update message_utils.py

--- utils/message_utils.py
+++ utils/message_utils.py
@@ -1,43 +1,11 @@
 # utils/message_utils.py
 
 from aiogram import Bot
-from aiogram.exceptions import TelegramBadRequest, TelegramForbiddenError
-import logging
+from aiogram.exceptions import TelegramBadRequest
 
-from aiogram.types import ReplyKeyboardMarkup, InlineKeyboardMarkup
-from typing import Union
-
-logger = logging.getLogger(__name__)
-
-async def safe_delete_message(bot: Bot, chat_id: int, message_id: int):
-    """
-    Безпечне видалення повідомлення з обробкою винятків.
-    """
+async def safe_delete_message(bot: Bot, chat_id: int, message_id: int) -> bool:
     try:
         await bot.delete_message(chat_id=chat_id, message_id=message_id)
-        logger.info(f"Повідомлення {message_id} успішно видалено в чаті {chat_id}.")
-    except (TelegramForbiddenError, TelegramBadRequest) as e:
-        logger.warning(f"Не вдалося видалити повідомлення {message_id} в чаті {chat_id}: {e}")
-    except Exception as e:
-        logger.error(f"Невідома помилка при видаленні повідомлення {message_id} в чаті {chat_id}: {e}")
-
-async def check_and_edit_message(bot: Bot, chat_id: int, message_id: int, text: str, keyboard: Union[ReplyKeyboardMarkup, InlineKeyboardMarkup]):
-    """
-    Перевіряє можливість редагування повідомлення і редагує його.
-    Повертає True, якщо редагування успішне, інакше False.
-    """
-    try:
-        await bot.edit_message_text(
-            chat_id=chat_id,
-            message_id=message_id,
-            text=text,
-            reply_markup=keyboard
-        )
-        logger.info(f"Повідомлення {message_id} в чаті {chat_id} успішно відредаговано.")
         return True
-    except TelegramBadRequest as e:
-        logger.warning(f"Не вдалося відредагувати повідомлення {message_id} в чаті {chat_id}: {e}")
-        return False
-    except Exception as e:
-        logger.error(f"Невідома помилка при редагуванні повідомлення {message_id} в чаті {chat_id}: {e}")
+    except TelegramBadRequest:
         return False
