803f6676d8a1947a558d1fda07cddcadd92fba00
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-03 01:30:28 +0200

    Update user.py

--- utils/models/user.py
+++ utils/models/user.py
@@ -1,13 +1,22 @@
 # utils/models/user.py
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from utils.base import Base  # Імпорт базового класу
+from typing import List
 
-class User:
+class User(Base):
+    """
+    Модель користувача.
+    """
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    # Інші поля...
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    # Додайте інші поля за потребою
 
-    def some_method(self):
-        from utils.db import Base
-        # Використання Base тут
+    # Встановлення зв'язку з моделлю Item
+    items: List["Item"] = relationship("Item", back_populates="owner")
+
+    def __repr__(self):
+        return f"<User(id={self.id}, name='{self.name}', email='{self.email}')>"
