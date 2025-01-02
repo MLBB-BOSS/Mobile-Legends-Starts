172f07cf1bfa15d2a223b2bd83573e46cfce4ed3
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-02 04:42:48 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,6 +1,5 @@
 from aiogram import Dispatcher
 from .intro_handler import router as intro_router
-from .menu_handler import router as menu_router  # Додайте інші обробники, якщо необхідно
 from keyboards.inline_menus import get_main_menu_keyboard
 
 def setup_handlers(dp: Dispatcher):
