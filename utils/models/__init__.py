015468fb10de9e4a0d7d2b4ed5e01332e8166bce
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-03 16:53:44 +0200

    Update __init__.py

--- utils/models/__init__.py
+++ utils/models/__init__.py
@@ -1,6 +1,5 @@
 from utils.db_base import Base
 from .user import User, UserStats
-from .feedback import Feedback
 from .bug_report import BugReport
 
 __all__ = ["Base", "User", "UserStats", "Feedback", "BugReport"]
