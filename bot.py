85f8526f948bcd1b5cf4d952c4c1ce8202ed8fa5
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-03 18:20:09 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 # bot.py
 import os
-from aiogram import Bot, Dispatcher, executor, types
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
 from utils.db import init_db, check_connection
 from dotenv import load_dotenv
 
@@ -18,14 +20,18 @@ init_db()
 
 # Створіть екземпляри бота та диспетчера
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher()
 
-@dp.message_handler(commands=['start', 'help'])
+@dp.message(Command(commands=['start', 'help']))
 async def send_welcome(message: types.Message):
     await message.reply("Привіт! Я ваш бот.")
 
-if __name__ == '__main__':
+async def main():
     if check_connection():
-        executor.start_polling(dp, skip_updates=True)
+        # Запустіть диспетчер
+        await dp.start_polling(bot)
     else:
         print("Не вдалося підключитися до бази даних.")
+
+if __name__ == '__main__':
+    asyncio.run(main())
