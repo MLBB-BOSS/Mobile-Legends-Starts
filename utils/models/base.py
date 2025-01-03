e574ea21be78c6de410147064daf321217952659
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-03 17:57:24 +0200

    Update base.py

--- utils/models/base.py
+++ utils/models/base.py
@@ -1,9 +1,18 @@
-# models/base.py
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.ext.asyncio import AsyncAttrs
+# utils/models/base.py
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
+from sqlalchemy import Column, Integer, DateTime, func
 
-class Base(AsyncAttrs, DeclarativeBase):
-    """Base class for all models"""
-    
-    def to_dict(self):
-        return {c.key: getattr(self, c.key) for c in self.__table__.columns}
+@as_declarative()
+class Base:
+    """
+    Базовий клас для всіх моделей SQLAlchemy.
+    """
+    id: int
+    __name__: str
+
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
+
+    created_at = Column(DateTime, default=func.now(), nullable=False)
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
