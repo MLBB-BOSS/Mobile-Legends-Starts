cabf548fb1d72053083ba537e6016ebdb5725a95
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-03 17:28:46 +0200

    Update base.py

--- utils/models/base.py
+++ utils/models/base.py
@@ -1,15 +1,9 @@
-# utils/models/base.py
-from sqlalchemy.ext.declarative import as_declarative, declared_attr
+# models/base.py
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import AsyncAttrs
 
-@as_declarative()
-class Base:
-    """
-    Базовий клас для всіх моделей SQLAlchemy.
-    """
-    id: int
-    __name__: str
-
-    # Автоматично генерує ім'я таблиці на основі назви класу
-    @declared_attr
-    def __tablename__(cls) -> str:
-        return cls.__name__.lower()
+class Base(AsyncAttrs, DeclarativeBase):
+    """Base class for all models"""
+    
+    def to_dict(self):
+        return {c.key: getattr(self, c.key) for c in self.__table__.columns}
