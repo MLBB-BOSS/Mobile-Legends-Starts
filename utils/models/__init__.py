1766a7a9d8cd068ac0eb633183c3823f14a12763
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-03 17:02:11 +0200

    Update __init__.py

--- utils/models/__init__.py
+++ utils/models/__init__.py
@@ -1,5 +1,6 @@
 from utils.db_base import Base
 from .user import User, UserStats
 from .bug_report import BugReport
+from .feedback import Feedback  # Додайте цей рядок
 
 __all__ = ["Base", "User", "UserStats", "Feedback", "BugReport"]
