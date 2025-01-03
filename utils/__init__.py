5ff4437f2277613d99e15d6075b204346f71a001
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-03 03:17:35 +0200

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,8 +1,9 @@
 # utils/__init__.py
-from .models import User, UserStats, Feedback, BugReport
+from .models import Base, User, UserStats, Feedback, BugReport
 from .db import async_session, init_db
 
 __all__ = [
+    'Base',
     'User',
     'UserStats',
     'Feedback',
