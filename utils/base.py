43e5eda44fe3676d8f2859c6ee23c37b587525d0
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-03 18:13:05 +0200

    Update base.py

--- utils/base.py
+++ utils/base.py
@@ -1,5 +1,18 @@
-# utils/base.py
-from sqlalchemy.ext.declarative import declarative_base
+# utils/models/base.py
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
+from sqlalchemy import Column, Integer, DateTime, func
 
-# Створення базового класу для моделей SQLAlchemy
-Base = declarative_base()
+@as_declarative()
+class Base:
+    """
+    Базовий клас для всіх моделей SQLAlchemy.
+    """
+    id: int
+    __name__: str
+
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
+
+    created_at = Column(DateTime, default=func.now(), nullable=False)
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
