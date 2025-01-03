9909cdea20827180638c109d09f6b17de7ce11b2
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-03 02:21:33 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,11 +1,5 @@
 # handlers/__init__.py
 from .base import router as base_router
+# Імпортуйте інші обробники тут
 
 __all__ = ['base_router']  # Додайте інші обробники до цього списку
-
-def setup_handlers(dp):
-    dp.include_router(base_router)
-    # Якщо у вас будуть інші роутери, додайте їх тут
-    # Наприклад:
-    # from .challenges import router as challenges_router
-    # dp.include_router(challenges_router)
