6a4e6379dad1a86da615b0cd982fa599aa138adb
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-03 17:49:11 +0200

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,14 +1,4 @@
 # utils/__init__.py
 from .models import Base, User, UserStats, Feedback, BugReport
-from .db import init_db, get_session, check_connection
 
-__all__ = [
-    'Base',
-    'User',
-    'UserStats',
-    'Feedback',
-    'BugReport',
-    'init_db',
-    'get_session',
-    'check_connection'
-]
+__all__ = ['Base', 'User', 'UserStats', 'Feedback', 'BugReport']
