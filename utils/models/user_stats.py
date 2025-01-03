21e2b3327358003e3175591b6316e771eb69f142
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-03 17:55:16 +0200

    Update user_stats.py

--- utils/models/user_stats.py
+++ utils/models/user_stats.py
@@ -1,27 +1,15 @@
-# models/user_stats.py
-
-from sqlalchemy import Column, Integer, ForeignKey, DateTime
+# utils/models/user_stats.py
+from sqlalchemy import Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from .base import Base
-from datetime import datetime
 
 class UserStats(Base):
-    __tablename__ = "user_stats"
+    __tablename__ = 'user_stats'
 
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"), unique=True, nullable=False)
-    level = Column(Integer, default=1)
-    rating = Column(Integer, default=1000)
-    achievements_count = Column(Integer, default=0)
-    screenshots_count = Column(Integer, default=0)
-    missions_count = Column(Integer, default=0)
-    quizzes_count = Column(Integer, default=0)
-    total_matches = Column(Integer, default=0)
-    total_wins = Column(Integer, default=0)
-    total_losses = Column(Integer, default=0)
-    tournament_participations = Column(Integer, default=0)
-    badges_count = Column(Integer, default=0)
-    last_update = Column(DateTime, default=datetime.utcnow)
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    games_played = Column(Integer, default=0)
+    wins = Column(Integer, default=0)
+    losses = Column(Integer, default=0)
 
-    # Відношення до користувача
     user = relationship("User", back_populates="stats")
