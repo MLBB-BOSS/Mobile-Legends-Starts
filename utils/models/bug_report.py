fadf9cd065ad4559e27c697fa01033190ab44408
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-03 17:31:51 +0200

    Update bug_report.py

--- utils/models/bug_report.py
+++ utils/models/bug_report.py
@@ -1,11 +1,16 @@
-from sqlalchemy import Column, Integer, String, DateTime
-from utils.db_base import Base
+# models/bug_report.py
+
+from sqlalchemy import Column, Integer, ForeignKey, String, DateTime
+from sqlalchemy.orm import relationship
+from utils.db import Base
 from datetime import datetime
 
 class BugReport(Base):
     __tablename__ = "bug_reports"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, nullable=False)
-    description = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    report = Column(String, nullable=False)
     timestamp = Column(DateTime, default=datetime.utcnow)
+
+    user = relationship("User", back_populates="bug_reports")
