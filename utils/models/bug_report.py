dac9ab4b6764f6f35f1fa3f86b016b5865d613fa
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-03 18:59:48 +0200

    Update bug_report.py

--- utils/models/bug_report.py
+++ utils/models/bug_report.py
@@ -1,5 +1,5 @@
 # utils/models/bug_report.py
-from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from .base import Base
 
@@ -7,6 +7,7 @@ class BugReport(Base):
     __tablename__ = 'bug_reports'
 
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)  # Змінено на 'users.id'
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)  # Відповідь на 'users.id'
+    description = Column(String, nullable=False)
 
     user = relationship("User", back_populates="bug_reports")
