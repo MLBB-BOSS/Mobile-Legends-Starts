5323c3d528435e07c8bebb8daa3c1a1722916164
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-04 05:22:57 +0200

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,1 +1,9 @@
 # keyboards/__init__.py
+from .menus import (
+    get_main_menu_keyboard,
+    get_main_menu_inline_keyboard,
+    get_navigation_menu,
+    get_navigation_inline_keyboard,
+    get_profile_menu,
+    get_profile_inline_keyboard
+)
