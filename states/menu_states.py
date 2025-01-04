b6f0cba859f08d249992f089a6315857692d66b2
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-04 10:52:38 +0200

    Update menu_states.py

--- states/menu_states.py
+++ states/menu_states.py
@@ -1,5 +1,28 @@
 from aiogram.fsm.state import State, StatesGroup
 
+class MenuStates(StatesGroup):
+    INTRO_PAGE_1 = State()
+    INTRO_PAGE_2 = State()
+    INTRO_PAGE_3 = State()
+    MAIN_MENU = State()
+    NAVIGATION_MENU = State()
+    PROFILE_MENU = State()
+    HEROES_MENU = State()
+    HERO_CLASS_MENU = State()
+    BUILDS_MENU = State()
+    VOTING_MENU = State()
+    FEEDBACK_MENU = State()
+    SETTINGS_MENU = State()
+    STATISTICS_MENU = State()
+    ACHIEVEMENTS_MENU = State()
+    SEARCH_HERO = State()
+    SEARCH_TOPIC = State()
+    REPORT_BUG = State()
+    RECEIVE_FEEDBACK = State()
+    CHANGE_USERNAME = State()
+    COMPARISON_STEP_1 = State()
+    COMPARISON_STEP_2 = State()
+
 class MainMenuState(StatesGroup):
     main = State()
     navigation = State()
