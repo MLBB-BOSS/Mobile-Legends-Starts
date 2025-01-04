7315147bccbeee4bf2d86576a9a0d52ddb0f63c2
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-04 03:49:02 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,5 +1,4 @@
 from .main_menu import router as main_menu_router
 from .profile import router as profile_router
-from .main_menu import router as main_menu_router
 
 __all__ = ['base_router', 'main_menu_router', 'profile_router']
