e9179cc267d49aafc5ea2634fb4892fe06141b19
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <74383033+MLBB-BOSS@users.noreply.github.com>
2025-01-04 05:18:22 +0200

    Update main_menu.py

--- handlers/main_menu.py
+++ handlers/main_menu.py
@@ -14,7 +14,7 @@ from keyboards.menus import (
     get_main_menu_inline_keyboard,
     get_navigation_menu,
     get_navigation_inline_keyboard,
-    get_profile_menu_keyboard,
+    get_profile_menu,
     get_profile_inline_keyboard
 )
 
